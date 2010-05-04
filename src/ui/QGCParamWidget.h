60941ff6d17931e9254f4b9aeeebb8ef499c9bb4
pixhawk <pixhawk@student.ethz.ch>
2010-05-04 08:48:34 +0200

    Finished commenting new parameter widget.

--- src/ui/QGCParamWidget.h
+++ src/ui/QGCParamWidget.h
@@ -1,3 +1,34 @@
+/*=====================================================================
+
+QGroundControl Open Source Ground Control Station
+
+(c) 2009, 2010 QGROUNDCONTROL/PIXHAWK PROJECT
+<http://www.qgroundcontrol.org>
+<http://pixhawk.ethz.ch>
+
+This file is part of the QGROUNDCONTROL project
+
+    QGROUNDCONTROL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    QGROUNDCONTROL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+
+======================================================================*/
+
+/**
+ * @file
+ *   @brief Declaration of class QGCParamWidget
+ *   @author Lorenz Meier <mail@qgroundcontrol.org>
+ */
+
 #ifndef QGCPARAMWIDGET_H
 #define QGCPARAMWIDGET_H
 
@@ -19,6 +50,7 @@ public:
     /** @brief Get the UAS of this widget */
     UASInterface* getUAS();
 signals:
+    /** @brief A parameter was changed in the widget, NOT onboard */
     void parameterChanged(int component, QString parametername, float value);
 public slots:
     /** @brief Add a component to the list */
