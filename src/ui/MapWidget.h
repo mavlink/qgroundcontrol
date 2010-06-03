ce526f7dfba0efb7df2a7b90f52064f2620ae63c
pixhawk <pixhawk@student.ethz.ch>
2010-06-03 07:52:13 +0200

    Working MAV tracking

--- src/ui/MapWidget.h
+++ src/ui/MapWidget.h
@@ -33,6 +33,7 @@ This file is part of the PIXHAWK project
 #define MAPWIDGET_H
 
 #include <QtGui/QWidget>
+#include <QMap>
 #include "qmapcontrol.h"
 #include "UASInterface.h"
 
@@ -71,7 +72,9 @@ protected:
     Layer* osmLayer;
     Layer* gSatLayer;
 
+    QMap<int, CirclePoint*> uasIcons;
     UASInterface* mav;
+    quint64 lastUpdate;
 
 private:
     Ui::MapWidget *m_ui;
