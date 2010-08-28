9c668b5c7407a25d990dded24246da15e0fc0649
Bryan Godbolt <godbolt@ualberta.ca>
2010-08-27 18:37:00 -0600

    able to read opal parameters into a map.  ready to rewrite parameter sending

--- src/comm/QGCParamID.cc
+++ src/comm/QGCParamID.cc
@@ -1,6 +1,21 @@
 #include "QGCParamID.h"
 using namespace OpalRT;
 
-QGCParamID::QGCParamID()
+QGCParamID::QGCParamID(const char *paramid):QString(paramid)
 {
 }
+
+QGCParamID::QGCParamID(const QGCParamID &other):QString(other)
+{
+
+}
+
+/*
+bool QGCParamID::operator<(const QGCParamID& other)
+{
+    const QString *lefthand, *righthand;
+    lefthand = this;
+    righthand = &other;
+    return lefthand < righthand;
+}
+*/
