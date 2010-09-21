b2f67f060e94d658e76f39bc58a8626433ca46ed
Bryan Godbolt <godbolt@ece.ualberta.ca>
2010-09-21 14:47:36 -0600

    working on radio calibration

--- src/ui/RadioCalibration/RadioCalibrationWindow.h
+++ src/ui/RadioCalibration/RadioCalibrationWindow.h
@@ -7,14 +7,21 @@
 #include <QPushButton>
 #include <QVector>
 #include <QGridLayout>
+#include <QHBoxLayout>
+#include <QDebug>
 
 #include "AirfoilServoCalibrator.h"
 #include "SwitchCalibrator.h"
 #include "CurveCalibrator.h"
 
+#include "UASManager.h"
+#include "UASInterface.h"
+#include "mavlink.h"
+
 class RadioCalibrationWindow : public QWidget
 {
 Q_OBJECT
+
 public:
     explicit RadioCalibrationWindow(QWidget *parent = 0);
 
@@ -22,6 +29,12 @@ signals:
 
 public slots:
     void setChannel(int ch, float raw, float normalized);
+    void loadFile() {this->radio->loadFile();}
+    void saveFile() {this->radio->saveFile();}
+    void send() {this->radio->send();}
+    void receive() {this->radio->receive();}
+    void setUASId(int id) {this->radio->setUASId(id);}
+
 
 protected:
         AirfoilServoCalibrator *aileron;
@@ -31,6 +44,53 @@ protected:
         CurveCalibrator *pitch;
         CurveCalibrator *throttle;
 
+        class RadioCalibrationData
+        {
+        public:
+            explicit RadioCalibrationData(RadioCalibrationWindow *parent=0);
+            RadioCalibrationData(RadioCalibrationData&);            
+            RadioCalibrationData(const QVector<float>& aileron,
+                                 const QVector<float>& elevator,
+                                 const QVector<float>& rudder,
+                                 const QVector<float>& gyro,
+                                 const QVector<float>& pitch,
+                                 const QVector<float>& throttle,
+                                 RadioCalibrationWindow *parent=0);
+
+            enum RadioElement
+            {
+                AILERON=0,
+                ELEVATOR,
+                RUDDER,
+                GYRO,
+                PITCH,
+                THROTTLE
+            };
+
+            void loadFile();
+            void saveFile();
+            void send();
+            void receive();
+            void setUASId(int id);
+
+        protected:
+            QVector<QVector<float> > *data;
+            int uasID;
+
+            void init(const QVector<float>& aileron,
+                      const QVector<float>& elevator,
+                      const QVector<float>& rudder,
+                      const QVector<float>& gyro,
+                      const QVector<float>& pitch,
+                      const QVector<float>& throttle);
+
+        private:
+            RadioCalibrationWindow *parent;
+
+
+        };
+
+        RadioCalibrationData *radio;
 };
 
 #endif // RADIOCALIBRATIONWINDOW_H
