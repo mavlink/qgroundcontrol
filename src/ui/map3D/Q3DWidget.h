ea3075794a9d7cd991ff26878acca4f914c1e745
Lionel Heng <hengli@student.ethz.ch>
2010-09-22 17:10:11 +0200

    Added some functionality to 3D View --Lionel

--- src/ui/map3D/Q3DWidget.h
+++ src/ui/map3D/Q3DWidget.h
@@ -9,40 +9,40 @@
 
 enum CameraState
 {
-	IDLE = 0,
-	ROTATING = 1,
-	MOVING = 2,
-	ZOOMING = 3
+    IDLE = 0,
+    ROTATING = 1,
+    MOVING = 2,
+    ZOOMING = 3
 };
 
 struct CameraPose
 {
-        CameraState state;
-	float pan, tilt, distance;
-	float xOffset, yOffset, zOffset;
-	float xOffset2D, yOffset2D, rotation2D, zoom, warpX, warpY;
+    CameraState state;
+    float pan, tilt, distance;
+    float xOffset, yOffset, zOffset;
+    float xOffset2D, yOffset2D, rotation2D, zoom, warpX, warpY;
 };
 
 struct CameraParams
 {
-	float zoomSensitivity;
-	float rotateSensitivity;
-	float moveSensitivity;
+    float zoomSensitivity;
+    float rotateSensitivity;
+    float moveSensitivity;
 
-	float minZoomRange;
-	float cameraFov;
-	float minClipRange;
-	float maxClipRange;
+    float minZoomRange;
+    float cameraFov;
+    float minClipRange;
+    float maxClipRange;
 
-	float zoomSensitivity2D;
-	float rotateSensitivity2D;
-	float moveSensitivity2D;
+    float zoomSensitivity2D;
+    float rotateSensitivity2D;
+    float moveSensitivity2D;
 };
 
 enum MouseState
 {
-	MOUSE_STATE_UP = 0,
-	MOUSE_STATE_DOWN = 1
+    MOUSE_STATE_UP = 0,
+    MOUSE_STATE_DOWN = 1
 };
 
 typedef void (*DisplayFunc)(void *);
@@ -52,125 +52,128 @@ typedef void (*MotionFunc)(int32_t, int32_t, void *);
 
 class Q3DWidget: public QGLWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-        explicit Q3DWidget(QWidget* parent);
-        ~Q3DWidget();
+    explicit Q3DWidget(QWidget* parent);
+    ~Q3DWidget();
 
-        void initialize(int32_t windowX, int32_t windowY,
-                        int32_t windowWidth, int32_t windowHeight, float fps);
+    void initialize(int32_t windowX, int32_t windowY,
+                    int32_t windowWidth, int32_t windowHeight, float fps);
 
-	void setCameraParams(float zoomSensitivity, float rotateSensitivity,
-						 float moveSensitivity, float minZoomRange,
-						 float camera_fov, float minClipRange,
-						 float maxClipRange);
+    void setCameraParams(float zoomSensitivity, float rotateSensitivity,
+                         float moveSensitivity, float minZoomRange,
+                         float cameraFov, float minClipRange,
+                         float maxClipRange);
 
-	void setCameraLimit(bool onoff);
+    void setCameraLimit(bool onoff);
+    void setCameraLock(bool onoff);
 
-	void set2DCameraParams(float zoomSensitivity,
-						   float rotateSensitivity,
-						   float moveSensitivity);
+    void set2DCameraParams(float zoomSensitivity,
+                           float rotateSensitivity,
+                           float moveSensitivity);
 
-	void set3D(bool onoff);
-	bool is3D(void) const;
+    void set3D(bool onoff);
+    bool is3D(void) const;
 
-	void setInitialCameraPos(float pan, float tilt, float range,
-							 float xOffset, float yOffset, float zOffset);
-	void setInitial2DCameraPos(float xOffset, float yOffset,
-							   float rotation, float zoom);
-        void setCameraPose(const CameraPose& cameraPose);
-        CameraPose getCameraPose(void) const;
+    void setInitialCameraPos(float pan, float tilt, float range,
+                             float xOffset, float yOffset, float zOffset);
+    void setInitial2DCameraPos(float xOffset, float yOffset,
+                               float rotation, float zoom);
+    void setCameraPose(const CameraPose& cameraPose);
+    CameraPose getCameraPose(void) const;
 
-        void setDisplayFunc(DisplayFunc func, void* clientData);
-        void setKeyboardFunc(KeyboardFunc func, void* clientData);
-        void setMouseFunc(MouseFunc func, void* clientData);
-        void setMotionFunc(MotionFunc func, void* clientData);
-	void addTimerFunc(uint32_t msecs, void(*func)(void *),
-					  void* clientData);
+    void setDisplayFunc(DisplayFunc func, void* clientData);
+    void setKeyboardFunc(KeyboardFunc func, void* clientData);
+    void setMouseFunc(MouseFunc func, void* clientData);
+    void setMotionFunc(MotionFunc func, void* clientData);
+    void addTimerFunc(uint32_t msecs, void(*func)(void *),
+                      void* clientData);
 
-	void forceRedraw(void);
+    void forceRedraw(void);
 
-	void set2DWarping(float warpX, float warpY);
+    void set2DWarping(float warpX, float warpY);
 
-	void recenter(void);
-	void recenter2D(void);
+    void recenter(void);
+    void recenter2D(void);
 
-	void set2DRotation(bool onoff);
+    void set2DRotation(bool onoff);
 
-        void setDisplayMode2D(void);
+    void setDisplayMode2D(void);
 
-        std::pair<float,float> getPositionIn3DMode(int32_t mouseX,
-                                                   int32_t mouseY);
+    std::pair<float,float> getPositionIn3DMode(int32_t mouseX,
+                                               int32_t mouseY);
 
-        std::pair<float,float> getPositionIn2DMode(int32_t mouseX,
-                                                   int32_t mouseY);
+    std::pair<float,float> getPositionIn2DMode(int32_t mouseX,
+                                               int32_t mouseY);
 
-	int32_t getWindowWidth(void);
-	int32_t getWindowHeight(void);
-	int32_t getLastMouseX(void);
-	int32_t getLastMouseY(void);
-	int32_t getMouseX(void);
-	int32_t getMouseY(void);
+    int32_t getWindowWidth(void);
+    int32_t getWindowHeight(void);
+    int32_t getLastMouseX(void);
+    int32_t getLastMouseY(void);
+    int32_t getMouseX(void);
+    int32_t getMouseY(void);
 
 private Q_SLOTS:
-	void userTimer(void);
+    void userTimer(void);
+
+protected:
+    void rotateCamera(float dx, float dy);
+    void zoomCamera(float dy);
+    void moveCamera(float dx, float dy);
+    void rotateCamera2D(float dx);
+    void zoomCamera2D(float dx);
+    void moveCamera2D(float dx, float dy);
+
+    void switchTo3DMode(void);
+    void setDisplayMode3D(void);
+
+    float r2d(float angle);
+    float d2r(float angle);
 
 private:
-	void rotateCamera(float dx, float dy);
-	void zoomCamera(float dy);
-	void moveCamera(float dx, float dy);
-	void rotateCamera2D(float dx);
-	void zoomCamera2D(float dx);
-	void moveCamera2D(float dx, float dy);
+    // QGLWidget events
+    void initializeGL(void);
+    void paintGL(void);
+    void resizeGL(int32_t width, int32_t height);
 
-	void switchTo3DMode(void);
-        void setDisplayMode3D(void);
+    // Qt events
+    void keyPressEvent(QKeyEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
+    void wheelEvent(QWheelEvent *wheel);
+    void timerEvent(QTimerEvent* event);
+    void closeEvent(QCloseEvent* event);
 
-	float r2d(float angle);
-	float d2r(float angle);
+    DisplayFunc userDisplayFunc;
+    KeyboardFunc userKeyboardFunc;
+    MouseFunc userMouseFunc;
+    MotionFunc userMotionFunc;
 
-	// QGLWidget events
-	void initializeGL(void);
-	void paintGL(void);
-	void resizeGL(int32_t width, int32_t height);
+    void* userDisplayFuncData;
+    void* userKeyboardFuncData;
+    void* userMouseFuncData;
+    void* userMotionFuncData;
 
-	// Qt events
-	void keyPressEvent(QKeyEvent* event);
-	void mousePressEvent(QMouseEvent* event);
-	void mouseReleaseEvent(QMouseEvent* event);
-	void mouseMoveEvent(QMouseEvent* event);
-	void wheelEvent(QWheelEvent *wheel);
-	void timerEvent(QTimerEvent* event);
-	void closeEvent(QCloseEvent* event);
+    int32_t windowWidth, windowHeight;
+    float requestedFps;
+    CameraPose cameraPose;
+    int32_t lastMouseX, lastMouseY;
 
-        DisplayFunc userDisplayFunc;
-        KeyboardFunc userKeyboardFunc;
-        MouseFunc userMouseFunc;
-        MotionFunc userMotionFunc;
+    bool _is3D;
 
-	void* userDisplayFuncData;
-	void* userKeyboardFuncData;
-	void* userMouseFuncData;
-	void* userMotionFuncData;
+    bool _forceRedraw;
+    bool allow2DRotation;
+    bool limitCamera;
+    bool lockCamera;
 
-	int32_t windowWidth, windowHeight;
-	float requestedFps;
-        CameraPose cameraPose;
-	int32_t lastMouseX, lastMouseY;
+    CameraParams cameraParams;
 
-	bool _is3D;
+    QBasicTimer timer;
 
-	bool _forceRedraw;
-	bool allow2DRotation;
-	bool limitCamera;
-
-        CameraParams cameraParams;
-
-	QBasicTimer timer;
-
-	void (*timerFunc)(void *);
-	void* timerFuncData;
+    void (*timerFunc)(void *);
+    void* timerFuncData;
 };
 
 #endif
