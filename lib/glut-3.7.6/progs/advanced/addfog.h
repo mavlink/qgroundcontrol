0940ba85b3e9a7ccd4466b03e3f12edcd2fc2106
pixhawk <pixhawk@student.ethz.ch>
2010-10-01 08:38:23 +0200

    Removed GLUT

--- lib/glut-3.7.6/progs/advanced/addfog.h
+++
@@ -1,8 +0,0 @@
-
-extern void afEyeNearFar(GLfloat fnear, GLfloat ffar);
-extern void afFogStartEnd(GLfloat start, GLfloat end);
-extern void afFogMode(GLenum mode);
-extern void afFogDensity(GLfloat density);
-extern void afFogColor(GLfloat red, GLfloat green, GLfloat blue);
-extern void afDoFinalFogPass(GLint x, GLint y, GLsizei width, GLsizei height);
-
--- lib/glut-3.7.6/progs/demos/underwater/dino.h
+++
@@ -1,9 +0,0 @@
-
-/* Copyright (c) Mark J. Kilgard, 1997.  */
-
-/* This program is freely distributable without licensing fees 
-   and is provided without guarantee or warrantee expressed or 
-   implied. This program is -not- in the public domain. */
-
-extern int makeDinosaur(void);
-
