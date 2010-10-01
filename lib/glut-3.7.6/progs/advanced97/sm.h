77c97ed421456e21d93e37b7559ad8b1647b375d
pixhawk <pixhawk@student.ethz.ch>
2010-10-01 08:38:23 +0200

    Removed GLUT

--- lib/glut-3.7.6/progs/advanced97/sm.h
+++
@@ -1,5 +0,0 @@
-void* new_smoke(float x, float y, float z, float dx, float dy, float dz,
-	int elems, float intensity, unsigned texture);
-void delete_smoke(void *smoke);
-void draw_smoke(void *smoke);
-void update_smoke(void *smoke, float tick);
