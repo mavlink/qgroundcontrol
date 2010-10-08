576ba877db2988093b3721bc5dd60b3eb181ce95
lm <pixhawk@pixhawk02.(none)>
2010-10-08 17:32:39 +0200

    Eventually fixed HUD directions

--- src/ui/HUD.cc
+++ src/ui/HUD.cc
@@ -575,7 +575,7 @@ void HUD::paintHUD()
     if ((yawTrans < 5.0) && (yawTrans > -5.0)) yawTrans = 0;
 
     // Negate to correct direction
-    yawTrans -yawTrans;
+    yawTrans = -yawTrans;
 
     //qDebug() << "yaw translation" << yawTrans << "integral" << yawInt << "difference" << yawDiff << "yaw" << yaw;
 
