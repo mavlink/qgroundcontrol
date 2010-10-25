bc7f1971f1afebfdb2ced290562ff018d1c7bf93
pixhawk <mavteam@student.ethz.ch>
2010-10-25 18:11:48 +0200

    Temporary fix for 64bit Qt error

--- src/ui/QGCRemoteControlView.cc
+++ src/ui/QGCRemoteControlView.cc
@@ -173,7 +173,7 @@ void QGCRemoteControlView::changeEvent(QEvent *e)
     QWidget::changeEvent(e);
     switch (e->type()) {
     case QEvent::LanguageChange:
-        ui->retranslateUi(this);
+        //ui->retranslateUi(this);
         break;
     default:
         break;
