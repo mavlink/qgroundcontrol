4b2be9241c277f663378ed3baa9a08f7dad17419
tecnosapiens <jfroblesc@gmail.com>
2010-11-08 16:45:08 -0600

    add widget to slugsdatasensorview

--- src/uas/SlugsMAV.h
+++ src/uas/SlugsMAV.h
@@ -37,6 +37,60 @@ public:
 public slots:
     /** @brief Receive a MAVLink message from this MAV */
     void receiveMessage(LinkInterface* link, mavlink_message_t message);
+
+signals:
+    // ESPECIAL SLUGS MESSAGE
+    void slugsCPULoad(UASInterface* uas,
+                      uint8_t sensLoad,
+                      uint8_t ctrlLoad,
+                      uint8_t batVolt,
+                      quint64 time);
+
+    void slugsAirData(UASInterface* uas,
+                      float dinamicPressure,
+                      float staticPresure,
+                      uint16_t temperature,
+                      quint64 time);
+
+    void slugsSensorBias(UASInterface* uas,
+                         double axBias,
+                         double ayBias,
+                         double azBias,
+                         double gxBias,
+                         double gyBias,
+                         double gzBias,
+                         quint64 time);
+
+    void slugsDiagnostic(UASInterface* uas,
+                         double diagFl1,
+                         double diagFl2,
+                         double diagFl3,
+                         int16_t diagSh1,
+                         int16_t diagSh2,
+                         int16_t diagSh3,
+                         quint64 time);
+
+    void slugsPilotConsolePWM(UASInterface* uas,
+                              uint16_t dt,
+                              uint16_t dla,
+                              uint16_t dra,
+                              uint16_t dr,
+                              uint16_t de,
+                              quint64 time);
+
+    void slugsPWM(UASInterface* uas,
+                  uint16_t dt_c,
+                  uint16_t dla_c,
+                  uint16_t dra_c,
+                  uint16_t dr_c,
+                  uint16_t dle_c,
+                  uint16_t dre_c,
+                  uint16_t dlf_c,
+                  uint16_t drf_c,
+                  uint16_t da1_c,
+                  uint16_t da2_c,
+                  quint64 time);
+
 };
 
 #endif // SLUGSMAV_H
