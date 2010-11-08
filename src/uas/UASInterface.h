c49b32bd2a8c9913f2b7c72bdfda7008c158f00b
tecnosapiens <jfroblesc@gmail.com>
2010-11-08 16:26:41 -0600

    add widget to SlugsDataSensorView.ui

--- src/uas/UASInterface.h
+++ src/uas/UASInterface.h
@@ -383,6 +383,62 @@ signals:
      * @param fix 0: No IR/Ultrasound sensor, N > 0: Found N active sensors
      */
     void irUltraSoundLocalizationChanged(UASInterface* uas, int fix);
+
+
+    // ESPECIAL SLUGS MESSAGE
+    void slugsCPULoad(UASInterface* uas,
+                      uint8_t sensLoad,
+                      uint8_t ctrlLoad,
+                      uint8_t batVolt,
+                      quint64 time);
+
+    void slugsAirData(UASInterface* uas,
+                      float dinamicPressure,
+                      float staticPresure,
+                      uint16_t temperature,
+                      quint64 time);
+
+    void slugsSensorBias(UASInterface* uas,
+                         double axBias,
+                         double ayBias,
+                         double azBias,
+                         double gxBias,
+                         double gyBias,
+                         double gzBias,
+                         quint64 time);
+
+    void slugsDiagnostic(UASInterface* uas,
+                         double diagFl1,
+                         double diagFl2,
+                         double diagFl3,
+                         int16_t diagSh1,
+                         int16_t diagSh2,
+                         int16_t diagSh3,
+                         quint64 time);
+
+    void slugsPilotConsolePWM(UASInterface* uas,
+                              uint16_t dt,
+                              uint16_t dla,
+                              uint16_t dra,
+                              uint16_t dr,
+                              uint16_t de,
+                              quint64 time);
+
+    void slugsPWM(UASInterface* uas,
+                  uint16_t dt_c,
+                  uint16_t dla_c,
+                  uint16_t dra_c,
+                  uint16_t dr_c,
+                  uint16_t dle_c,
+                  uint16_t dre_c,
+                  uint16_t dlf_c,
+                  uint16_t drf_c,
+                  uint16_t da1_c,
+                  uint16_t da2_c,
+                  quint64 time);
+
+
+
 };
 
 Q_DECLARE_INTERFACE(UASInterface, "org.qgroundcontrol/1.0");
