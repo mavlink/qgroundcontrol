4175b2be6d92d696dcb3b49891b4b0f209c0aadf
tecnosapiens <jfroblesc@gmail.com>
2010-11-09 18:06:38 -0600

    add slugs message to slugsview

--- src/uas/SlugsMAV.cc
+++ src/uas/SlugsMAV.cc
@@ -32,7 +32,7 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
 
 #ifdef MAVLINK_ENABLED_SLUGS
 
-    case MAVLINK_MSG_ID_CPU_LOAD:
+    case MAVLINK_MSG_ID_CPU_LOAD://170
         {
             mavlink_cpu_load_t cpu_load;
             quint64 time = getUnixTime(0);
@@ -42,15 +42,15 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId, tr("ControlDSC Load"), cpu_load.ctrlLoad, time);
                 emit valueChanged(uasId, tr("Battery Volt"), cpu_load.batVolt, time);
 
-//                emit slugsCPULoad(uasId,
-//                                  cpu_load.sensLoad,
-//                                  cpu_load.ctrlLoad,
-//                                  cpu_load.batVolt,
-//                                  0);
-
+                emit slugsCPULoad(uasId,
+                                  cpu_load.sensLoad,
+                                  cpu_load.ctrlLoad,
+                                  cpu_load.batVolt,
+                                  0);
+qDebug()<<"--------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_CPU_LOAD 170";
             break;
         }
-    case MAVLINK_MSG_ID_AIR_DATA:
+    case MAVLINK_MSG_ID_AIR_DATA://171
         {
             mavlink_air_data_t air_data;
             quint64 time = getUnixTime(0);
@@ -59,15 +59,15 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId, tr("Static Pressure"),air_data.staticPressure, time);
                 emit valueChanged(uasId, tr("Temp"),air_data.temperature,time);
 
-//                emit slugsAirData(uasId,
-//                                  air_data.dynamicPressure,
-//                                  air_data.staticPressure,
-//                                  air_data.temperature,
-//                                  time);
-
+                emit slugsAirData(uasId,
+                                  air_data.dynamicPressure,
+                                  air_data.staticPressure,
+                                  air_data.temperature,
+                                  time);
+qDebug()<<"--------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_AIR_DATA 171";
             break;
         }
-    case MAVLINK_MSG_ID_SENSOR_BIAS:
+    case MAVLINK_MSG_ID_SENSOR_BIAS://172
         {
             mavlink_sensor_bias_t sensor_bias;
             quint64 time = getUnixTime(0);
@@ -79,7 +79,7 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId,tr("Gy Bias"),sensor_bias.gyBias,time);
                 emit valueChanged(uasId,tr("Gz Bias"),sensor_bias.gzBias,time);
 
-                emit slugsSensorBias(this,
+                emit slugsSensorBias(uasId,
                                      sensor_bias.axBias,
                                      sensor_bias.ayBias,
                                      sensor_bias.azBias,
@@ -87,12 +87,12 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                                      sensor_bias.gyBias,
                                      sensor_bias.gzBias,
                                      0);
-                qDebug()<<"Entro";
+               qDebug()<<"------------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_SENSOR_BIAS 172";
 
 
             break;
         }
-    case MAVLINK_MSG_ID_DIAGNOSTIC:
+    case MAVLINK_MSG_ID_DIAGNOSTIC://173
         {
             mavlink_diagnostic_t diagnostic;
             quint64 time = getUnixTime(0);
@@ -104,18 +104,18 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId,tr("Diag S2"),diagnostic.diagSh2,time);
                 emit valueChanged(uasId,tr("Diag S3"),diagnostic.diagSh3,time);
 
-//                emit slugsDiagnostic(uasId,
-//                                     diagnostic.diagFl1,
-//                                     diagnostic.diagFl2,
-//                                     diagnostic.diagFl3,
-//                                     diagnostic.diagSh1,
-//                                     diagnostic.diagSh2,
-//                                     diagnostic.diagSh3,
-//                                     0);
-
+                emit slugsDiagnostic(uasId,
+                                     diagnostic.diagFl1,
+                                     diagnostic.diagFl2,
+                                     diagnostic.diagFl3,
+                                     diagnostic.diagSh1,
+                                     diagnostic.diagSh2,
+                                     diagnostic.diagSh3,
+                                     0);
+qDebug()<<"------->>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_DIAGNOSTIC 173";
             break;
         }
-    case MAVLINK_MSG_ID_PILOT_CONSOLE:
+    case MAVLINK_MSG_ID_PILOT_CONSOLE://174
         {
             mavlink_pilot_console_t pilot;
             quint64 time = getUnixTime(0);
@@ -126,18 +126,19 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId,"dr",pilot.dr,time);
                 emit valueChanged(uasId,"de",pilot.de,time);
 
-//                emit slugsPilotConsolePWM(uasId,
-//                                          pilot.dt,
-//                                          pilot.dla,
-//                                          pilot.dra,
-//                                          pilot.dr,
-//                                          pilot.de,
-//                                          0);
+                emit slugsPilotConsolePWM(uasId,
+                                          pilot.dt,
+                                          pilot.dla,
+                                          pilot.dra,
+                                          pilot.dr,
+                                          pilot.de,
+                                          0);
+                qDebug()<<"---------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_PILOT_CONSOLE 174";
 
 
             break;
         }
-    case MAVLINK_MSG_ID_PWM_COMMANDS:
+    case MAVLINK_MSG_ID_PWM_COMMANDS://175
         {
             mavlink_pwm_commands_t pwm;
             quint64 time = getUnixTime(0);
@@ -153,23 +154,189 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                 emit valueChanged(uasId,"da1_c",pwm.aux1,time);
                 emit valueChanged(uasId,"da2_c",pwm.aux2,time);
 
-//                emit slugsPWM(uasId,
-//                              pwm.dt_c,
-//                              pwm.dla_c,
-//                              pwm.dra_c,
-//                              pwm.dr_c,
-//                              pwm.dle_c,
-//                              pwm.dre_c,
-//                              pwm.dlf_c,
-//                              pwm.drf_c,
-//                              pwm.aux1,
-//                              pwm.aux2,
-//                              0);
+                emit slugsPWM(uasId,
+                              pwm.dt_c,
+                              pwm.dla_c,
+                              pwm.dra_c,
+                              pwm.dr_c,
+                              pwm.dle_c,
+                              pwm.dre_c,
+                              pwm.dlf_c,
+                              pwm.drf_c,
+                              pwm.aux1,
+                              pwm.aux2,
+                              0);
+qDebug()<<"----------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_PWM_COMMANDS 175";
+
+            break;
+        }
+    case MAVLINK_MSG_ID_SLUGS_NAVIGATION://176
+        {
+            mavlink_slugs_navigation_t nav;
+            quint64 time = getUnixTime(0);
+            mavlink_msg_slugs_navigation_decode(&message,&nav);
+                emit valueChanged(uasId,"u_m",nav.u_m,time);
+                emit valueChanged(uasId,"phi_c",nav.phi_c,time);
+                emit valueChanged(uasId,"theta_c",nav.theta_c,time);
+                emit valueChanged(uasId,"psiDot_c",nav.psiDot_c,time);
+                emit valueChanged(uasId,"ay_body",nav.ay_body,time);
+                emit valueChanged(uasId,"totalDist",nav.totalDist,time);
+                emit valueChanged(uasId,"dist2Go",nav.dist2Go,time);
+                emit valueChanged(uasId,"fromWP",nav.fromWP,time);
+                emit valueChanged(uasId,"toWP",nav.toWP,time);
+
+                emit slugsNavegation(uasId,
+                                     nav.u_m,
+                                     nav.phi_c,
+                                     nav.theta_c,
+                                     nav.psiDot_c,
+                                     nav.ay_body,
+                                     nav.totalDist,
+                                     nav.dist2Go,
+                                     nav.fromWP,
+                                     nav.toWP,
+                                     time);
+
+            qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_SLUGS_NAVIGATION 176";
+
+            break;
+        }
+    case MAVLINK_MSG_ID_DATA_LOG://177
+        {
+            mavlink_data_log_t dataLog;
+            quint64 time = getUnixTime(0);
+            mavlink_msg_data_log_decode(&message,&dataLog);
+                emit valueChanged(uasId,"fl_1",dataLog.fl_1,time);
+                emit valueChanged(uasId,"fl_2",dataLog.fl_2,time);
+                emit valueChanged(uasId,"fl_3",dataLog.fl_3,time);
+                emit valueChanged(uasId,"fl_4",dataLog.fl_4,time);
+                emit valueChanged(uasId,"fl_5",dataLog.fl_5,time);
+                emit valueChanged(uasId,"fl_6",dataLog.fl_6,time);
+
+                emit slugsDataLog(uasId,
+                                  dataLog.fl_1,
+                                  dataLog.fl_2,
+                                  dataLog.fl_3,
+                                  dataLog.fl_4,
+                                  dataLog.fl_5,
+                                  dataLog.fl_6,
+                                  time);
+
+                qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO LOG DATA 177";
+
 
 
             break;
         }
 
+    case MAVLINK_MSG_ID_FILTERED_DATA://178
+        {
+
+            mavlink_filtered_data_t fil;
+            quint64 time = getUnixTime(0);
+            mavlink_msg_filtered_data_decode(&message,&fil);
+                emit valueChanged(uasId,"aX",fil.aX,time);
+                emit valueChanged(uasId,"aY",fil.aY,time);
+                emit valueChanged(uasId,"aZ",fil.aZ,time);
+                emit valueChanged(uasId,"gX",fil.gX,time);
+                emit valueChanged(uasId,"gY",fil.gY,time);
+                emit valueChanged(uasId,"gZ",fil.gZ,time);
+                emit valueChanged(uasId,"mX",fil.mX,time);
+                emit valueChanged(uasId,"mY",fil.mY,time);
+                emit valueChanged(uasId,"mZ",fil.mZ,time);
+
+                emit slugsFilteredData(uasId,
+                                       fil.aX,
+                                       fil.aY,
+                                       fil.aZ,
+                                       fil.gX,
+                                       fil.gY,
+                                       fil.gZ,
+                                       fil.mX,
+                                       fil.mY,
+                                       fil.mZ,
+                                       time);
+
+
+
+                qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO filtering data 178";
+
+
+
+            break;
+        }
+    case MAVLINK_MSG_ID_GPS_DATE_TIME://179
+        {
+            mavlink_gps_date_time_t gps;
+            quint64 time = getUnixTime(0);
+            mavlink_msg_gps_date_time_decode(&message,&gps);
+                emit valueChanged(uasId,"year",gps.year,time);
+                emit valueChanged(uasId,"month",gps.month,time);
+                emit valueChanged(uasId,"day",gps.day,time);
+                emit valueChanged(uasId,"hour",gps.hour,time);
+                emit valueChanged(uasId,"min",gps.min,time);
+                emit valueChanged(uasId,"sec",gps.sec,time);
+                emit valueChanged(uasId,"visSat",gps.visSat,time);
+
+
+                emit slugsGPSDateTime(uasId,
+                                       gps.year,
+                                       gps.month,
+                                       gps.day,
+                                       gps.hour,
+                                       gps.min,
+                                       gps.sec,
+                                       gps.visSat,
+                                       time);
+
+
+
+                qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_GPS_DATE_TIME 179";
+
+
+
+            break;
+        }
+    case MAVLINK_MSG_ID_MID_LVL_CMDS://180
+        {
+
+
+                qDebug()<<"------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_MID_LVL_CMDS 180";
+
+
+
+            break;
+        }
+    case MAVLINK_MSG_ID_CTRL_SRFC_PT://181
+        {
+
+
+                qDebug()<<"--------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_CTRL_SRFC_PT 181";
+
+
+
+            break;
+        }
+    case MAVLINK_MSG_ID_PID://182
+        {
+
+
+                qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_PID 182";
+
+
+
+            break;
+        }
+    case MAVLINK_MSG_ID_SLUGS_ACTION://183
+        {
+
+
+                qDebug()<<"-------->>>>>>>>>>>>>>> EMITIENDO MAVLINK_MSG_ID_SLUGS_ACTION 183";
+
+
+
+            break;
+        }
 #endif
 
     default:
--- src/ui/SlugsDataSensorView.h
+++ src/ui/SlugsDataSensorView.h
@@ -83,7 +83,7 @@ public slots:
          *
          * Adds the UAS and makes all the correct connections for data display on the Widgets
     */
-    void slugLocalPositionChanged(UASInterface* uasTemp,
+    void slugLocalPositionChanged(UASInterface* uas,
                                   double x,
                                   double y,
                                   double z,
@@ -93,7 +93,7 @@ public slots:
          *
          * Adds the UAS and makes all the correct connections for data display on the Widgets
     */
-    void slugSpeedLocalPositionChanged(UASInterface* uasTemp,
+    void slugSpeedLocalPositionChanged(UASInterface* uas,
                                        double vx,
                                        double vy,
                                        double vz,
@@ -103,7 +103,7 @@ public slots:
          *
          * Adds the UAS and makes all the correct connections for data display on the Widgets
     */
-    void slugAttitudeChanged(UASInterface* uasTemp,
+    void slugAttitudeChanged(UASInterface* uas,
                              double slugroll,
                              double slugpitch,
                              double slugyaw,
@@ -113,31 +113,135 @@ public slots:
          *
          * Adds the UAS and makes all the correct connections for data display on the Widgets
     */
-    void slugsSensorBiasChanged(UASInterface* uasTemp,
-                                            double axb,
-                                            double ayb,
-                                            double azb,
-                                            double gxb,
-                                            double gyb,
-                                            double gzb,
-                                            quint64 time);
+    void slugsSensorBiasChanged(int systemId,
+                                double axb,
+                                double ayb,
+                                double azb,
+                                double gxb,
+                                double gyb,
+                                double gzb,
+                                quint64 time);
     /**
          * @brief Adds the UAS for data display
          *
          * Adds the UAS and makes all the correct connections for data display on the Widgets
     */
-//    void slugsDiagnosticMessageChanged(UASInterface* uasTemp,
-//                                       double diagfl1,
-//                                       double diagfl2,
-//                                       double diagfl3,
-//                                       int16_t diagsh1,
-//                                       int16_t diagsh2,
-//                                       int16_t diagsh3,
-//                                       quint64 time);
+    void slugsDiagnosticMessageChanged(int systemId,
+                                       double diagfl1,
+                                       double diagfl2,
+                                       double diagfl3,
+                                       int16_t diagsh1,
+                                       int16_t diagsh2,
+                                       int16_t diagsh3,
+                                       quint64 time);
+
+    /**
+         * @brief Adds the UAS for data display
+         *
+         * Adds the UAS and makes all the correct connections for data display on the Widgets
+    */
+    void slugsCpuLoadChanged(int systemId,
+                             uint8_t sensload,
+                             uint8_t ctrlload,
+                             uint8_t batvolt,
+                             quint64 time);
+
+
+    /**
+         * @brief Adds the UAS for data display
+         *
+         * Adds the UAS and makes all the correct connections for data display on the Widgets
+    */
+    void slugsNavegationChanged(int systemId,
+                                double navu_m,
+                                double navphi_c,
+                                double navtheta_c,
+                                double navpsiDot_c,
+                                double navay_body,
+                                double navtotalDist,
+                                double navdist2Go,
+                                uint8_t navfromWP,
+                                uint8_t navtoWP,
+                                quint64 time);
+
+    /**
+         * @brief Adds the UAS for data display
+         *
+         * Adds the UAS and makes all the correct connections for data display on the Widgets
+    */
+   void  slugsDataLogChanged(int systemId,
+                            double logfl_1,
+                            double logfl_2,
+                            double logfl_3,
+                            double logfl_4,
+                            double logfl_5,
+                            double logfl_6,
+                            quint64 time);
+
+   /**
+        * @brief Adds the UAS for data display
+        *
+        * Adds the UAS and makes all the correct connections for data display on the Widgets
+   */
+
+   void slugsPWMChanged(int systemId,
+                        uint16_t vdt_c,
+                        uint16_t vdla_c,
+                        uint16_t vdra_c,
+                        uint16_t vdr_c,
+                        uint16_t vdle_c,
+                        uint16_t vdre_c,
+                        uint16_t vdlf_c,
+                        uint16_t vdrf_c,
+                        uint16_t vda1_c,
+                        uint16_t vda2_c,
+                        quint64 time);
+
+   /**
+        * @brief Adds the UAS for data display
+        *
+        * Adds the UAS and makes all the correct connections for data display on the Widgets
+   */
+   void slugsFilteredDataChanged(int systemId,
+                                 double filaX,
+                                 double filaY,
+                                 double filaZ,
+                                 double filgX,
+                                 double filgY,
+                                 double filgZ,
+                                 double filmX,
+                                 double filmY,
+                                 double filmZ,
+                                 quint64 time);
+
+    void slugsGlobalPositionChanged(UASInterface* uas,
+                                    double lat,
+                                    double lon,
+                                    double alt,
+                                    quint64 time);
+
+
+
+    void slugsGPSDateTimeChanged(int systemId,
+                                    uint8_t gpsyear,
+                                     uint8_t gpsmonth,
+                                     uint8_t gpsday,
+                                     uint8_t gpshour,
+                                     uint8_t gpsmin,
+                                     uint8_t gpssec,
+                                    uint8_t gpsvisSat,
+                                    quint64 time);
+
 
 protected:
     QTimer* updateTimer;
      UASInterface* activeUAS;
+
+     //Global Position
+     double Latitude;
+     double Longitude;
+     double Height;
+     quint64 timeGlobalPosition;
 
      // Position and Attitude
      //Position
@@ -177,7 +281,67 @@ protected:
      int16_t diagSh3;
      quint64 timeDiagnostic;
 
+     //CPU Load
+     uint8_t sensLoad;
+     uint8_t ctrlLoad;
+     uint8_t batVolt;
+     quint64 timeCpuLoad;
 
+     //navigation data
+     double u_m;
+     double phi_c;
+     double theta_c;
+     double psiDot_c;
+     double ay_body;
+     double totalDist;
+     double dist2Go;
+     uint8_t fromWP;
+     uint8_t toWP;
+     quint64 timeNavigation;
+
+     // Data Log
+     double Logfl_1;
+     double Logfl_2;
+     double Logfl_3;
+     double Logfl_4;
+     double Logfl_5;
+     double Logfl_6;
+     quint64 timeDataLog;
+
+     //pwm commands
+     uint16_t dt_c; ///< AutoPilot's throttle command
+     uint16_t dla_c; ///< AutoPilot's left aileron command
+     uint16_t dra_c; ///< AutoPilot's right aileron command
+     uint16_t dr_c; ///< AutoPilot's rudder command
+     uint16_t dle_c; ///< AutoPilot's left elevator command
+     uint16_t dre_c; ///< AutoPilot's right elevator command
+     uint16_t dlf_c; ///< AutoPilot's left  flap command
+     uint16_t drf_c; ///< AutoPilot's right flap command
+     uint16_t aux1; ///< AutoPilot's aux1 command
+     uint16_t aux2; ///< AutoPilot's aux2 command
+     quint64 timePWMCommand;
+
+     //filtered data
+     double aX; ///< Accelerometer X value (m/s^2)
+     double aY; ///< Accelerometer Y value (m/s^2)
+     double aZ; ///< Accelerometer Z value (m/s^2)
+     double gX; ///< Gyro X value (rad/s)
+     double gY; ///< Gyro Y value (rad/s)
+     double gZ; ///< Gyro Z value (rad/s)
+     double mX; ///< Magnetometer X (normalized to 1)
+     double mY; ///< Magnetometer Y (normalized to 1)
+     double mZ; ///< Magnetometer Z (normalized to 1)
+     quint64 timeFiltered;
+
+     //gps date and time
+     uint8_t year; ///< Year reported by Gps
+     uint8_t month; ///< Month reported by Gps
+     uint8_t day; ///< Day reported by Gps
+     uint8_t hour; ///< Hour reported by Gps
+     uint8_t min; ///< Min reported by Gps
+     uint8_t sec; ///< Sec reported by Gps
+     uint8_t visSat; ///< Visible sattelites reported by Gps
+     quint64 timeGPSDateTime;
 
 
 private:
