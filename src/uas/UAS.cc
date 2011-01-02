73b9e202c9bb0fabe3bccc70ba5e8df9d4bbff1e
pixhawk <tv@.(none)>
2011-01-02 19:42:22 +0100

    Fixed windows compile errors

--- src/uas/UAS.cc
+++ src/uas/UAS.cc
@@ -26,6 +26,19 @@
 #include "LinkManager.h"
 #include "SerialLink.h"
 
+#ifndef M_PI
+#define M_PI        3.14159265358979323846  /* pi */
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2      1.57079632679489661923  /* pi/2 */
+#endif
+
+#ifndef M_PI_4
+#define M_PI_4      0.78539816339744830962  /* pi/4 */
+#endif
+
+
 
 UAS::UAS(MAVLinkProtocol* protocol, int id) : UASInterface(),
 uasId(id),
