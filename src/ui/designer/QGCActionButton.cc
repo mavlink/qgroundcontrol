3b7e0db1c7da7eb7c3df8a9f1da292ed0c36b0ad
Lionel Heng <hengli@student.ethz.ch>
2011-01-04 00:08:41 +0100

    Improved custom widget functionality.

--- src/ui/designer/QGCActionButton.cc
+++ src/ui/designer/QGCActionButton.cc
@@ -1,18 +1,87 @@
 #include "QGCActionButton.h"
 #include "ui_QGCActionButton.h"
 
+#include "MAVLinkProtocol.h"
+#include "UASManager.h"
+
+const char* kActionLabels[MAV_ACTION_NB] =
+{"HOLD",
+ "START MOTORS",
+ "LAUNCH",
+ "RETURN",
+ "EMERGENCY LAND",
+ "EMERGENCY KILL",
+ "CONFIRM KILL",
+ "CONTINUE",
+ "STOP MOTORS",
+ "HALT",
+ "SHUTDOWN",
+ "REBOOT",
+ "SET MANUAL",
+ "SET AUTO",
+ "READ STORAGE",
+ "WRITE STORAGE",
+ "CALIBRATE RC",
+ "CALIBRATE GYRO",
+ "CALIBRATE MAG",
+ "CALIBRATE PRESSURE",
+ "START REC",
+ "PAUSE REC",
+ "STOP REC",
+ "TAKEOFF",
+ "NAVIGATE",
+ "LAND",
+ "LOITER",
+ "SET ORIGIN",
+ "RELAY ON",
+ "RELAY OFF",
+ "GET IMAGE",
+ "START VIDEO",
+ "STOP VIDEO",
+ "RESET MAP"};
+
 QGCActionButton::QGCActionButton(QWidget *parent) :
     QGCToolWidgetItem(parent),
-    ui(new Ui::QGCActionButton)
+    ui(new Ui::QGCActionButton),
+    uas(NULL)
 {
     ui->setupUi(this);
+
+    connect(UASManager::instance(), SIGNAL(activeUASSet(UASInterface*)),
+            this, SLOT(setActiveUAS(UASInterface*)));
+
+    connect(ui->actionButton, SIGNAL(clicked()), this, SLOT(sendAction()));
     connect(ui->editFinishButton, SIGNAL(clicked()), this, SLOT(endEditMode()));
+    connect(ui->editButtonName, SIGNAL(textChanged(QString)), this, SLOT(setActionButtonName(QString)));
+    connect(ui->editActionComboBox, SIGNAL(currentIndexChanged(QString)), ui->nameLabel, SLOT(setText(QString)));
     endEditMode();
+
+    // add action labels to combobox
+    for (int i = 0; i < MAV_ACTION_NB; i++)
+    {
+        ui->editActionComboBox->addItem(kActionLabels[i]);
+    }
 }
 
 QGCActionButton::~QGCActionButton()
 {
     delete ui;
+}
+
+void QGCActionButton::sendAction()
+{
+    if (uas)
+    {
+        MAV_ACTION action = static_cast<MAV_ACTION>(
+                ui->editActionComboBox->currentIndex());
+
+        uas->setAction(action);
+    }
+}
+
+void QGCActionButton::setActionButtonName(QString text)
+{
+    ui->actionButton->setText(text);
 }
 
 void QGCActionButton::startEditMode()
@@ -29,4 +98,9 @@ void QGCActionButton::endEditMode()
     ui->editActionsRefreshButton->hide();
     ui->editFinishButton->hide();
     isInEditMode = false;
+}
+
+void QGCActionButton::setActiveUAS(UASInterface *uas)
+{
+    this->uas = uas;
 }
