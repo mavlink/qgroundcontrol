9ec8923194915c00d870aac158038d3c552f4cc5
Alejandro <am.alex09@gmail.com>
2011-01-07 10:13:40 -0600

    send and receive datagram

--- src/ui/MainWindow.cc
+++ src/ui/MainWindow.cc
@@ -201,6 +201,22 @@ void MainWindow::buildCommonWidgets()
         protocolWidget    = new XMLCommProtocolWidget(this);
         addToCentralWidgetsMenu (protocolWidget, "Mavlink Generator", SLOT(showCentralWidget()),CENTRAL_PROTOCOL);
     }
+
+    //TODO temporaly debug
+    if (!slugsHilSimWidget)
+    {
+        slugsHilSimWidget = new QDockWidget(tr("Slugs Hil Sim"), this);
+        slugsHilSimWidget->setWidget( new SlugsHilSim(this));
+        addToToolsMenu (slugsHilSimWidget, tr("HIL Sim Configuration"), SLOT(showToolWidget()), MENU_SLUGS_HIL, Qt::LeftDockWidgetArea);
+    }
+
+    //TODO temporaly debug
+    if (!slugsCamControlWidget)
+    {
+        slugsCamControlWidget = new QDockWidget(tr("Slugs Video Camera Control"), this);
+        slugsCamControlWidget->setWidget(new SlugsVideoCamControl(this));
+        addToToolsMenu (slugsCamControlWidget, tr("Camera Control"), SLOT(showToolWidget()), MENU_SLUGS_CAMERA, Qt::BottomDockWidgetArea);
+    }
 }
 
 void MainWindow::buildPxWidgets()
@@ -378,12 +394,12 @@ void MainWindow::buildSlugsWidgets()
         addToToolsMenu (slugsHilSimWidget, tr("HIL Sim Configuration"), SLOT(showToolWidget()), MENU_SLUGS_HIL, Qt::LeftDockWidgetArea);
     }
 
-    if (!slugsCamControlWidget)
-    {
-        slugsCamControlWidget = new QDockWidget(tr("Slugs Video Camera Control"), this);
-        slugsCamControlWidget->setWidget(new SlugsVideoCamControl(this));
-        addToToolsMenu (slugsCamControlWidget, tr("Camera Control"), SLOT(showToolWidget()), MENU_SLUGS_CAMERA, Qt::BottomDockWidgetArea);
-    }
+//    if (!slugsCamControlWidget)
+//    {
+//        slugsCamControlWidget = new QDockWidget(tr("Slugs Video Camera Control"), this);
+//        slugsCamControlWidget->setWidget(new SlugsVideoCamControl(this));
+//        addToToolsMenu (slugsCamControlWidget, tr("Camera Control"), SLOT(showToolWidget()), MENU_SLUGS_CAMERA, Qt::BottomDockWidgetArea);
+//    }
 }
 
 
@@ -701,6 +717,12 @@ void MainWindow::connectCommonWidgets()
 
         // it notifies that a waypoint global goes to do create and a map graphic too
         connect(waypointsDockWidget->widget(), SIGNAL(createWaypointAtMap(QPointF)), mapWidget, SLOT(createWaypointGraphAtMap(QPointF)));
+    }
+
+    //TODO temporaly debug
+    if (slugsHilSimWidget && slugsHilSimWidget->widget()){
+        connect(UASManager::instance(), SIGNAL(activeUASSet(UASInterface*)),
+                slugsHilSimWidget->widget(), SLOT(activeUasSet(UASInterface*)));
     }
 }
 
