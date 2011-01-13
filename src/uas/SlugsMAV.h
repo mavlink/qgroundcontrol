537e83b188aba3632764475295e970dae44debbb
lm <pixhawk@student.ethz.ch>
2011-01-13 20:40:32 +0100

    Fixed include guard fails for SLUGS message set

--- src/uas/SlugsMAV.h
+++ src/uas/SlugsMAV.h
@@ -36,7 +36,9 @@ class SlugsMAV : public UAS
 public:
     SlugsMAV(MAVLinkProtocol* mavlink, int id = 0);
 
+#ifdef MAVLINK_ENABLED_SLUGS
     mavlink_pwm_commands_t* getPwmCommands();
+#endif
 
 public slots:
     /** @brief Receive a MAVLink message from this MAV */
