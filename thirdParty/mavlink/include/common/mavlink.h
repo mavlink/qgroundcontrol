3a06de877c7785154bafbee5be0fe321e5b5a9f7
lm <mail@qgroundcontrol.org>
2011-09-04 10:17:58 +0200

    Added correct MAVLink version to thirdparty directory

--- thirdParty/mavlink/include/common/mavlink.h
+++ thirdParty/mavlink/include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Wednesday, July 27 2011, 14:17 UTC
+ *	 Generated on Monday, August 22 2011, 15:48 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
--- thirdParty/mavlink/include/pixhawk/mavlink.h
+++ thirdParty/mavlink/include/pixhawk/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Wednesday, July 27 2011, 14:17 UTC
+ *	 Generated on Wednesday, August 17 2011, 06:03 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
