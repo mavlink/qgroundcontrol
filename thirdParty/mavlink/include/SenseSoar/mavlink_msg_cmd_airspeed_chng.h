1ec93e7117dcca57a425c2e350a3aca7daea7936
LM <lm@student.ethz.ch>
2012-01-11 14:53:24 +0100

    Updated sensesoar to MAVLink 1.0, updated APM

--- thirdParty/mavlink/include/SenseSoar/mavlink_msg_cmd_airspeed_chng.h
+++ thirdParty/mavlink/include/SenseSoar/mavlink_msg_cmd_airspeed_chng.h
@@ -2,13 +2,24 @@
 
 #define MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG 192
 
-typedef struct __mavlink_cmd_airspeed_chng_t 
+typedef struct __mavlink_cmd_airspeed_chng_t
 {
-	uint8_t target; ///< Target ID
-	float spCmd; ///< commanded airspeed
-
+ float spCmd; ///< commanded airspeed
+ uint8_t target; ///< Target ID
 } mavlink_cmd_airspeed_chng_t;
 
+#define MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG_LEN 5
+#define MAVLINK_MSG_ID_192_LEN 5
+
+
+
+#define MAVLINK_MESSAGE_INFO_CMD_AIRSPEED_CHNG { \
+	"CMD_AIRSPEED_CHNG", \
+	2, \
+	{  { "spCmd", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_cmd_airspeed_chng_t, spCmd) }, \
+         { "target", NULL, MAVLINK_TYPE_UINT8_T, 0, 4, offsetof(mavlink_cmd_airspeed_chng_t, target) }, \
+         } \
+}
 
 
 /**
@@ -21,19 +32,29 @@ typedef struct __mavlink_cmd_airspeed_chng_t
  * @param spCmd commanded airspeed
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_msg_cmd_airspeed_chng_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, uint8_t target, float spCmd)
+static inline uint16_t mavlink_msg_cmd_airspeed_chng_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+						       uint8_t target, float spCmd)
 {
-	uint16_t i = 0;
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+	char buf[5];
+	_mav_put_float(buf, 0, spCmd);
+	_mav_put_uint8_t(buf, 4, target);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 5);
+#else
+	mavlink_cmd_airspeed_chng_t packet;
+	packet.spCmd = spCmd;
+	packet.target = target;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 5);
+#endif
+
 	msg->msgid = MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG;
-
-	i += put_uint8_t_by_index(target, i, msg->payload); // Target ID
-	i += put_float_by_index(spCmd, i, msg->payload); // commanded airspeed
-
-	return mavlink_finalize_message(msg, system_id, component_id, i);
+	return mavlink_finalize_message(msg, system_id, component_id, 5, 209);
 }
 
 /**
- * @brief Pack a cmd_airspeed_chng message
+ * @brief Pack a cmd_airspeed_chng message on a channel
  * @param system_id ID of this system
  * @param component_id ID of this component (e.g. 200 for IMU)
  * @param chan The MAVLink channel this message was sent over
@@ -42,15 +63,26 @@ static inline uint16_t mavlink_msg_cmd_airspeed_chng_pack(uint8_t system_id, uin
  * @param spCmd commanded airspeed
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_msg_cmd_airspeed_chng_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, uint8_t target, float spCmd)
+static inline uint16_t mavlink_msg_cmd_airspeed_chng_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+							   mavlink_message_t* msg,
+						           uint8_t target,float spCmd)
 {
-	uint16_t i = 0;
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+	char buf[5];
+	_mav_put_float(buf, 0, spCmd);
+	_mav_put_uint8_t(buf, 4, target);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 5);
+#else
+	mavlink_cmd_airspeed_chng_t packet;
+	packet.spCmd = spCmd;
+	packet.target = target;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 5);
+#endif
+
 	msg->msgid = MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG;
-
-	i += put_uint8_t_by_index(target, i, msg->payload); // Target ID
-	i += put_float_by_index(spCmd, i, msg->payload); // commanded airspeed
-
-	return mavlink_finalize_message_chan(msg, system_id, component_id, chan, i);
+	return mavlink_finalize_message_chan(msg, system_id, component_id, chan, 5, 209);
 }
 
 /**
@@ -77,13 +109,25 @@ static inline uint16_t mavlink_msg_cmd_airspeed_chng_encode(uint8_t system_id, u
 
 static inline void mavlink_msg_cmd_airspeed_chng_send(mavlink_channel_t chan, uint8_t target, float spCmd)
 {
-	mavlink_message_t msg;
-	mavlink_msg_cmd_airspeed_chng_pack_chan(mavlink_system.sysid, mavlink_system.compid, chan, &msg, target, spCmd);
-	mavlink_send_uart(chan, &msg);
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+	char buf[5];
+	_mav_put_float(buf, 0, spCmd);
+	_mav_put_uint8_t(buf, 4, target);
+
+	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG, buf, 5, 209);
+#else
+	mavlink_cmd_airspeed_chng_t packet;
+	packet.spCmd = spCmd;
+	packet.target = target;
+
+	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CMD_AIRSPEED_CHNG, (const char *)&packet, 5, 209);
+#endif
 }
 
 #endif
+
 // MESSAGE CMD_AIRSPEED_CHNG UNPACKING
+
 
 /**
  * @brief Get field target from cmd_airspeed_chng message
@@ -92,7 +136,7 @@ static inline void mavlink_msg_cmd_airspeed_chng_send(mavlink_channel_t chan, ui
  */
 static inline uint8_t mavlink_msg_cmd_airspeed_chng_get_target(const mavlink_message_t* msg)
 {
-	return (uint8_t)(msg->payload)[0];
+	return _MAV_RETURN_uint8_t(msg,  4);
 }
 
 /**
@@ -102,12 +146,7 @@ static inline uint8_t mavlink_msg_cmd_airspeed_chng_get_target(const mavlink_mes
  */
 static inline float mavlink_msg_cmd_airspeed_chng_get_spCmd(const mavlink_message_t* msg)
 {
-	generic_32bit r;
-	r.b[3] = (msg->payload+sizeof(uint8_t))[0];
-	r.b[2] = (msg->payload+sizeof(uint8_t))[1];
-	r.b[1] = (msg->payload+sizeof(uint8_t))[2];
-	r.b[0] = (msg->payload+sizeof(uint8_t))[3];
-	return (float)r.f;
+	return _MAV_RETURN_float(msg,  0);
 }
 
 /**
@@ -118,6 +157,10 @@ static inline float mavlink_msg_cmd_airspeed_chng_get_spCmd(const mavlink_messag
  */
 static inline void mavlink_msg_cmd_airspeed_chng_decode(const mavlink_message_t* msg, mavlink_cmd_airspeed_chng_t* cmd_airspeed_chng)
 {
-	cmd_airspeed_chng->target = mavlink_msg_cmd_airspeed_chng_get_target(msg);
+#if MAVLINK_NEED_BYTE_SWAP
 	cmd_airspeed_chng->spCmd = mavlink_msg_cmd_airspeed_chng_get_spCmd(msg);
+	cmd_airspeed_chng->target = mavlink_msg_cmd_airspeed_chng_get_target(msg);
+#else
+	memcpy(cmd_airspeed_chng, _MAV_PAYLOAD(msg), 5);
+#endif
 }
