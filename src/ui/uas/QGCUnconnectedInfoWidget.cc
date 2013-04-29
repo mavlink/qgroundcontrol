d8e7efaaf75d2ed0bacf78820615131ae321a9a4
Lorenz Meier <lm@inf.ethz.ch>
2013-04-29 20:44:09 +0200

    More usability improvements

--- src/ui/uas/QGCUnconnectedInfoWidget.cc
+++ src/ui/uas/QGCUnconnectedInfoWidget.cc
@@ -13,6 +13,10 @@ QGCUnconnectedInfoWidget::QGCUnconnectedInfoWidget(QWidget *parent) :
 
     connect(ui->simulationButton, SIGNAL(clicked()), this, SLOT(simulate()));
     connect(ui->connectButton, SIGNAL(clicked()), this, SLOT(addLink()));
+
+    // Overwrite global style sheet
+    ui->connectButton->setStyleSheet("* { max-height: 1000; background-color: transparent; border-color: transparent; }");
+    //ui->connectButton->setStyleSheet("QToolButton {background-color: green; border-radius: 20px; } QButton {} QPushButton {}");
 }
 
 QGCUnconnectedInfoWidget::~QGCUnconnectedInfoWidget()
