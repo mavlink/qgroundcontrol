87a3f8f2984855980188e537dcc30408acb8c544
Michael Carpenter <malcom2073@gmail.com>
2013-06-05 08:41:04 -0400

    Additional functionaltiy of Actions view, including setwaypoint,change speed, and ARM/DISARM

--- src/ui/uas/UASActionsWidget.h
+++ src/ui/uas/UASActionsWidget.h
@@ -3,7 +3,8 @@
 
 #include <QWidget>
 #include "ui_UASActionsWidget.h"
-
+#include <UASManager.h>
+#include <UASInterface.h>
 class UASActionsWidget : public QWidget
 {
     Q_OBJECT
@@ -14,6 +15,16 @@ public:
     
 private:
     Ui::UASActionsWidget ui;
+    UASInterface *m_uas;
+private slots:
+    void armButtonClicked();
+    void armingChanged(bool state);
+    void currentWaypointChanged(quint16 wpid);
+    void updateWaypointList();
+    void activeUASSet(UASInterface *uas);
+    void goToWaypointClicked();
+    void changeAltitudeClicked();
+    void changeSpeedClicked();
 };
 
 #endif // UASACTIONSWIDGET_H
