9bdcda47a57b041fa7fd5b00e29ab3b82e93b61b
Michael Carpenter <malcom2073@gmail.com>
2013-06-05 12:54:33 -0400

    Fix for a couple of floating windows that shouldn't have been shown

--- src/ui/QGCTabbedInfoView.cpp
+++ src/ui/QGCTabbedInfoView.cpp
@@ -4,7 +4,7 @@ QGCTabbedInfoView::QGCTabbedInfoView(QWidget *parent) : QWidget(parent)
 {
     ui.setupUi(this);
     actionsWidget = new UASActionsWidget(this);
-    quickView = new UASQuickView(this);\
+    quickView = new UASQuickView(this);
     rawView = new UASRawStatusView(this);
     ui.tabWidget->addTab(quickView,"Quick");
     ui.tabWidget->addTab(actionsWidget,"Actions");
