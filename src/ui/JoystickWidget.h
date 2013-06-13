316620fdd3aa8298360f5a50a3ed7c9364326bb2
Bryant <bwmairs@ucsc.edu>
2013-06-12 18:02:17 -0700

    Refactored much of the joystick interface with the largest changes being the removal of dead/unused code including broken event-based SDL reading code. Additionally the mapping code should now work, though it hasn't been tested with controller switching or a real UAS. Finally the existing settings code has been commented-out as it is not compatible with the current state of this code and needs to be able to support different joysticks based on name.

--- src/ui/JoystickWidget.h
+++ src/ui/JoystickWidget.h
@@ -33,6 +33,7 @@ This file is part of the PIXHAWK project
 
 #include <QtGui/QDialog>
 #include <QLabel>
+#include <QMap>
 #include "JoystickInput.h"
 #include "MainWindow.h"
 #include "JoystickAxis.h"
@@ -52,26 +53,7 @@ public:
     virtual ~JoystickWidget();
 
 public slots:
-    /**
-     * @brief Receive raw joystick values
-     *
-     * @param roll forward / pitch / x axis, front: 32'767, center: 0, back: -32'768
-     * @param pitch left / roll / y axis, left: -32'768, middle: 0, right: 32'767
-     * @param yaw turn axis, left-turn: -32'768, centered: 0, right-turn: 32'767
-     * @param thrust Thrust, 0%: 0, 100%: 65535
-     * @param xHat hat vector in forward-backward direction, +1 forward, 0 center, -1 backward
-     * @param yHat hat vector in left-right direction, -1 left, 0 center, +1 right
-     */
-    void updateJoystick(double roll, double pitch, double yaw, double thrust, int xHat, int yHat);
-    /** @brief Back/forth movement */
-    void setX(float x);
-    /** @brief Left/right movement */
-    void setY(float y);
-    /** @brief Wrist rotation */
-    void setZ(float z);
-    /** @brief Throttle lever */
-    void setThrottle(float thrust);
-    /** @brief Hat switch position */
+    void updateAxisValue(int axis, float value);
     void setHat(float x, float y);
     /** @brief Trigger a UI change based on a button being pressed */
     void joystickButtonPressed(int key);
@@ -98,6 +80,8 @@ protected:
 protected slots:
     /** @brief Update the UI for a new joystick based on SDL ID. */
     void updateUIForJoystick(int id);
+    /** @brief Change the stored mapping for a given axis. */
+    void setMappingAxis(int axisID, int newMapping);
 
 private:
     Ui::JoystickWidget *m_ui;
