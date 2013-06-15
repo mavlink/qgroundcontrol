f820eefc8e286503ebb591cb82d83305a18beced
Bryant <bwmairs@ucsc.edu>
2013-06-15 00:31:54 -0700

    Joystick axes can now be specified as being exclusively-positive in the range [0:1.0] for the throttle. This should really be a vehicle-specific setting.

--- src/ui/JoystickAxis.cc
+++ src/ui/JoystickAxis.cc
@@ -9,9 +9,11 @@ JoystickAxis::JoystickAxis(int id, QWidget *parent) :
     ui(new Ui::JoystickAxis)
 {
     ui->setupUi(this);
+    ui->limitRangeCheckBox->hide(); // Hide the range checkbox by default. It's only activated by switching to the Throttle axis option.
     ui->label->setText(QString::number(id));
     connect(ui->comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(mappingComboBoxChanged(int)));
-    connect(ui->checkBox, SIGNAL(clicked(bool)), this, SLOT(inversionCheckBoxChanged(bool)));
+    connect(ui->invertedCheckBox, SIGNAL(clicked(bool)), this, SLOT(inversionCheckBoxChanged(bool)));
+    connect(ui->limitRangeCheckBox, SIGNAL(clicked(bool)), this, SLOT(limitRangeCheckBoxChanged(bool)));
 }
 
 JoystickAxis::~JoystickAxis()
@@ -26,10 +28,31 @@ void JoystickAxis::setValue(float value)
 
 void JoystickAxis::mappingComboBoxChanged(int newMapping)
 {
+    if (newMapping == JoystickInput::JOYSTICK_INPUT_MAPPING_THROTTLE)
+    {
+        ui->limitRangeCheckBox->show();
+    }
+    else
+    {
+        ui->limitRangeCheckBox->hide();
+    }
     emit mappingChanged(id, (JoystickInput::JOYSTICK_INPUT_MAPPING)newMapping);
 }
 
 void JoystickAxis::inversionCheckBoxChanged(bool inverted)
 {
     emit inversionChanged(id, inverted);
+}
+
+void JoystickAxis::limitRangeCheckBoxChanged(bool limited)
+{
+    if (limited)
+    {
+        ui->progressBar->setRange(0, 100);
+    }
+    else
+    {
+        ui->progressBar->setRange(-100, 100);
+    }
+    emit rangeLimitChanged(id, limited);
 }
