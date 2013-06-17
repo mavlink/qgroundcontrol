051c6f5d263fd0f63efc7604be6d56ad0b891cbb
Bryant <bwmairs@ucsc.edu>
2013-06-16 22:58:05 -0700

    The UASInterface now defines a systemCanReverse() function for indicating if the UAS is capable of commanding a reverse thrust.

--- src/uas/UAS.h
+++ src/uas/UAS.h
@@ -528,6 +528,40 @@ public:
         paramManager = manager;
     }
     int getSystemType();
+
+    /**
+     * @brief Returns true for systems that can reverse. If the system has no control over position, it returns false as
+     * @return If the specified vehicle type can
+     */
+    bool systemCanReverse() const
+    {
+        switch(type)
+        {
+        case MAV_TYPE_GENERIC:
+        case MAV_TYPE_FIXED_WING:
+        case MAV_TYPE_ROCKET:
+        case MAV_TYPE_FLAPPING_WING:
+
+        // System types that don't have movement
+        case MAV_TYPE_ANTENNA_TRACKER:
+        case MAV_TYPE_GCS:
+        case MAV_TYPE_FREE_BALLOON:
+        default:
+            return false;
+        case MAV_TYPE_QUADROTOR:
+        case MAV_TYPE_COAXIAL:
+        case MAV_TYPE_HELICOPTER:
+        case MAV_TYPE_AIRSHIP:
+        case MAV_TYPE_GROUND_ROVER:
+        case MAV_TYPE_SURFACE_BOAT:
+        case MAV_TYPE_SUBMARINE:
+        case MAV_TYPE_HEXAROTOR:
+        case MAV_TYPE_OCTOROTOR:
+        case MAV_TYPE_TRICOPTER:
+            return true;
+        }
+    }
+
     QString getSystemTypeName()
     {
         switch(type)
