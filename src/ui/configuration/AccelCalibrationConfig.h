4b8b2db4e888b27c24dff01f3d0d7d60a1950648
Michael Carpenter <malcom2073@gmail.com>
2013-06-24 15:33:03 -0400

    Working accelerometer calibation for APM3.0 and greater

--- src/ui/configuration/AccelCalibrationConfig.h
+++ src/ui/configuration/AccelCalibrationConfig.h
@@ -3,7 +3,8 @@
 
 #include <QWidget>
 #include "ui_AccelCalibrationConfig.h"
-
+#include "UASManager.h"
+#include "UASInterface.h"
 class AccelCalibrationConfig : public QWidget
 {
     Q_OBJECT
@@ -11,9 +12,14 @@ class AccelCalibrationConfig : public QWidget
 public:
     explicit AccelCalibrationConfig(QWidget *parent = 0);
     ~AccelCalibrationConfig();
-    
+private slots:
+    void activeUASSet(UASInterface *uas);
+    void calibrateButtonClicked();
+    void uasTextMessageReceived(int uasid, int componentid, int severity, QString text);
 private:
+    int accelAckCount;
     Ui::AccelCalibrationConfig ui;
+    UASInterface *m_uas;
 };
 
 #endif // ACCELCALIBRATIONCONFIG_H
