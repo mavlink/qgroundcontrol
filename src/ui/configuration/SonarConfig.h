27670bddffc86f94f3ac7ab77d633e76db542940
Michael Carpenter <malcom2073@gmail.com>
2013-06-27 14:28:24 -0400

    Implemented Sonar Config

--- src/ui/configuration/SonarConfig.h
+++ src/ui/configuration/SonarConfig.h
@@ -2,16 +2,20 @@
 #define SONARCONFIG_H
 
 #include <QWidget>
+#include "AP2ConfigWidget.h"
 #include "ui_SonarConfig.h"
 
-class SonarConfig : public QWidget
+class SonarConfig : public AP2ConfigWidget
 {
     Q_OBJECT
     
 public:
     explicit SonarConfig(QWidget *parent = 0);
     ~SonarConfig();
-    
+private slots:
+    void parameterChanged(int uas, int component, QString parameterName, QVariant value);
+    void checkBoxToggled(bool enabled);
+    void sonarTypeChanged(int index);
 private:
     Ui::SonarConfig ui;
 };
