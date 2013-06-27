4bf1e5b21f6b1625768e651e1b19e999940cf2d9
Michael Carpenter <malcom2073@gmail.com>
2013-06-27 14:56:53 -0400

    Implemented Airspeed configuration

--- src/ui/configuration/AirspeedConfig.h
+++ src/ui/configuration/AirspeedConfig.h
@@ -2,16 +2,20 @@
 #define AIRSPEEDCONFIG_H
 
 #include <QWidget>
+#include "AP2ConfigWidget.h"
 #include "ui_AirspeedConfig.h"
 
-class AirspeedConfig : public QWidget
+class AirspeedConfig : public AP2ConfigWidget
 {
     Q_OBJECT
     
 public:
     explicit AirspeedConfig(QWidget *parent = 0);
     ~AirspeedConfig();
-    
+private slots:
+    void parameterChanged(int uas, int component, QString parameterName, QVariant value);
+    void useCheckBoxClicked(bool checked);
+    void enableCheckBoxClicked(bool checked);
 private:
     Ui::AirspeedConfig ui;
 };
