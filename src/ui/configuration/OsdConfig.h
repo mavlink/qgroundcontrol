c8cd771e57c6c78158eb2ca2c5c10e39162f1c43
Michael Carpenter <malcom2073@gmail.com>
2013-06-28 08:35:41 -0400

    Implementation of OSD Configuration

--- src/ui/configuration/OsdConfig.h
+++ src/ui/configuration/OsdConfig.h
@@ -2,16 +2,18 @@
 #define OSDCONFIG_H
 
 #include <QWidget>
+#include "AP2ConfigWidget.h"
 #include "ui_OsdConfig.h"
 
-class OsdConfig : public QWidget
+class OsdConfig : public AP2ConfigWidget
 {
     Q_OBJECT
     
 public:
     explicit OsdConfig(QWidget *parent = 0);
     ~OsdConfig();
-    
+private slots:
+    void enableButtonClicked();
 private:
     Ui::OsdConfig ui;
 };
