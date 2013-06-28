fad0c692be016b6957d595d274079d1c5be9742c
Michael Carpenter <malcom2073@gmail.com>
2013-06-28 18:25:37 -0400

    Implementation of Camera Gimbal configuration

--- src/ui/configuration/CameraGimbalConfig.h
+++ src/ui/configuration/CameraGimbalConfig.h
@@ -2,16 +2,23 @@
 #define CAMERAGIMBALCONFIG_H
 
 #include <QWidget>
+#include "AP2ConfigWidget.h"
 #include "ui_CameraGimbalConfig.h"
 
-class CameraGimbalConfig : public QWidget
+class CameraGimbalConfig : public AP2ConfigWidget
 {
     Q_OBJECT
     
 public:
     explicit CameraGimbalConfig(QWidget *parent = 0);
     ~CameraGimbalConfig();
-    
+private slots:
+    void parameterChanged(int uas, int component, QString parameterName, QVariant value);
+    void updateTilt();
+    void updateRoll();
+    void updatePan();
+    void updateShutter();
+
 private:
     Ui::CameraGimbalConfig ui;
 };
