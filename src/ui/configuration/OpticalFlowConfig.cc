d440a4d62bf7b9b087bba0aa9b6da1a2249c5a04
Michael Carpenter <malcom2073@gmail.com>
2013-06-28 08:27:27 -0400

    Implementation of Optical Flow config

--- src/ui/configuration/OpticalFlowConfig.cc
+++ src/ui/configuration/OpticalFlowConfig.cc
@@ -1,11 +1,36 @@
 #include "OpticalFlowConfig.h"
+#include <QMessageBox>
 
-
-OpticalFlowConfig::OpticalFlowConfig(QWidget *parent) : QWidget(parent)
+OpticalFlowConfig::OpticalFlowConfig(QWidget *parent) : AP2ConfigWidget(parent)
 {
     ui.setupUi(this);
+    connect(ui.enableCheckBox,SIGNAL(clicked(bool)),this,SLOT(enableCheckBoxClicked(bool)));
 }
 
 OpticalFlowConfig::~OpticalFlowConfig()
 {
+}
+void OpticalFlowConfig::parameterChanged(int uas, int component, QString parameterName, QVariant value)
+{
+    if (parameterName == "FLOW_ENABLE")
+    {
+        if (value.toInt() == 0)
+        {
+            ui.enableCheckBox->setChecked(false);
+        }
+        else
+        {
+            ui.enableCheckBox->setChecked(true);
+        }
+    }
+}
+
+void OpticalFlowConfig::enableCheckBoxClicked(bool checked)
+{
+    if (!m_uas)
+    {
+        QMessageBox::information(0,tr("Error"),tr("Please connect to a MAV before attempting to set configuration"));
+        return;
+    }
+    m_uas->setParameter(0,"FLOW_ENABLE",checked ? 1 : 0);
 }
