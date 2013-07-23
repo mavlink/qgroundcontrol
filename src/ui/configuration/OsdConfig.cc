b53e86fd6b88733b7f3ff471410a6840222d269a
Michael Carpenter <malcom2073@gmail.com>
2013-07-23 10:42:16 -0400

    Change so OsdConfig only sends requests to start telemetry on a single serial port

--- src/ui/configuration/OsdConfig.cc
+++ src/ui/configuration/OsdConfig.cc
@@ -25,13 +25,4 @@ void OsdConfig::enableButtonClicked()
     m_uas->getParamManager()->setParameter(1,"SR0_RAW_CTRL",2);
     m_uas->getParamManager()->setParameter(1,"SR0_RAW_SENS",2);
     m_uas->getParamManager()->setParameter(1,"SR0_RC_CHAN",2);
-
-    m_uas->getParamManager()->setParameter(1,"SR3_EXT_STAT",2);
-    m_uas->getParamManager()->setParameter(1,"SR3_EXTRA1",10);
-    m_uas->getParamManager()->setParameter(1,"SR3_EXTRA2",10);
-    m_uas->getParamManager()->setParameter(1,"SR3_EXTRA3",2);
-    m_uas->getParamManager()->setParameter(1,"SR3_POSITION",3);
-    m_uas->getParamManager()->setParameter(1,"SR3_RAW_CTRL",2);
-    m_uas->getParamManager()->setParameter(1,"SR3_RAW_SENS",2);
-    m_uas->getParamManager()->setParameter(1,"SR3_RC_CHAN",2);
 }
