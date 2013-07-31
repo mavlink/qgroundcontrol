82c29b90e3196ffafcff8092d4963baef153209e
Michael Carpenter <malcom2073@gmail.com>
2013-07-30 21:08:09 -0400

    Reordered InfoView tabs, so Messages is at the end

--- src/ui/QGCTabbedInfoView.cpp
+++ src/ui/QGCTabbedInfoView.cpp
@@ -7,10 +7,10 @@ QGCTabbedInfoView::QGCTabbedInfoView(QWidget *parent) : QWidget(parent)
     actionsWidget = new UASActionsWidget(this);
     quickView = new UASQuickView(this);
     rawView = new UASRawStatusView(this);
-    ui.tabWidget->addTab(messageView,"Messages");
     ui.tabWidget->addTab(quickView,"Quick");
     ui.tabWidget->addTab(actionsWidget,"Actions");
     ui.tabWidget->addTab(rawView,"Status");
+    ui.tabWidget->addTab(messageView,"Messages");
 }
 void QGCTabbedInfoView::addSource(MAVLinkDecoder *decoder)
 {
