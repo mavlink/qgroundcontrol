365a330c83475ddd6075328a78499244f27bd269
Michael Carpenter <malcom2073@gmail.com>
2013-07-31 09:43:39 -0400

    Fix for Standard and Advanced param views not displaying or accepting edits properly

--- src/ui/configuration/StandardParamConfig.cc
+++ src/ui/configuration/StandardParamConfig.cc
@@ -10,7 +10,9 @@ StandardParamConfig::~StandardParamConfig()
 }
 void StandardParamConfig::addRange(QString title,QString description,QString param,double min,double max)
 {
-    ParamWidget *widget = new ParamWidget(ui.scrollAreaWidgetContents);
+    ParamWidget *widget = new ParamWidget(param,ui.scrollAreaWidgetContents);
+    connect(widget,SIGNAL(doubleValueChanged(QString,double)),this,SLOT(doubleValueChanged(QString,double)));
+    connect(widget,SIGNAL(intValueChanged(QString,int)),this,SLOT(intValueChanged(QString,int)));
     paramToWidgetMap[param] = widget;
     widget->setupDouble(title + "(" + param + ")",description,0,min,max);
     ui.verticalLayout->addWidget(widget);
@@ -19,7 +21,9 @@ void StandardParamConfig::addRange(QString title,QString description,QString par
 
 void StandardParamConfig::addCombo(QString title,QString description,QString param,QList<QPair<int,QString> > valuelist)
 {
-    ParamWidget *widget = new ParamWidget(ui.scrollAreaWidgetContents);
+    ParamWidget *widget = new ParamWidget(param,ui.scrollAreaWidgetContents);
+    connect(widget,SIGNAL(doubleValueChanged(QString,double)),this,SLOT(doubleValueChanged(QString,double)));
+    connect(widget,SIGNAL(intValueChanged(QString,int)),this,SLOT(intValueChanged(QString,int)));
     paramToWidgetMap[param] = widget;
     widget->setupCombo(title + "(" + param + ")",description,valuelist);
     ui.verticalLayout->addWidget(widget);
@@ -29,6 +33,31 @@ void StandardParamConfig::parameterChanged(int uas, int component, QString param
 {
     if (paramToWidgetMap.contains(parameterName))
     {
-        paramToWidgetMap[parameterName]->setValue(value.toDouble());
+        if (value.type() == QVariant::Double)
+        {
+            paramToWidgetMap[parameterName]->setValue(value.toDouble());
+        }
+        else
+        {
+            paramToWidgetMap[parameterName]->setValue(value.toInt());
+        }
     }
 }
+void StandardParamConfig::doubleValueChanged(QString param,double value)
+{
+    if (!m_uas)
+    {
+        this->showNullMAVErrorMessageBox();
+    }
+    m_uas->getParamManager()->setParameter(1,param,value);
+}
+
+void StandardParamConfig::intValueChanged(QString param,int value)
+{
+    if (!m_uas)
+    {
+        this->showNullMAVErrorMessageBox();
+    }
+    m_uas->getParamManager()->setParameter(1,param,value);
+}
+
