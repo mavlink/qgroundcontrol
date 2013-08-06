452781479586fb9bf1a5022e1f60256f7d28677a
tstellanova <tstellanova+github@gmail.com>
2013-08-05 21:29:48 -0700

    utility methods for data model

--- src/uas/UASParameterDataModel.cc
+++ src/uas/UASParameterDataModel.cc
@@ -7,20 +7,44 @@ UASParameterDataModel::UASParameterDataModel(QObject *parent) :
 {
 
 
+}
 
+void UASParameterDataModel::addPendingIfParameterChanged(int componentId, QString& key,  QVariant &value)
+{
+    addComponent(componentId);
+    QMap<QString, QVariant> *existParams = getOnbardParametersForComponent(componentId);
+    QMap<QString, QVariant> *pendParams = getPendingParametersForComponent(componentId);
 
-
+    QVariant existValue = existParams->value(key);
+    QVariant pendValue = pendParams->value(key);
+    if (!(existValue == pendValue)) {
+        setPendingParameter(componentId,key,value);
+    }
 }
 
 
 void UASParameterDataModel::setPendingParameter(int componentId, QString& key,  QVariant &value)
 {
-    QMap<QString, QVariant> *compPendingParams = pendingParameters.value(componentId);
-    //TODO insert blank map if necessary
-    if (NULL == compPendingParams) {
-        pendingParameters.insert(componentId,new QMap<QString, QVariant>());
-        compPendingParams = pendingParameters.value(componentId);
-    }
+    //ensure we have a placeholder map for this component
+    addComponent(componentId);
+    QMap<QString, QVariant> *params = getPendingParametersForComponent(componentId);
+    params->insert(key,value);
+}
 
-    compPendingParams->insert(key,value);
+void UASParameterDataModel::setOnboardParameter(int componentId, QString& key,  QVariant &value)
+{
+    //ensure we have a placeholder map for this component
+    addComponent(componentId);
+    QMap<QString, QVariant> *params = getOnbardParametersForComponent(componentId);
+    params->insert(key,value);
+}
+
+void UASParameterDataModel::addComponent(int componentId)
+{
+    if (!onboardParameters.contains(componentId)) {
+        onboardParameters.insert(componentId, new QMap<QString, QVariant>());
+    }
+    if (!pendingParameters.contains(componentId)) {
+        pendingParameters.insert(componentId, new QMap<QString, QVariant>());
+    }
 }
