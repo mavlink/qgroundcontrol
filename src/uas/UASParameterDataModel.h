ca20853343109bf52424d3db8a46458fb5c95942
tstellanova <tstellanova+github@gmail.com>
2013-08-05 21:29:48 -0700

    utility methods for data model

--- src/uas/UASParameterDataModel.h
+++ src/uas/UASParameterDataModel.h
@@ -12,8 +12,23 @@ public:
     explicit UASParameterDataModel(QObject *parent = 0);
     
 
+    virtual void addComponent(int componentId);
+
     /** @brief Write a new pending parameter value that may be eventually sent to the UAS */
     virtual void setPendingParameter(int componentId,  QString& paramKey,  QVariant& paramValue);
+    virtual void setOnboardParameter(int componentId, QString& key,  QVariant &value);
+
+    /** @brief add this parameter to pending list iff it has changed from onboard value */
+    virtual void addPendingIfParameterChanged(int componentId, QString& key,  QVariant &value);
+
+
+    QMap<QString , QVariant>* getPendingParametersForComponent(int componentId) {
+        return pendingParameters.value(componentId);
+    }
+
+    QMap<QString , QVariant>* getOnbardParametersForComponent(int componentId) {
+        return onboardParameters.value(componentId);
+    }
 
     QMap<int, QMap<QString, QVariant>* >  getPendingParameters() {
        return pendingParameters;
