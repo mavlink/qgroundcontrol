f87953f077501597c7efbfa14e82369f8e4fb431
Anton Babushkin <anton.babushkin@me.com>
2013-08-23 23:05:06 +0200

    Indicate AUTO submodes.

---
+++ src/comm/px4_custom_mode.h
@@ -0,0 +1,30 @@
+#ifndef PX4_CUSTOM_MODE_H
+#define PX4_CUSTOM_MODE_H
+
+enum PX4_CUSTOM_MAIN_MODE {
+    PX4_CUSTOM_MAIN_MODE_MANUAL = 1,
+    PX4_CUSTOM_MAIN_MODE_SEATBELT,
+    PX4_CUSTOM_MAIN_MODE_EASY,
+    PX4_CUSTOM_MAIN_MODE_AUTO,
+};
+
+enum PX4_CUSTOM_SUB_MODE_AUTO {
+    PX4_CUSTOM_SUB_MODE_AUTO_READY = 1,
+    PX4_CUSTOM_SUB_MODE_AUTO_TAKEOFF,
+    PX4_CUSTOM_SUB_MODE_AUTO_LOITER,
+    PX4_CUSTOM_SUB_MODE_AUTO_MISSION,
+    PX4_CUSTOM_SUB_MODE_AUTO_RTL,
+    PX4_CUSTOM_SUB_MODE_AUTO_LAND,
+};
+
+union px4_custom_mode {
+    struct {
+        uint16_t reserved;
+        uint8_t main_mode;
+        uint8_t sub_mode;
+    };
+    uint32_t data;
+    float data_float;
+};
+
+#endif // PX4_CUSTOM_MODE_H
