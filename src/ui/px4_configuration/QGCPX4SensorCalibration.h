39c46410bc1e7628e8a687302787f67e30037654
Lorenz Meier <lm@inf.ethz.ch>
2013-08-29 16:30:57 +0200

    Improved calibration screen a lot, gives now much better feedback what to do

--- src/ui/px4_configuration/QGCPX4SensorCalibration.h
+++ src/ui/px4_configuration/QGCPX4SensorCalibration.h
@@ -42,12 +42,28 @@ public slots:
      */
     virtual void contextMenuEvent(QContextMenuEvent* event);
 
+    void setAutopilotOrientation(int index);
+    void setGpsOrientation(int index);
+    void parameterChanged(int uas, int component, QString parameterName, QVariant value);
+
+protected slots:
+
     void setInstructionImage(const QString &path);
+
+    void setAutopilotImage(const QString &path);
+
+    void setGpsImage(const int index);
+
+    void setAutopilotImage(const int index);
+
+    void setGpsImage(const QString &path);
 
 protected:
     UASInterface* activeUAS;
     QAction* clearAction;
     QPixmap instructionIcon;
+    QPixmap autopilotIcon;
+    QPixmap gpsIcon;
     bool accelStarted;
     bool accelDone[6];
     bool gyroStarted;
@@ -55,6 +71,10 @@ protected:
     QStringList accelAxes;
 
     virtual void resizeEvent(QResizeEvent* event);
+
+    void setMagCalibrated(bool calibrated);
+    void setGyroCalibrated(bool calibrated);
+    void setAccelCalibrated(bool calibrated);
     
 private:
     Ui::QGCPX4SensorCalibration *ui;
