6e3dde3004f23df98c6207773a86c28b6e7c92f6
Lorenz Meier <lm@inf.ethz.ch>
2013-12-17 10:16:07 +0100

    Improved log download UI widget, request button operational

--- src/ui/logging/QGCLogDownloader.h
+++ src/ui/logging/QGCLogDownloader.h
@@ -2,6 +2,7 @@
 #define QGCLOGDOWNLOADER_H
 
 #include <QWidget>
+#include "uas/UASInterface.h"
 
 namespace Ui {
 class QGCLogDownloader;
@@ -14,9 +15,17 @@ class QGCLogDownloader : public QWidget
 public:
     explicit QGCLogDownloader(QWidget *parent = 0);
     ~QGCLogDownloader();
+
+public slots:
+    void setActiveUAS(UASInterface* uas);
+    void forgetUAS(UASInterface* uas);
+
+protected:
+    UASInterface* mav;
     
 private:
     Ui::QGCLogDownloader *ui;
+
 };
 
 #endif // QGCLOGDOWNLOADER_H
