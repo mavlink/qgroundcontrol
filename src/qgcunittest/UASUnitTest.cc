843e266d5e200f71735716465f186b07bf3fa45d
Don Gagne <don@thegagnes.com>
2014-05-07 10:20:34 -0700

    Allow voltage to be within range

--- src/qgcunittest/UASUnitTest.cc
+++ src/qgcunittest/UASUnitTest.cc
@@ -73,14 +73,20 @@ void UASUnitTest::getCommunicationStatus_test()
 void UASUnitTest::filterVoltage_test()
 {
     float verificar=uas->filterVoltage(0.4f);
-    QCOMPARE(verificar, 7.36f);
+
+    // We allow the voltage returned to be within a small delta
+    const float allowedDelta = 0.05f;
+    const float desiredVoltage = 7.36f;
+    QVERIFY(verificar > (desiredVoltage - allowedDelta) && verificar < (desiredVoltage + allowedDelta));
 }
+
 void UASUnitTest:: getAutopilotType_test()
 {
     int type = uas->getAutopilotType();
     // Verify that upon construction the autopilot is set to -1
     QCOMPARE(type, -1);
 }
+
 void UASUnitTest::setAutopilotType_test()
 {
     uas->setAutopilotType(2);
