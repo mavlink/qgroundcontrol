d79f25088a9956cb0f6ae16b7c4a895fad46b481
Lorenz Meier <lm@inf.ethz.ch>
2014-05-30 10:47:56 +0200

    Add missing OS-guard

--- src/main.cc
+++ src/main.cc
@@ -84,9 +84,11 @@ int WindowsCrtReportHook(int reportType, char* message, int* returnValue)
 int main(int argc, char *argv[])
 {
 
+#ifdef Q_OS_MAC
     // Prevent Apple's app nap from screwing us over
     // tip: the domain can be cross-checked on the command line with <defaults domains>
     QProcess::execute("defaults write org.qgroundcontrol.qgroundcontrol NSAppSleepDisabled -bool YES");
+#endif
 
     // install the message handler
 #ifdef Q_OS_WIN
