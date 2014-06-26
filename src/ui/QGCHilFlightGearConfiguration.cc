92af88b41d25e39443a524ebf515d6aaa3ace014
Don Gagne <don@thegagnes.com>
2014-06-26 10:17:20 -0700

    Fix linux release build

--- src/ui/QGCHilFlightGearConfiguration.cc
+++ src/ui/QGCHilFlightGearConfiguration.cc
@@ -77,6 +77,7 @@ QGCHilFlightGearConfiguration::QGCHilFlightGearConfiguration(UAS* mav, QWidget *
     Q_ASSERT(success);
     success = connect(_ui.optionsPlainTextEdit, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(_showContextMenu(const QPoint &)));
     Q_ASSERT(success);
+    Q_UNUSED(success);  // Silence release build unused variable warning
 }
 
 QGCHilFlightGearConfiguration::~QGCHilFlightGearConfiguration()
