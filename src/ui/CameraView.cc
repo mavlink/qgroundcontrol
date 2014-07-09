e09943d273f2c67099d0903e434fb9674a46fdcf
Bryant Mairs <bwmairs@gmail.com>
2014-07-08 18:42:02 -0700

    Qt5 doesn't declare as many GL functions as it used to, need to include system library headers now.

--- src/ui/CameraView.cc
+++ src/ui/CameraView.cc
@@ -30,6 +30,7 @@ This file is part of the QGROUNDCONTROL project
 
 #include "CameraView.h"
 #include <QDebug>
+#include <gl/gl.h>
 
 CameraView::CameraView(int width, int height, int depth, int channels, QWidget* parent) : QGLWidget(parent)
 {
