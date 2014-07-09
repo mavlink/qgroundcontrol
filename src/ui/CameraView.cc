42a4ef8cb4c83e4c3401874d10d82c30403eccd3
Bryant Mairs <bwmairs@gmail.com>
2014-07-08 22:55:01 -0700

    To include OpenGL primitives, we include the QtOpenGL header instead, which has all the platform-specific includes taken care of for us.

--- src/ui/CameraView.cc
+++ src/ui/CameraView.cc
@@ -30,7 +30,7 @@ This file is part of the QGROUNDCONTROL project
 
 #include "CameraView.h"
 #include <QDebug>
-#include <gl/gl.h>
+#include <QtOpenGL>
 
 CameraView::CameraView(int width, int height, int depth, int channels, QWidget* parent) : QGLWidget(parent)
 {
