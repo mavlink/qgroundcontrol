c1cdaa04478dd604f02183e0f26f214a3fd2574c
Don Gagne <don@thegagnes.com>
2014-12-01 10:17:59 -0800

    Remove signal overrides

--- src/qgcunittest/MockUASManager.h
+++ src/qgcunittest/MockUASManager.h
@@ -39,11 +39,11 @@ class MockUASManager : public UASManagerInterface
     Q_OBJECT
     
 signals:
-    // The following signals from UASManager interface are supported
-    void activeUASSet(UASInterface* UAS);
-    void activeUASSet(int systemId);
-    void activeUASStatusChanged(UASInterface* UAS, bool active);
-    void activeUASStatusChanged(int systemId, bool active);
+    // The following signals from UASManager interface are supported:
+    //      void activeUASSet(UASInterface* UAS);
+    //      void activeUASSet(int systemId);
+    //      void activeUASStatusChanged(UASInterface* UAS, bool active);
+    //      void activeUASStatusChanged(int systemId, bool active);
     
 public:
     // Implemented UASManagerInterface overrides
