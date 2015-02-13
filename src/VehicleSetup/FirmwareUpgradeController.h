d7ac2490532ea798ae50029fc863bdcff675e2f0
Don Gagne <don@thegagnes.com>
2015-02-12 16:09:24 -0800

    Convert Firmware Upgrade to QML
    - Much simpler UI
    - MVC style qml programming model

--- src/VehicleSetup/FirmwareUpgradeController.h
+++ src/VehicleSetup/FirmwareUpgradeController.h
@@ -2,7 +2,7 @@
  
  QGroundControl Open Source Ground Control Station
  
- (c) 2009, 2014 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ (c) 2009, 2015 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
  
  This file is part of the QGROUNDCONTROL project
  
@@ -22,43 +22,65 @@
  ======================================================================*/
 
 /// @file
-///     @brief PX4 Firmware Upgrade UI
 ///     @author Don Gagne <don@thegagnes.com>
 
-#ifndef PX4FirmwareUpgrade_H
-#define PX4FirmwareUpgrade_H
+#ifndef FirmwareUpgradeController_H
+#define FirmwareUpgradeController_H
 
-#include <QWidget>
+#include "PX4FirmwareUpgradeThread.h"
+
+#include <QObject>
 #include <QUrl>
 #include <QTimer>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+#include <QPixmap>
+#include <QQuickItem>
 
 #include "qextserialport.h"
 
 #include <stdint.h>
 
-#include "PX4FirmwareUpgradeThread.h"
-
-#include "ui_PX4FirmwareUpgrade.h"
-
-namespace Ui {
-    class PX4RCCalibration;
-}
-
-class PX4FirmwareUpgrade : public QWidget
+// Firmware Upgrade MVC Controller for FirmwareUpgrade.qml.
+class FirmwareUpgradeController : public QObject
 {
     Q_OBJECT
-
+    
 public:
-    explicit PX4FirmwareUpgrade(QWidget *parent = 0);
-    ~PX4FirmwareUpgrade();
+    FirmwareUpgradeController(void);
+    
+    /// Supported firmware types
+    typedef enum {
+        StableFirmware,
+        BetaFirmware,
+        DeveloperFirmware,
+        CustomFirmware
+    } FirmwareType_t;
 
+    Q_ENUMS(FirmwareType_t)
+    
+    /// Firmare type to load
+    Q_PROPERTY(FirmwareType_t firmwareType READ firmwareType WRITE setFirmwareType)
+    
+    /// Upgrade push button in UI
+    Q_PROPERTY(QQuickItem* upgradeButton READ upgradeButton WRITE setUpgradeButton)
+
+    /// TextArea for log output
+    Q_PROPERTY(QQuickItem* statusLog READ statusLog WRITE setStatusLog)
+    
+    /// Begins the firware upgrade process
+    Q_INVOKABLE void doFirmwareUpgrade(void);
+
+    FirmwareType_t firmwareType(void) { return _firmwareType; }
+    void setFirmwareType(FirmwareType_t firmwareType) { _firmwareType = firmwareType; }
+    
+    QQuickItem* upgradeButton(void) { return _upgradeButton; }
+    void setUpgradeButton(QQuickItem* upgradeButton) { _upgradeButton = upgradeButton; }
+    
+    QQuickItem* statusLog(void) { return _statusLog; }
+    void setStatusLog(QQuickItem* statusLog) { _statusLog = statusLog; }
+    
 private slots:
-    void _tryAgainButton(void);
-    void _cancelButton(void);
-    void _nextButton(void);
-    void _firmwareSelected(int index);
     void _downloadProgress(qint64 curr, qint64 total);
     void _downloadFinished(void);
     void _downloadError(QNetworkReply::NetworkError code);
@@ -73,56 +95,18 @@ private slots:
     void _eraseProgressTick(void);
 
 private:
-    /// @brief The various states that the upgrade process progresses through.
-    enum upgradeStates {
-        upgradeStateBegin,
-        upgradeStateBoardSearch,
-        upgradeStateBoardNotFound,
-        upgradeStateBootloaderSearch,
-        upgradeStateBootloaderNotFound,
-        upgradeStateBootloaderError,
-        upgradeStateFirmwareSelect,
-        upgradeStateFirmwareDownloading,
-        upgradeStateDownloadFailed,
-        upgradeStateErasing,
-        upgradeStateEraseError,
-        upgradeStateFlashing,
-        upgradeStateFlashError,
-        upgradeStateVerifying,
-        upgradeStateVerifyError,
-        upgradeStateBoardUpgraded,
-        upgradeStateMax
-    };
-    
-    void _setupState(enum upgradeStates state);
-    void _updateIndicatorUI(void);
-    
     void _findBoard(void);
     void _findBootloader(void);
     void _cancel(void);
-    void _cancelFind(void);
     void _getFirmwareFile(void);
     
     void _setBoardIcon(int boardID);
-    void _setFirmwareCombo(int boardID);
     
     void _downloadFirmware(void);
-    void _cancelDownload(void);
     
     void _erase(void);
-    
-    typedef void (PX4FirmwareUpgrade::*stateFunc)(void);
-    struct stateMachineEntry {
-        enum upgradeStates  state;      ///< State machine state, used to verify correctness of entry
-        stateFunc           next;       ///< Method to call when Next is clicked, NULL for Next not available
-        stateFunc           cancel;     ///< Method to call when Cancel is clicked, NULL for Cancel not available
-        stateFunc           tryAgain;   ///< Method to call when Try Again is clicked, NULL for Try Again not available
-        const char*         msg;        ///< Text message to display to user for this state
-    };
-    
-    const struct stateMachineEntry* _getStateMachineEntry(enum upgradeStates state);
-    
-    enum upgradeStates _upgradeState;       ///< Current state of the upgrade state machines
+        
+    void _appendStatusLog(const QString& text);
     
     QString _portName;
     QString _portDescription;
@@ -154,7 +138,11 @@ private:
     static const int    _findBoardTimeoutMsec = 30000;      ///< Amount of time for user to plug in USB
     static const int    _findBootloaderTimeoutMsec = 5000;  ///< Amount time to look for bootloader
     
-    Ui::PX4FirmwareUpgrade* _ui;
+    FirmwareType_t  _firmwareType;      ///< Firmware type to load
+    QQuickItem*     _upgradeButton;     ///< Upgrade button in ui
+    QQuickItem*     _statusLog;         ///< Status log TextArea Qml control
+    
+    bool _searchingForBoard;    ///< true: searching for board, false: search for bootloader
 };
 
-#endif // PX4FirmwareUpgrade_H
+#endif
