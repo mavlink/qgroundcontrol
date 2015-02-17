bd22d5cec82be6f91a989c586b7d7440be205c33
Don Gagne <don@thegagnes.com>
2015-02-17 13:45:19 -0800

    Updates taking into account feedback

--- CodingStyle.h
+++ CodingStyle.h
@@ -39,16 +39,22 @@
 #include <QXmlStreamReader>
 #include <QLoggingCategory>
 
+#include <limits.h>
+
 #include "Fact.h"
 #include "UASInterface.h"
 
-// Note how the Qt headers and the QGroundControl headers above are kept seperate
+// Note how the Qt headers, Syste, headers and the QGroundControl headers above are kept in seperate groups
 
 // If you are going to use a logging category for a class it should have the same name as the class
 // with a suffix of Log.
 Q_DECLARE_LOGGING_CATEGORY(CodingStyleLog)
 
-/// Here is the class documentation. Class names are PascalCase.
+/// Here is the class documentation. Class names are PascalCase. If you override any of the Qt base classes to provide
+/// generic base implementations for widespread use prefix the class name with QGC. For example:
+///     QGCMessageBox - is a QGC special vesion of Qt MessageBox
+///     QGCPalette - is a QGC special version of Qt Palette
+/// For normal single use classes do no prefix them name with QGC.
 
 class CodingStyle : public QObject
 {
@@ -84,10 +90,14 @@ private:
     // non-obvious private methods goes in the code file, not the header.
     void _privateMethod(void);
     
+    void _methodWithManyArguments(QWidget* parent, const QString& caption, const QString& dir, Options options1, Options options2, Options options3);
+
     /// Document non-obvious variables in the header file. Long descriptions go here.
     int _privateVariable1;
     
     int _privateVariable2;    ///< Short descriptions go here
+    
+    static const int _privateStaticVariable;
 };
 
 #endif
