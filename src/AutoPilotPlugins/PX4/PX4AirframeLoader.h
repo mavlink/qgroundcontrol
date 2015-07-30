0aea27a814ea6185ba28a306ea4862fbb823e3ad
Lorenz Meier <lm@inf.ethz.ch>
2015-07-30 15:29:42 +0200

    Add airframe meta file loader

---
+++ src/AutoPilotPlugins/PX4/PX4AirframeLoader.h
@@ -0,0 +1,13 @@
+#ifndef PX4AIRFRAMELOADER_H
+#define PX4AIRFRAMELOADER_H
+
+#include <QObject>
+
+class PX4AirframeLoader
+{
+public:
+    PX4AirframeLoader();
+    ~PX4AirframeLoader();
+};
+
+#endif // PX4AIRFRAMELOADER_H
