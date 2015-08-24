29091cf8b35e86d782beca33ad6c0f9a540ad798
Don Gagne <don@thegagnes.com>
2015-08-23 20:53:03 -0700

    --fake-mobile command line support

--- src/QGCApplication.h
+++ src/QGCApplication.h
@@ -109,6 +109,9 @@ public:
     
     /// Show a non-modal message to the user
     void showToolBarMessage(const QString& message);
+
+	/// @return true: Fake ui into showing mobile interface
+	bool fakeMobile(void) { return _fakeMobile; }
     
 public slots:
     /// You can connect to this slot to show an information message box from a different thread.
@@ -177,6 +180,8 @@ private:
     QTimer              _missingParamsDelayedDisplayTimer;                ///< Timer use to delay missing fact display
     QStringList         _missingParams;                                  ///< List of missing facts to be displayed
     MavManager*         _pMavManager;
+
+	bool				_fakeMobile;	///< true: Fake ui into displaying mobile interface
 
     /// Unit Test have access to creating and destroying singletons
     friend class UnitTest;
