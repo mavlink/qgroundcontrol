13cc2cfbf73788fedc3639055de51e9fb6b64277
Don Gagne <don@thegagnes.com>
2015-08-26 11:23:41 -0700

    Pass in autopilot type

--- src/uas/QGCMAVLinkUASFactory.cc
+++ src/uas/QGCMAVLinkUASFactory.cc
@@ -6,35 +6,19 @@ QGCMAVLinkUASFactory::QGCMAVLinkUASFactory(QObject *parent) :
 {
 }
 
-UASInterface* QGCMAVLinkUASFactory::createUAS(MAVLinkProtocol* mavlink, LinkInterface* link, int sysid, mavlink_heartbeat_t* heartbeat, QObject* parent)
+UASInterface* QGCMAVLinkUASFactory::createUAS(MAVLinkProtocol* mavlink, LinkInterface* link, int sysid, MAV_AUTOPILOT autopilotType)
 {
-    QPointer<QObject> p;
-
-    if (parent != NULL)
-    {
-        p = parent;
-    }
-    else
-    {
-        p = mavlink;
-    }
-
     UASInterface* uasInterface;
 
-    UAS* uasObject = new UAS(mavlink, sysid);
+    UAS* uasObject = new UAS(mavlink, sysid, autopilotType);
     Q_CHECK_PTR(uasObject);
     uasInterface = uasObject;
-    
-    uasObject->setSystemType((int)heartbeat->type);
 
     // Connect this robot to the UAS object
     // It is IMPORTANT here to use the right object type,
     // else the slot of the parent object is called (and thus the special
     // packets never reach their goal)
     connect(mavlink, &MAVLinkProtocol::messageReceived, uasObject, &UAS::receiveMessage);
-
-    // Set the autopilot type
-    uasInterface->setAutopilotType((int)heartbeat->autopilot);
 
     // Make UAS aware that this link can be used to communicate with the actual robot
     uasInterface->addLink(link);
