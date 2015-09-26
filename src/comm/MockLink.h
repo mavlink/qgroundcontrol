6ff4ff7386419405fd8874cafd4cc23ee4bb0b19
Don Gagne <don@thegagnes.com>
2015-09-25 19:33:16 -0700

    Add COMMAND_LONG arm/disarm support

--- src/comm/MockLink.h
+++ src/comm/MockLink.h
@@ -124,6 +124,7 @@ private:
     void _handleMissionRequest(const mavlink_message_t& msg);
     void _handleMissionItem(const mavlink_message_t& msg);
     void _handleFTP(const mavlink_message_t& msg);
+    void _handleCommandLong(const mavlink_message_t& msg);
     float _floatUnionForParam(int componentId, const QString& paramName);
     void _setParamFloatUnionIntoMap(int componentId, const QString& paramName, float paramFloat);
 
