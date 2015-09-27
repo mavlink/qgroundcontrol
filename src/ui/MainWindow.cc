b0bb0c7d8d808a980e46fe430258879b7a941f83
Don Gagne <don@thegagnes.com>
2015-09-26 21:51:46 -0700

    No widgets in mobile builds

--- src/ui/MainWindow.cc
+++ src/ui/MainWindow.cc
@@ -823,6 +823,8 @@ void MainWindow::_storeCurrentViewState(void)
 {
     // HIL dock widgets are dynamic and are not part of the saved state
     _hideAllHilDockWidgets();
+    
+#ifndef __mobile__
     // Save list of visible widgets
     bool firstWidget = true;
     QString widgetNames = "";
@@ -836,6 +838,7 @@ void MainWindow::_storeCurrentViewState(void)
         }
     }
     settings.setValue(_getWindowStateKey() + "WIDGETS", widgetNames);
+#endif
     settings.setValue(_getWindowStateKey(), saveState());
     settings.setValue(_getWindowGeometryKey(), saveGeometry());
 }
@@ -906,6 +909,7 @@ void MainWindow::_loadCurrentViewState(void)
     // Hide all widgets from previous view
     _hideAllDockWidgets();
 
+#ifndef __mobile__
     // Restore the widgets for the new view
     QString widgetNames = settings.value(_getWindowStateKey() + "WIDGETS", defaultWidgets).toString();
     qDebug() << widgetNames;
@@ -916,6 +920,7 @@ void MainWindow::_loadCurrentViewState(void)
             _showDockWidget(widgetName, true);
         }
     }
+#endif
 
     if (settings.contains(_getWindowStateKey())) {
         restoreState(settings.value(_getWindowStateKey()).toByteArray());
