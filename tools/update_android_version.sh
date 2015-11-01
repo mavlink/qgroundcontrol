9ebd2bafa3128866d5d3d5bf4d29601f34fc8e3f
Daniel Agar <daniel@agar.ca>
2015-11-01 00:56:14 -0400

    fix android auto versionName update

--- tools/update_android_version.sh
+++ tools/update_android_version.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
+MANIFEST_FILE=android/AndroidManifest.xml
+
 VERSIONCODE=`git rev-list master --first-parent --count`
 VERSIONNAME=`git describe --always --tags | sed -e 's/^v//'`
 
 echo "VersionCode: ${VERSIONCODE}"
 echo "VersionName: ${VERSIONNAME}"
 
-sed -i -e "s/android:versionCode=\"[0-9][0-9]*\"/android:versionCode=\"${VERSIONCODE}\"/" android/AndroidManifest.xml
-set -i -e "s/android:versionName=\".*\"/android:versionName=\"${VERSIONNAME}\"/" android/AndroidManifest.xml
-
+sed -i -e "s/android:versionCode=\"[0-9][0-9]*\"/android:versionCode=\"${VERSIONCODE}\"/" $MANIFEST_FILE
+sed -i -e 's/versionName *= *"[^"]*"/versionName="'$VERSIONNAME'"/' $MANIFEST_FILE
