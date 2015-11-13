41866c2ff23f859ceddcf4ccf3280e7faaadfc5e
Don Gagne <don@thegagnes.com>
2015-11-13 10:48:48 -0800

    Stop logging on last vehicle gone

--- src/comm/MAVLinkProtocol.cc
+++ src/comm/MAVLinkProtocol.cc
@@ -108,6 +108,10 @@ void MAVLinkProtocol::setToolbox(QGCToolbox *toolbox)
    connect(this, &MAVLinkProtocol::protocolStatusMessage, _app, &QGCApplication::criticalMessageBoxOnMainThread);
    connect(this, &MAVLinkProtocol::saveTempFlightDataLog, _app, &QGCApplication::saveTempFlightDataLogOnMainThread);
 
+#ifndef __mobile__
+   connect(_multiVehicleManager->vehicles(), &QmlObjectListModel::countChanged, this, &MAVLinkProtocol::_vehicleCountChanged);
+#endif
+
    emit versionCheckChanged(m_enable_version_check);
 }
 
@@ -217,13 +221,6 @@ void MAVLinkProtocol::_linkStatusChanged(LinkInterface* link, bool connected)
         }
         Q_UNUSED(found);
         Q_ASSERT(found);
-        
-#ifndef __mobile__
-        if (_connectedLinks.count() == 0) {
-            // Last link is gone, close out logging
-            _stopLogging();
-        }
-#endif
     }
 }
 
@@ -727,6 +724,14 @@ void MAVLinkProtocol::deleteTempLogFiles(void)
     
     foreach(const QFileInfo fileInfo, fileInfoList) {
         QFile::remove(fileInfo.filePath());
+    }
+}
+
+void MAVLinkProtocol::_vehicleCountChanged(int count)
+{
+    if (count == 0) {
+        // Last vehicle is gone, close out logging
+        _stopLogging();
     }
 }
 #endif
