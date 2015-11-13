a78ec0384ca7cbbefdcf0fe313d7c9b5dcccd958
Tomaz Canabrava <tomaz.canabrava@intel.com>
2015-11-13 14:11:20 -0200

    Update License on file, also adds test for sha
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>

--- src/AutoPilotPlugins/APM/apmremoteparamscontroller.cpp
+++ src/AutoPilotPlugins/APM/apmremoteparamscontroller.cpp
@@ -1,27 +1,28 @@
+/*=====================================================================
+
+ QGroundControl Open Source Ground Control Station
+
+ (c) 2009 - 2014 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+
+ This file is part of the QGROUNDCONTROL project
+
+ QGROUNDCONTROL is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QGROUNDCONTROL is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+
+ ======================================================================*/
+
 #include "apmremoteparamscontroller.h"
 
-/*===================================================================
-APM_PLANNER Open Source Ground Control Station
-
-(c) 2014 APM_PLANNER PROJECT <http://www.diydrones.com>
-(c) author: Bill Bonney <billbonney@communistech.com>
-
-This file is part of the APM_PLANNER project
-
-    APM_PLANNER is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    APM_PLANNER is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with APM_PLANNER. If not, see <http://www.gnu.org/licenses/>.
-
-======================================================================*/
 #include <QMessageBox>
 #include <QDesktopServices>
 #include <QFile>
@@ -32,6 +33,7 @@ This file is part of the APM_PLANNER project
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonArray>
+#include <QCryptographicHash>
 #include <QApplication>
 
 #include "apmremoteparamscontroller.h"
@@ -39,12 +41,17 @@ This file is part of the APM_PLANNER project
 #define FRAME_PARAMS_LIST QUrl("https://api.github.com/repos/diydrones/ardupilot/contents/Tools/Frame_params")
 #define FRAME_PARAMS_URL "https://raw.github.com/diydrones/ardupilot/master/Tools/Frame_params/"
 
-static const QString dataLocation = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).at(0);
+static QString dataLocation;
 
 APMRemoteParamsController::APMRemoteParamsController() :
-    m_networkReply(NULL)
+    m_networkReply(NULL),
+    m_downloadedParamFile(NULL)
 {
+    dataLocation = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).at(0)
+           + QDir::separator() + qApp->applicationName();
     refreshParamList();
+
+    qDebug() << "DataLocation " << dataLocation;
 }
 
 QString APMRemoteParamsController::statusText() const
@@ -87,8 +94,12 @@ void APMRemoteParamsController::refreshParamList()
 */
 void APMRemoteParamsController::startFileDownloadRequest()
 {
-    if (curr == end)
+    if (curr == end) {
+        // invalidate the iterators, clean memory.
+        m_documentArray = QJsonArray();
         return;
+
+    }
 
     QJsonObject obj = (*curr).toObject();
     QUrl url(obj["download_url"].toString());
@@ -97,12 +108,50 @@ void APMRemoteParamsController::startFileDownloadRequest()
     if (!parameterDir.exists())
         parameterDir.mkpath(dataLocation);
 
-    QString filename = obj["name"].toString();
-    QFile::remove(filename);
-    m_downloadedParamFile = new QFile(parameterDir.absoluteFilePath(filename));
+    QString filename = parameterDir.absoluteFilePath(obj["name"].toString());
+    /* The resulting JSON object has a 'sha' hash, but testing the sha hash against all of the below
+     * algorithms (even md4 or md5) didn't returned a single positive match.
+     *
+     * when they fix that I'll try to use the code below to only download the files that changed,
+     * so, currently I'm downloading everything.
+     */
+#if 0
+    QFileInfo info(filename);
+    if (info.exists()) {
+        QFile file(filename);
+        file.open(QIODevice::ReadOnly);
+        QByteArray file_data = file.readAll();
+        QByteArray sha1 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha1);
+        QByteArray md5 = QCryptographicHash::hash(file_data, QCryptographicHash::Md5);
+        QByteArray sha3_256 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha3_256);
+        QByteArray sha3_224 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha3_224);
+        QByteArray sha3_384 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha3_384);
+        QByteArray sha3_512 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha3_512);
+        QByteArray sha_256 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha256);
+        QByteArray sha_224 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha224);
+        QByteArray sha_384 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha384);
+        QByteArray sha_512 = QCryptographicHash::hash(file_data, QCryptographicHash::Sha512);
+        QByteArray md4 = QCryptographicHash::hash(file_data, QCryptographicHash::Md4);
+        qDebug() << sha1.toHex();
+        qDebug() << md5.toHex();
+        qDebug() << sha3_256.toHex();
+        qDebug() << sha3_224.toHex();
+        qDebug() << sha3_384.toHex();
+        qDebug() << sha3_512.toHex();
+        qDebug() << sha_256.toHex();
+        qDebug() << sha_224.toHex();
+        qDebug() << sha_384.toHex();
+        qDebug() << sha_512.toHex();
+        qDebug() << md4.toHex();
+        qDebug() << "----------";
+        qDebug() << obj["sha"].toString();
+    }
+#endif
+    if(m_downloadedParamFile)
+        m_downloadedParamFile->deleteLater();
+    m_downloadedParamFile = new QFile(filename);
     m_downloadedParamFile->open(QIODevice::WriteOnly);
 
-    qDebug() << url;
     m_networkReply = m_networkAccessManager.get(QNetworkRequest(url));
     connect(m_networkReply, SIGNAL(finished()), this, SLOT(httpFinished()));
     connect(m_networkReply, SIGNAL(readyRead()), this, SLOT(httpReadyRead()));
