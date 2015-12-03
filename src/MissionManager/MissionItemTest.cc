48aac9db568ef8489d28ef3e3a3af04c3b2fd0fb
Don Gagne <don@thegagnes.com>
2015-12-03 14:47:46 -0800

    Add default value unit test

--- src/MissionManager/MissionItemTest.cc
+++ src/MissionManager/MissionItemTest.cc
@@ -36,8 +36,9 @@ const MissionItemTest::ItemInfo_t MissionItemTest::_rgItemInfo[] = {
 };
 
 const MissionItemTest::FactValue_t MissionItemTest::_rgFactValuesWaypoint[] = {
-    { "Altitude:",  70.1234567 },
-    { "Hold:",      10.1234567 },
+    { "Altitude:",      70.1234567 },
+    { "Hold:",          10.1234567 },
+    { "Accept radius:", 20.1234567 },
 };
 
 const MissionItemTest::FactValue_t MissionItemTest::_rgFactValuesLoiterUnlim[] = {
@@ -175,4 +176,14 @@ void MissionItemTest::_test(void)
         delete item;
         delete loadedItem;
     }
+}
+
+void MissionItemTest::_testDefaultValues(void)
+{
+    MissionItem item;
+
+    item.setCommand(MAV_CMD_NAV_WAYPOINT);
+    item.setFrame(MAV_FRAME_GLOBAL_RELATIVE_ALT);
+    QCOMPARE(item.param2(), 3.0);
+    QCOMPARE(item.param7(), MissionItem::defaultAltitude);
 }
