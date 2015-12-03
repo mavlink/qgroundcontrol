8d020e3a444719c6164b0b58e7826a1cb64f3c02
dogmaphobic <mavlink@grubba.com>
2015-12-03 13:01:00 -0500

    Missed a few places where Mock Link had references on Release builds.

--- src/FactSystem/FactSystemTestBase.cc
+++ src/FactSystem/FactSystemTestBase.cc
@@ -1,24 +1,24 @@
 /*=====================================================================
- 
+
  QGroundControl Open Source Ground Control Station
- 
+
  (c) 2009 - 2014 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
- 
+
  This file is part of the QGROUNDCONTROL project
- 
+
  QGROUNDCONTROL is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  QGROUNDCONTROL is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
- 
+
  ======================================================================*/
 
 /// @file
@@ -26,7 +26,9 @@
 
 #include "FactSystemTestBase.h"
 #include "LinkManager.h"
+#ifdef QT_DEBUG
 #include "MockLink.h"
+#endif
 #include "MultiVehicleManager.h"
 #include "QGCApplication.h"
 #include "QGCMessageBox.h"
@@ -37,15 +39,15 @@
 /// FactSystem Unit Test
 FactSystemTestBase::FactSystemTestBase(void)
 {
-    
+
 }
 
 void FactSystemTestBase::_init(MAV_AUTOPILOT autopilot)
 {
     UnitTest::init();
-    
+
     _connectMockLink(autopilot);
-    
+
     _plugin = qgcApp()->toolbox()->multiVehicleManager()->activeVehicle()->autopilotPlugin();
     Q_ASSERT(_plugin);
 }
@@ -63,7 +65,7 @@ void FactSystemTestBase::_parameter_default_component_id_test(void)
     QVERIFY(fact != NULL);
     QVariant factValue = fact->rawValue();
     QCOMPARE(factValue.isValid(), true);
-    
+
     QCOMPARE(factValue.toInt(), 3);
 }
 
@@ -74,17 +76,17 @@ void FactSystemTestBase::_parameter_specific_component_id_test(void)
     QVERIFY(fact != NULL);
     QVariant factValue = fact->rawValue();
     QCOMPARE(factValue.isValid(), true);
-    
-    
+
+
     QCOMPARE(factValue.toInt(), 3);
-    
+
     // Test another component id
     QVERIFY(_plugin->factExists(FactSystem::ParameterProvider, 51, "COMPONENT_51"));
     fact = _plugin->getFact(FactSystem::ParameterProvider, 51, "COMPONENT_51");
     QVERIFY(fact != NULL);
     factValue = fact->rawValue();
     QCOMPARE(factValue.isValid(), true);
-    
+
     QCOMPARE(factValue.toInt(), 51);
 }
 
@@ -92,18 +94,18 @@ void FactSystemTestBase::_parameter_specific_component_id_test(void)
 void FactSystemTestBase::_qml_test(void)
 {
     QGCQuickWidget* widget = new QGCQuickWidget;
-    
+
     widget->setAutoPilot(_plugin);
-    
+
     widget->setSource(QUrl::fromUserInput("qrc:unittest/FactSystemTest.qml"));
-    
+
     QQuickItem* rootObject = widget->rootObject();
     QObject* control = rootObject->findChild<QObject*>("testControl");
     QVERIFY(control != NULL);
     QVariant qmlValue = control->property("text").toInt();
 
     QCOMPARE(qmlValue.toInt(), 3);
-    
+
     delete widget;
 }
 
@@ -111,25 +113,25 @@ void FactSystemTestBase::_qml_test(void)
 void FactSystemTestBase::_qmlUpdate_test(void)
 {
     QGCQuickWidget* widget = new QGCQuickWidget;
-    
+
     widget->setAutoPilot(_plugin);
-    
+
     widget->setSource(QUrl::fromUserInput("qrc:unittest/FactSystemTest.qml"));
-    
+
     // Change the value
-    
+
     QVariant paramValue = 12;
     _plugin->getParameterFact(FactSystem::defaultComponentId, "RC_MAP_THROTTLE")->setRawValue(paramValue);
 
     QTest::qWait(500); // Let the signals flow through
-    
+
     // Make sure the qml has the right value
-    
+
     QQuickItem* rootObject = widget->rootObject();
     QObject* control = rootObject->findChild<QObject*>("testControl");
     QVERIFY(control != NULL);
     QCOMPARE(control->property("text").toInt(), 12);
-    
+
     delete widget;
 }
 
