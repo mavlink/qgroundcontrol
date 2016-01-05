e25df0708b4310a7a65bfc8251c7759ef7697b60
Don Gagne <don@thegagnes.com>
2016-01-05 13:36:55 -0800

    Mission protocol robustness rework

--- src/comm/MockLink.h
+++ src/comm/MockLink.h
@@ -131,8 +131,7 @@ public:
 
     /// Sets a failure mode for unit testing
     ///     @param failureMode Type of failure to simulate
-    ///     @param firstTimeOnly true: fail first call, success subsequent calls, false: fail all calls
-    void setMissionItemFailureMode(MockLinkMissionItemHandler::FailureMode_t failureMode, bool firstTimeOnly);
+    void setMissionItemFailureMode(MockLinkMissionItemHandler::FailureMode_t failureMode);
 
     /// Called to send a MISSION_ACK message while the MissionManager is in idle state
     void sendUnexpectedMissionAck(MAV_MISSION_RESULT ackType) { _missionItemHandler.sendUnexpectedMissionAck(ackType); }
