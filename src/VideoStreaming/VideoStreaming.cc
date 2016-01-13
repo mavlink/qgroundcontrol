42460887671bb1664560d584d075e4639b42a7f3
dogmaphobic <mavlink@grubba.com>
2016-01-13 03:18:59 -0500

    Revert ""Fix" for hanging on exit when Video Streaming build is enabled."
    This reverts commit 02f69bea56b3ef2e19536dfd750d0f23928d102f.

--- src/VideoStreaming/VideoStreaming.cc
+++ src/VideoStreaming/VideoStreaming.cc
@@ -114,16 +114,7 @@ void initializeVideoStreaming(int &argc, char* argv[])
 
 void shutdownVideoStreaming()
 {
-    /* From: http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gstreamer/html/gstreamer-Gst.html#gst-deinit
-     *
-     * "It is normally not needed to call this function in a normal application as the resources will automatically
-     * be freed when the program terminates. This function is therefore mostly used by testsuites and other memory
-     * profiling tools."
-     *
-     * It's causing a hang on exit. It hangs while deleting some thread.
-     *
 #if defined(QGC_GST_STREAMING)
     gst_deinit();
 #endif
-    */
 }
