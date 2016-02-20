131973ce84758e279a16110c3d4d7a8526dd2f21
Lorenz Meier <lm@inf.ethz.ch>
2016-02-20 15:34:46 +0100

    Android build debugging

--- tools/update_android_version.sh
+++ tools/update_android_version.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 MANIFEST_FILE=android/AndroidManifest.xml
 
@@ -10,12 +10,16 @@ echo "VersionName: ${VERSIONNAME}"
 
 if [ -n "$VERSIONCODE" ]; then
 	sed -i -e "s/android:versionCode=\"[0-9][0-9]*\"/android:versionCode=\"$VERSIONCODE\"/" $MANIFEST_FILE
+	echo "Android version: ${VERSIONCODE}"
 else
 	echo "Error versionCode empty"
+	exit 1
 fi
 
 if [ -n "$VERSIONNAME" ]; then
 	sed -i -e 's/versionName *= *"[^"]*"/versionName="'$VERSIONNAME'"/' $MANIFEST_FILE
+	echo "Android name: ${VERSIONNAME}"
 else
 	echo "Error versionName empty"
+	exit 1
 fi
