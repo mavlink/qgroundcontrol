d8d8fedf11e4d921f818fc91515bdf4dbe1a6eb8
Don Gagne <don@thegagnes.com>
2016-02-29 09:05:35 -0800

    Better debugging support

--- src/qgcunittest/MultiSignalSpy.h
+++ src/qgcunittest/MultiSignalSpy.h
@@ -26,6 +26,7 @@
 
 #include <QObject>
 #include <QSignalSpy>
+#include <QGeoCoordinate>
 
 /// @file
 ///     @brief This class allows you to keep track of signal counts on a set of signals associated with an object.
@@ -46,42 +47,44 @@ public:
     /// @param mask bit mask specifying which signals to check. The lowest order bit represents
     ///     index 0 into the rgSignals array and so on up the bit mask.
     /// @return true if signal count = 1 for the specified signals
-    bool checkSignalByMask(quint16 mask);
+    bool checkSignalByMask(quint32 mask);
 
     /// @return true if signal count = 1 for specified signals and signal count of 0
     ///     for all other signals
-    bool checkOnlySignalByMask(quint16 mask);
+    bool checkOnlySignalByMask(quint32 mask);
 
     /// @param mask bit mask specifying which signals to check. The lowest order bit represents
     ///     index 0 into the rgSignals array and so on up the bit mask.
     /// @return true if signal count >= 1 for the specified signals
-    bool checkSignalsByMask(quint16 mask);
+    bool checkSignalsByMask(quint32 mask);
 
     /// @return true if signal count >= 1 for specified signals and signal count of 0
     ///     for all other signals
-    bool checkOnlySignalsByMask(quint16 mask);
+    bool checkOnlySignalsByMask(quint32 mask);
 
-    bool checkNoSignalByMask(quint16 mask);
+    bool checkNoSignalByMask(quint32 mask);
     bool checkNoSignals(void);
 
-    void clearSignalByIndex(quint16 index);
-    void clearSignalsByMask(quint16 mask);
+    void clearSignalByIndex(quint32 index);
+    void clearSignalsByMask(quint32 mask);
     void clearAllSignals(void);
 
-    bool waitForSignalByIndex(quint16 index, int msec);
+    bool waitForSignalByIndex(quint32 index, int msec);
     
-    QSignalSpy* getSpyByIndex(quint16 index);
+    QSignalSpy* getSpyByIndex(quint32 index);
 
-    /// Returns the boolean value for a signal which has a single bool param
-    bool pullBoolFromSignalIndex(quint16 index);
-    
+    // Returns the value type for the first parameter of the signal
+    bool pullBoolFromSignalIndex(quint32 index);
+    int pullIntFromSignalIndex(quint32 index);
+    QGeoCoordinate pullQGeoCoordinateFromSignalIndex(quint32 index);
+
 private:
     // QObject overrides
     void timerEvent(QTimerEvent * event);
     
-    void _printSignalState(void);
-    bool _checkSignalByMaskWorker(quint16 mask, bool multipleSignalsAllowed);
-    bool _checkOnlySignalByMaskWorker(quint16 mask, bool multipleSignalsAllowed);
+    void _printSignalState(quint32 mask);
+    bool _checkSignalByMaskWorker(quint32 mask, bool multipleSignalsAllowed);
+    bool _checkOnlySignalByMaskWorker(quint32 mask, bool multipleSignalsAllowed);
 
     QObject*        _signalEmitter;
     const char**    _rgSignals;
