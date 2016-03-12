caa0242db80a84fa7b2bceca8bfdd2b160fa4ed8
Don Gagne <don@thegagnes.com>
2016-03-12 13:49:26 -0800

    Screen sleep control for iOS

---
+++ src/MobileScreenMgr.h
@@ -0,0 +1,36 @@
+/*=====================================================================
+ 
+ QGroundControl Open Source Ground Control Station
+ 
+ (c) 2009 - 2014 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ 
+ This file is part of the QGROUNDCONTROL project
+ 
+ QGROUNDCONTROL is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ QGROUNDCONTROL is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+ 
+ ======================================================================*/
+
+#ifndef MobileScreenMgr_H
+#define MobileScreenMgr_H
+
+#ifdef __mobile__
+class MobileScreenMgr {
+    
+public:
+    /// Turns on/off screen sleep on mobile devices
+    static void setKeepScreenOn(bool keepScreenOn);
+};
+#endif
+
+#endif
