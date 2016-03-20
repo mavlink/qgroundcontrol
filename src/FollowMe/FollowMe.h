e6a0c546e2b892204effde2bfaea52b54bd009b1
Jimmy Johnson <catch22@fastmail.net>
2016-03-19 22:42:49 -0700

    fixing ci build error

--- src/FollowMe/FollowMe.h
+++ src/FollowMe/FollowMe.h
@@ -80,7 +80,9 @@ private:
     struct simulated_motion_s {
         int lon;
         int lat;
-    }_simulated_motion[4];
+    };
+
+    static simulated_motion_s _simulated_motion[4];
 
     int _simulate_motion_timer;
     int _simulate_motion_index;
