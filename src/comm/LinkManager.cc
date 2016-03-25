28ab64a1480ac636cc55be894e84ca1720aa9f1e
Nate Weibley <nate.weibley@prioria.com>
2016-03-25 14:20:00 -0400

    Force link writes to occur via a signal/slot connection.
    This mechanism automatically queues data into the correct thread where necessary, otherwise the method is invoked directly with low overhead.
    
    At the same time remove the UDP socket event workaround. It is not a problem.

--- src/comm/LinkManager.cc
+++ src/comm/LinkManager.cc
@@ -881,7 +881,7 @@ void LinkManager::_activeLinkCheck(void)
     if (!found && link) {
         // See if we can get an NSH prompt on this link
         bool foundNSHPrompt = false;
-        link->writeBytes("\r", 1);
+        link->writeBytesSafe("\r", 1);
         QSignalSpy spy(link, SIGNAL(bytesReceived(LinkInterface*, QByteArray)));
         if (spy.wait(100)) {
             QList<QVariant> arguments = spy.takeFirst();
