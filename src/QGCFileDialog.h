8188f645d72de0fddfee1b7ddb77f5a9bc2050a6
Don Gagne <don@thegagnes.com>
2016-03-31 09:25:44 -0700

    Update for new Clang errors

--- src/QGCFileDialog.h
+++ src/QGCFileDialog.h
@@ -134,7 +134,7 @@ public:
 private slots:
     /// @brief The exec slot is private because we only want QGCFileDialog users to use the static methods. Otherwise it will break
     ///        unit testing.
-    int exec(void) { return QGCFileDialog::exec(); }
+    int exec(void) { return QFileDialog::exec(); }
     
 private:
     static void    _validate(Options& options);
