9fbaee87ee6195c389c57cd73e19ea62850da01b
probonopd <probonopd@users.noreply.github.com>
2016-04-02 13:57:40 +0200

    Make it possible to run qgroundcontrol-start.sh from outside its directory

--- deploy/qgroundcontrol-start.sh
+++ deploy/qgroundcontrol-start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=`pwd`/Qt/libs:$LD_LIBRARY_PATH
-export QML2_IMPORT_PATH=`pwd`/Qt/qml
-export QT_PLUGIN_PATH=`pwd`/Qt/plugins
-./qgroundcontrol "$@"
+HERE="$(dirname "$(readlink -f "${0}")")"
+export LD_LIBRARY_PATH="${HERE}/Qt/libs":$LD_LIBRARY_PATH
+export QML2_IMPORT_PATH="${HERE}/Qt/qml"
+export QT_PLUGIN_PATH="${HERE}/Qt/plugins"
+"${HERE}/qgroundcontrol" "$@"
