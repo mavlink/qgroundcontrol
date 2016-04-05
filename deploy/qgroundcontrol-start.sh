c9a344be14fd6f7ac4841dca720abe302e5ac609
Daniel Agar <daniel@agar.ca>
2016-04-05 13:42:21 -0400

    qgroundcontrol-start.sh add AppImage lib path

--- deploy/qgroundcontrol-start.sh
+++ deploy/qgroundcontrol-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HERE="$(dirname "$(readlink -f "${0}")")"
-export LD_LIBRARY_PATH="${HERE}/Qt/libs":$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="${HERE}/usr/lib/":"${HERE}/Qt/libs":$LD_LIBRARY_PATH
 export QML2_IMPORT_PATH="${HERE}/Qt/qml"
 export QT_PLUGIN_PATH="${HERE}/Qt/plugins"
 "${HERE}/qgroundcontrol" "$@"
