275ea54a85b9901b03344c30b51a861c15743889
dogmaphobic <mavlink@grubba.com>
2016-04-30 02:38:09 -0400

    Support for new firmware. Added setting to set the WiFi mode (AP or Station)

--- src/AutoPilotPlugins/Common/ESP8266ComponentController.cc
+++ src/AutoPilotPlugins/Common/ESP8266ComponentController.cc
@@ -1,24 +1,24 @@
 /*=====================================================================
- 
+
  QGroundControl Open Source Ground Control Station
- 
+
  (c) 2009, 2016 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
- 
+
  This file is part of the QGROUNDCONTROL project
- 
+
  QGROUNDCONTROL is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  QGROUNDCONTROL is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
- 
+
  ======================================================================*/
 
 /// @file
@@ -72,6 +72,21 @@ ESP8266ComponentController::version()
     uint32_t uv = getParameterFact(MAV_COMP_ID_UDP_BRIDGE, "SW_VER")->rawValue().toUInt();
     QString versionString = QString("%1.%2.%3").arg(uv >> 24).arg((uv >> 16) & 0xFF).arg(uv & 0xFFFF);
     return versionString;
+}
+
+//-----------------------------------------------------------------------------
+QString
+ESP8266ComponentController::wifiIPAddress()
+{
+    if(_ipAddress.isEmpty()) {
+        if(parameterExists(MAV_COMP_ID_UDP_BRIDGE, "WIFI_IPADDRESS")) {
+            QHostAddress address(ntohl(getParameterFact(MAV_COMP_ID_UDP_BRIDGE, "WIFI_IPADDRESS")->rawValue().toUInt()));
+            _ipAddress = address.toString();
+        } else {
+            _ipAddress = "192.168.4.1";
+        }
+    }
+    return _ipAddress;
 }
 
 //-----------------------------------------------------------------------------
