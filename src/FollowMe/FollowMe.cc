594ae8134108e92a7ea695c9609ed56f0fc9de52
Don Gagne <don@thegagnes.com>
2016-06-29 08:49:36 -0700

    Follow Me is PX4 only (#3689)

--- src/FollowMe/FollowMe.cc
+++ src/FollowMe/FollowMe.cc
@@ -38,7 +38,7 @@ void FollowMe::followMeHandleManager(const QString&)
 
     for (int i=0; i< vehicles.count(); i++) {
         Vehicle* vehicle = qobject_cast<Vehicle*>(vehicles[i]);
-        if(vehicle->flightMode().compare(PX4FirmwarePlugin::followMeFlightMode, Qt::CaseInsensitive) == 0) {
+        if (vehicle->px4Firmware() && vehicle->flightMode().compare(PX4FirmwarePlugin::followMeFlightMode, Qt::CaseInsensitive) == 0) {
             _enable();
             return;
         }
