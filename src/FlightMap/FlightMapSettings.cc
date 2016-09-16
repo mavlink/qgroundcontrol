50f8db09eac37fb853a5a64b389206493f62b4d9
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2016-09-16 20:26:53 +0200

    Maps:Added Statkart Topo2 (very useful norwegian topographic map)
    This map is an absolute necessary for navigating the northern parts.

--- src/FlightMap/FlightMapSettings.cc
+++ src/FlightMap/FlightMapSettings.cc
@@ -32,6 +32,7 @@ void FlightMapSettings::setToolbox(QGCToolbox *toolbox)
 #ifndef QGC_NO_GOOGLE_MAPS
     _supportedMapProviders << "Google";
 #endif
+    _supportedMapProviders << "Statkart";
     _loadSettings();
 }
 
@@ -82,6 +83,8 @@ void FlightMapSettings::_setMapTypesForCurrentProvider(void)
         _mapTypes << "Street Map" << "Satellite Map" << "Hybrid Map";
     } else if (_mapProvider == "Google") {
         _mapTypes << "Street Map" << "Satellite Map" << "Terrain Map";
+    } else if (_mapProvider == "Statkart") {
+        _mapTypes << "Topo2";
     }
 #endif
     emit mapTypesChanged(_mapTypes);
