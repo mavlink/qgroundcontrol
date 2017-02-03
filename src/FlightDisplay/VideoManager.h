5766d7df47ba0d3a985162ccd882d29ecc8322fb
Gus Grubba <mavlink@grubba.com>
2017-02-02 20:05:15 -0500

    Allow disabling video recording

--- src/FlightDisplay/VideoManager.h
+++ src/FlightDisplay/VideoManager.h
@@ -30,18 +30,19 @@ public:
     VideoManager    (QGCApplication* app);
     ~VideoManager   ();
 
-    Q_PROPERTY(bool             hasVideo        READ    hasVideo                                    NOTIFY hasVideoChanged)
-    Q_PROPERTY(bool             isGStreamer     READ    isGStreamer                                 NOTIFY isGStreamerChanged)
-    Q_PROPERTY(QString          videoSourceID   READ    videoSourceID                               NOTIFY videoSourceIDChanged)
-    Q_PROPERTY(QString          videoSource     READ    videoSource     WRITE setVideoSource        NOTIFY videoSourceChanged)
-    Q_PROPERTY(QStringList      videoSourceList READ    videoSourceList                             NOTIFY videoSourceListChanged)
-    Q_PROPERTY(bool             videoRunning    READ    videoRunning                                NOTIFY videoRunningChanged)
-    Q_PROPERTY(quint16          udpPort         READ    udpPort         WRITE setUdpPort            NOTIFY udpPortChanged)
-    Q_PROPERTY(QString          rtspURL         READ    rtspURL         WRITE setRtspURL            NOTIFY rtspURLChanged)
-    Q_PROPERTY(QString          videoSavePath   READ    videoSavePath                               NOTIFY videoSavePathChanged)
-    Q_PROPERTY(bool             uvcEnabled      READ    uvcEnabled                                  CONSTANT)
-    Q_PROPERTY(VideoSurface*    videoSurface    MEMBER  _videoSurface                               CONSTANT)
-    Q_PROPERTY(VideoReceiver*   videoReceiver   MEMBER  _videoReceiver                              CONSTANT)
+    Q_PROPERTY(bool             hasVideo            READ    hasVideo                                    NOTIFY hasVideoChanged)
+    Q_PROPERTY(bool             isGStreamer         READ    isGStreamer                                 NOTIFY isGStreamerChanged)
+    Q_PROPERTY(QString          videoSourceID       READ    videoSourceID                               NOTIFY videoSourceIDChanged)
+    Q_PROPERTY(QString          videoSource         READ    videoSource     WRITE setVideoSource        NOTIFY videoSourceChanged)
+    Q_PROPERTY(QStringList      videoSourceList     READ    videoSourceList                             NOTIFY videoSourceListChanged)
+    Q_PROPERTY(bool             videoRunning        READ    videoRunning                                NOTIFY videoRunningChanged)
+    Q_PROPERTY(quint16          udpPort             READ    udpPort         WRITE setUdpPort            NOTIFY udpPortChanged)
+    Q_PROPERTY(QString          rtspURL             READ    rtspURL         WRITE setRtspURL            NOTIFY rtspURLChanged)
+    Q_PROPERTY(QString          videoSavePath       READ    videoSavePath                               NOTIFY videoSavePathChanged)
+    Q_PROPERTY(bool             uvcEnabled          READ    uvcEnabled                                  CONSTANT)
+    Q_PROPERTY(VideoSurface*    videoSurface        MEMBER  _videoSurface                               CONSTANT)
+    Q_PROPERTY(VideoReceiver*   videoReceiver       MEMBER  _videoReceiver                              CONSTANT)
+    Q_PROPERTY(bool             recordingEnabled    READ    recordingEnabled                            CONSTANT)
 
     Q_INVOKABLE void setVideoSavePathByUrl (QUrl url);
 
@@ -59,6 +60,12 @@ public:
     bool        uvcEnabled          () { return false; }
 #else
     bool        uvcEnabled          ();
+#endif
+
+#if defined(QGC_GST_STREAMING) && defined(QGC_ENABLE_VIDEORECORDING)
+    bool        recordingEnabled    () { return true; }
+#else
+    bool        recordingEnabled    () { return false; }
 #endif
 
     void        setVideoSource          (QString vSource);
