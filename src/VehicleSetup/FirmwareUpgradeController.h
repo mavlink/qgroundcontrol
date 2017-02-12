143434f49190aaa417bc8a938d34b35f0ed6b584
Phillip Kocmoud <pkocmoud@hyper-it.com>
2017-02-12 11:00:58 -0600

    Add support for the AUAV X2.1 Flight Controller (#4501)
    * Fix minor spelling error
    
    * Added support for the AUAV x2.1 Flight Controller
        also removed a duplicate entry for MindPX
        from src/comm/USBBoardInfo.json
    
    Adds entries required to correctly identify, download and flash PX4
    firmware to the AUAV X2.1 Flight Controller
    
    * Update USBBoardInfo.json
    
    Removed un-needed fallback entries. This has been tested!

--- src/VehicleSetup/FirmwareUpgradeController.h
+++ src/VehicleSetup/FirmwareUpgradeController.h
@@ -198,6 +198,7 @@ private:
     QHash<FirmwareIdentifier, QString> _rgPX4FMUV2Firmware;
     QHash<FirmwareIdentifier, QString> _rgAeroCoreFirmware;
     QHash<FirmwareIdentifier, QString> _rgPX4FMUV1Firmware;
+    QHash<FirmwareIdentifier, QString> _rgAUAVX2_1Firmware;
     QHash<FirmwareIdentifier, QString> _rgMindPXFMUV2Firmware;
     QHash<FirmwareIdentifier, QString> _rgTAPV1Firmware;
     QHash<FirmwareIdentifier, QString> _rgASCV1Firmware;
