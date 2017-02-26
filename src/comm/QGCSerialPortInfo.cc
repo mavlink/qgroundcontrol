3bc22fdc5361bffedece4504172610e73a76960b
Dennis Shtatnov <densht@gmail.com>
2017-02-26 09:59:15 -0500

    Improve Syslink UI

--- src/comm/QGCSerialPortInfo.cc
+++ src/comm/QGCSerialPortInfo.cc
@@ -204,8 +204,6 @@ bool QGCSerialPortInfo::getBoardInfo(QGCSerialPortInfo::BoardType_t& boardType, 
     if (boardType == BoardTypeUnknown) {
         // Fall back to port name matching which could lead to incorrect board mapping. But in some cases the
         // vendor and product id do not come through correctly so this is used as a last chance detection method.
-<<<<<<< c4da69536e067addfbf394609e9369c1c2d00129
-
         for (int i=0; i<_boardFallbackList.count(); i++) {
             const BoardFallback_t& boardFallback = _boardFallbackList[i];
 
@@ -214,44 +212,6 @@ bool QGCSerialPortInfo::getBoardInfo(QGCSerialPortInfo::BoardType_t& boardType, 
                 if (boardFallback.androidOnly) {
                     continue;
                 }
-=======
-        if (description() == "PX4 FMU v4.x" || description() == "PX4 BL FMU v4.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found PX4 FMU V4 (by name matching fallback)";
-            boardType = BoardTypePX4FMUV4;
-        } else if (description() == "PX4 FMU v2.x" || description() == "PX4 BL FMU v2.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found PX4 FMU V2 (by name matching fallback)";
-            boardType = BoardTypePX4FMUV2;
-        } else if (description() == "PX4 FMU v1.x" || description() == "PX4 BL FMU v1.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found PX4 FMU V1 (by name matching fallback)";
-            boardType = BoardTypePX4FMUV1;
-        } else if (description().startsWith("PX4 FMU")) {
-            qCDebug(QGCSerialPortInfoLog) << "Found PX4 FMU, assuming V2 (by name matching fallback)";
-            boardType = BoardTypePX4FMUV2;
-        } else if (description().contains(QRegExp("PX4.*Flow", Qt::CaseInsensitive))) {
-            qCDebug(QGCSerialPortInfoLog) << "Found possible px4 flow camera (by name matching fallback)";
-            boardType = BoardTypePX4Flow;
-        } else if (description() == "MindPX FMU v2.x" || description() == "MindPX BL FMU v2.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found MindPX FMU V2 (by name matching fallback)";
-            boardType = BoardTypeMINDPXFMUV2;
-        } else if (description() == "PX4 TAP v1.x" || description() == "PX4 BL TAP v1.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found TAP V1 (by name matching fallback)";
-            boardType = BoardTypeTAPV1;
-        } else if (description() == "PX4 ASC v1.x" || description() == "PX4 BL ASC v1.x") {
-            qCDebug(QGCSerialPortInfoLog) << "Found ASC V1 (by name matching fallback)";
-            boardType = BoardTypeASCV1;
-        } else if (description() == "PX4 Crazyflie v2.0" || description() == "Crazyflie BL") {
-            qCDebug(QGCSerialPortInfoLog) << "Found Crazyflie 2.0 (by name matching fallback)";
-            boardType = BoardTypeCrazyflie2;
-        } else if (description() == "FT231X USB UART") {
-            qCDebug(QGCSerialPortInfoLog) << "Found possible Radio (by name matching fallback)";
-            boardType = BoardTypeSikRadio;
-#ifdef __android__
-        } else if (description().endsWith("USB UART")) {
-            // This is a fairly broad fallbacks for radios which will also catch most FTDI devices. That would
-            // cause problems on desktop due to incorrect connections. Since mobile is more anal about connecting
-            // it will work fine here.
-            boardType = BoardTypeSikRadio;
->>>>>>> Add firmware upgrade support for CF2
 #endif
                 boardType = boardFallback.boardType;
                 name = _boardTypeToString(boardType);
@@ -298,19 +258,6 @@ QList<QGCSerialPortInfo> QGCSerialPortInfo::availablePorts(void)
     return list;
 }
 
-<<<<<<< c4da69536e067addfbf394609e9369c1c2d00129
-=======
-bool QGCSerialPortInfo::boardTypePixhawk(void) const
-{
-    BoardType_t boardType = this->boardType();
-
-    return boardType == BoardTypePX4FMUV1 || boardType == BoardTypePX4FMUV2
-            || boardType == BoardTypePX4FMUV4 || boardType == BoardTypeAeroCore
-            || boardType == BoardTypeMINDPXFMUV2 || boardType == BoardTypeTAPV1
-            || boardType == BoardTypeASCV1 || boardType == BoardTypeCrazyflie2;
-}
-
->>>>>>> Add firmware upgrade support for CF2
 bool QGCSerialPortInfo::isBootloader(void) const
 {
     BoardType_t boardType;
