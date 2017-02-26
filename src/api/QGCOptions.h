906e49838fa8c6840ec2e55755fcd64bdf41be8b
Don Gagne <don@thegagnes.com>
2017-02-26 13:18:00 -0800

    Remove deprecated methods
    Replaced by new settings capability

--- src/api/QGCOptions.h
+++ src/api/QGCOptions.h
@@ -26,60 +26,36 @@ public:
 
     Q_PROPERTY(bool     combineSettingsAndSetup     READ combineSettingsAndSetup    CONSTANT)
     Q_PROPERTY(bool     enableVirtualJoystick       READ enableVirtualJoystick      CONSTANT)
-    Q_PROPERTY(bool     definesVideo                READ definesVideo               CONSTANT)
-    Q_PROPERTY(uint16_t videoUDPPort                READ videoUDPPort               CONSTANT)
-    Q_PROPERTY(QString  videoRSTPUrl                READ videoRSTPUrl               CONSTANT)
     Q_PROPERTY(double   toolbarHeightMultiplier     READ toolbarHeightMultiplier    CONSTANT)
     Q_PROPERTY(double   defaultFontPointSize        READ defaultFontPointSize       CONSTANT)
     Q_PROPERTY(bool     enablePlanViewSelector      READ enablePlanViewSelector     CONSTANT)
     Q_PROPERTY(CustomInstrumentWidget*  instrumentWidget READ instrumentWidget      CONSTANT)
 
 
-    //! Should QGC hide its settings menu and colapse it into one single menu (Settings and Vehicle Setup)?
-    /*!
-        @return true if QGC should consolidate both menus into one.
-    */
+    /// Should QGC hide its settings menu and colapse it into one single menu (Settings and Vehicle Setup)?
+    /// @return true if QGC should consolidate both menus into one.
     virtual bool        combineSettingsAndSetup     () { return false; }
-    //! Should QGC use virtual Joysticks?
-    /*!
-        @return false to disable Virtual Joysticks.
-    */
+
+    /// Should QGC use virtual Joysticks?
+    /// @return false to disable Virtual Joysticks.
     virtual bool        enableVirtualJoystick       () { return true;  }
-    //! Does your plugin defines its on video source?
-    /*!
-        @return true to define your own video source.
-    */
-    virtual bool        definesVideo                () { return false; }
-    //! UDP port to use for (RTP) video source.
-    /*!
-        @return UDP Port to use. Return 0 to disable UDP RTP.
-    */
-    virtual uint16_t    videoUDPPort                () { return 0; }
-    //! RTSP URL to use for video source.
-    /*!
-        @return RTSP url to use. Return "" to disable RTSP.
-    */
-    virtual QString     videoRSTPUrl                () { return QString(); }
-    //! Main ToolBar Multiplier.
-    /*!
-        @return Factor to use when computing toolbar height
-    */
+
+    /// Main ToolBar Multiplier.
+    /// @return Factor to use when computing toolbar height
     virtual double      toolbarHeightMultiplier     () { return 1.0; }
-    //! Application wide default font point size
-    /*!
-        @return Font size or 0.0 to use computed size.
-    */
+
+    /// Application wide default font point size
+    /// @return Font size or 0.0 to use computed size.
     virtual double      defaultFontPointSize        () { return 0.0; }
-    //! Enable Plan View Selector (Mission, Fence or Rally)
-    /*!
-        @return True or false
-    */
+
+    /// Enable Plan View Selector (Mission, Fence or Rally)
+    /// @return True or false
     virtual bool        enablePlanViewSelector      () { return true; }
-    //! Provides an alternate instrument widget for the Fly View
-    /*!
-        @return An alternate widget (see QGCInstrumentWidget.qml, the default widget)
-    */
+
+    /// Provides an alternate instrument widget for the Fly View
+    /// @return An alternate widget (see QGCInstrumentWidget.qml, the default widget)
     virtual CustomInstrumentWidget* instrumentWidget();
+
 private:
     CustomInstrumentWidget* _defaultInstrumentWidget;
 };
