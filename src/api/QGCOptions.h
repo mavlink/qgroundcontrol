b0273cae1177cb27d8608d9587d46e1194e898cb
DonLakeFlyer <don@thegagnes.com>
2017-03-15 12:12:53 -0700

    Core plugin can determine Sensor pages to show

--- src/api/QGCOptions.h
+++ src/api/QGCOptions.h
@@ -24,11 +24,16 @@ class QGCOptions : public QObject
 public:
     QGCOptions(QObject* parent = NULL);
 
-    Q_PROPERTY(bool     combineSettingsAndSetup     READ combineSettingsAndSetup    CONSTANT)
-    Q_PROPERTY(double   toolbarHeightMultiplier     READ toolbarHeightMultiplier    CONSTANT)
-    Q_PROPERTY(bool     enablePlanViewSelector      READ enablePlanViewSelector     CONSTANT)
-    Q_PROPERTY(CustomInstrumentWidget*  instrumentWidget READ instrumentWidget      CONSTANT)
-
+    Q_PROPERTY(bool                     combineSettingsAndSetup         READ combineSettingsAndSetup        CONSTANT)
+    Q_PROPERTY(double                   toolbarHeightMultiplier         READ toolbarHeightMultiplier        CONSTANT)
+    Q_PROPERTY(bool                     enablePlanViewSelector          READ enablePlanViewSelector         CONSTANT)
+    Q_PROPERTY(CustomInstrumentWidget*  instrumentWidget                READ instrumentWidget               CONSTANT)
+    Q_PROPERTY(bool                     showSensorCalibrationCompass    READ showSensorCalibrationCompass   NOTIFY showSensorCalibrationCompassChanged)
+    Q_PROPERTY(bool                     showSensorCalibrationGyro       READ showSensorCalibrationGyro      NOTIFY showSensorCalibrationGyroChanged)
+    Q_PROPERTY(bool                     showSensorCalibrationAccel      READ showSensorCalibrationAccel     NOTIFY showSensorCalibrationAccelChanged)
+    Q_PROPERTY(bool                     showSensorCalibrationLevel      READ showSensorCalibrationLevel     NOTIFY showSensorCalibrationLevelChanged)
+    Q_PROPERTY(bool                     showSensorCalibrationAirspeed   READ showSensorCalibrationAirspeed  NOTIFY showSensorCalibrationAirspeedChanged)
+    Q_PROPERTY(bool                     showSensorCalibrationOrient     READ showSensorCalibrationOrient    NOTIFY showSensorCalibrationOrientChanged)
 
     /// Should QGC hide its settings menu and colapse it into one single menu (Settings and Vehicle Setup)?
     /// @return true if QGC should consolidate both menus into one.
@@ -45,6 +50,22 @@ public:
     /// Provides an alternate instrument widget for the Fly View
     /// @return An alternate widget (see QGCInstrumentWidget.qml, the default widget)
     virtual CustomInstrumentWidget* instrumentWidget();
+
+    /// By returning false you can hide the following sensor calibration pages
+    bool showSensorCalibrationCompass   () const { return true; }
+    bool showSensorCalibrationGyro      () const { return true; }
+    bool showSensorCalibrationAccel     () const { return true; }
+    bool showSensorCalibrationLevel     () const { return true; }
+    bool showSensorCalibrationAirspeed  () const { return true; }
+    bool showSensorCalibrationOrient    () const { return true; }
+
+signals:
+    void showSensorCalibrationCompassChanged    (bool show);
+    void showSensorCalibrationGyroChanged       (bool show);
+    void showSensorCalibrationAccelChanged      (bool show);
+    void showSensorCalibrationLevelChanged      (bool show);
+    void showSensorCalibrationAirspeedChanged   (bool show);
+    void showSensorCalibrationOrientChanged     (bool show);
 
 private:
     CustomInstrumentWidget* _defaultInstrumentWidget;
