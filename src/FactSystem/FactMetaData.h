4b69460c81a2ace5288244523d15f10e109ff56d
DonLakeFlyer <don@thegagnes.com>
2017-04-18 10:41:19 -0700

    Allow translation of cm/px units

--- src/FactSystem/FactMetaData.h
+++ src/FactSystem/FactMetaData.h
@@ -175,6 +175,8 @@ private:
     static QVariant _knotsToMetersPerSecond(const QVariant& knots);
     static QVariant _percentToNorm(const QVariant& percent);
     static QVariant _normToPercent(const QVariant& normalized);
+    static QVariant _centimetersToInches(const QVariant& centimeters);
+    static QVariant _inchesToCentimeters(const QVariant& inches);
 
     struct AppSettingsTranslation_s {
         const char* rawUnits;
@@ -218,6 +220,7 @@ private:
         static const qreal knotsToKPH;
         static const qreal milesToMeters;
         static const qreal feetToMeters;
+        static const qreal inchesToCentimeters;
     } constants;
 
     struct BuiltInTranslation_s {
