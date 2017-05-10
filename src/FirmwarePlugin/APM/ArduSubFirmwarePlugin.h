eba10f0c7ceac2f5d23f45a9fc672f0bcb909596
Jacob Walser <jwalser90@gmail.com>
2017-05-10 18:05:25 -0400

    Add pilot gain to sub firmware facts

--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
@@ -110,16 +110,19 @@ public:
     Q_PROPERTY(Fact* tetherTurns   READ tetherTurns   CONSTANT)
     Q_PROPERTY(Fact* lightsLevel1  READ lightsLevel1  CONSTANT)
     Q_PROPERTY(Fact* lightsLevel2  READ lightsLevel2  CONSTANT)
+    Q_PROPERTY(Fact* pilotGain     READ pilotGain     CONSTANT)
 
     Fact* camTilt       (void) { return &_camTiltFact; }
     Fact* tetherTurns   (void) { return &_tetherTurnsFact; }
     Fact* lightsLevel1  (void) { return &_lightsLevel1Fact; }
     Fact* lightsLevel2  (void) { return &_lightsLevel2Fact; }
+    Fact* pilotGain     (void) { return &_pilotGainFact; }
 
     static const char* _camTiltFactName;
     static const char* _tetherTurnsFactName;
     static const char* _lightsLevel1FactName;
     static const char* _lightsLevel2FactName;
+    static const char* _pilotGainFactName;
 
     static const char* _settingsGroup;
 
@@ -128,5 +131,6 @@ private:
     Fact            _tetherTurnsFact;
     Fact            _lightsLevel1Fact;
     Fact            _lightsLevel2Fact;
+    Fact            _pilotGainFact;
 };
 #endif
