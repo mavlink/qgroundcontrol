787ca30fa80ba777728cdfab6bc3b9140b651369
Don Gagne <Don@laptop-3.local>
2017-06-28 19:35:44 -0700

    Allow RADIO_STATUS message to pass through to vehicle

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -464,9 +464,11 @@ void Vehicle::resetCounters()
 
 void Vehicle::_mavlinkMessageReceived(LinkInterface* link, mavlink_message_t message)
 {
-
     if (message.sysid != _id && message.sysid != 0) {
-        return;
+        // We allow RADIO_STATUS messages which come from a link the vehicle is using to pass through and be handled
+        if (!(message.msgid == MAVLINK_MSG_ID_RADIO_STATUS && _containsLink(link))) {
+            return;
+        }
     }
 
     if (!_containsLink(link)) {
