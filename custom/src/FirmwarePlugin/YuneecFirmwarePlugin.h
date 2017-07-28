f7ea8c2b91b652726afdaa7849e0b1344c86a350
Gus Grubba <mavlink@grubba.com>
2017-07-28 13:53:29 -0400

    Camera manager instance management Play beep only once when camera is initialized and ready Don't assume gimbal yaw position is -90

--- custom/src/FirmwarePlugin/YuneecFirmwarePlugin.h
+++ custom/src/FirmwarePlugin/YuneecFirmwarePlugin.h
@@ -32,12 +32,11 @@ public:
     void                batteryConsumptionData              (Vehicle* vehicle, int& mAhBattery, double& hoverAmps, double& cruiseAmps) const final;
     bool                vehicleYawsToNextWaypointInMission  (const Vehicle* vehicle) const final;
     QString             internalParameterMetaDataFile       (Vehicle* vehicle) override { Q_UNUSED(vehicle); return QString(":/typhoonh/YuneecParameterFactMetaData.xml"); }
-    QGCCameraManager*   cameraManager                       (Vehicle *vehicle) override final;
+    QGCCameraManager*   createCameraManager                 (Vehicle *vehicle) override final;
     QGCCameraControl*   createCameraControl                 (const mavlink_camera_information_t* info, Vehicle* vehicle, int compID, QObject* parent = NULL) override final;
 
 private:
     QVariantList            _toolBarIndicators;
-    YuneecCameraManager*    _cameraManager;
 
     static QVariantList _cameraList;        ///< Yuneec camera list
 };
