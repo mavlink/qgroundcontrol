0ee5212e6821e5b0c7db27187b9d66015a75d888
Dario Röthlisberger <dario.roethlisberger@gmail.com>
2017-07-31 14:09:42 +0200

    dockerbuild: add some debug output to test exit

--- custom/dockerbuild.sh
+++ custom/dockerbuild.sh
@@ -6,6 +6,8 @@ GSTSYMLINK=$ROOTDIR/../gstreamer-1.0-android-x86-1.5.2
 docker_repo=879326759580.dkr.ecr.eu-central-1.amazonaws.com/datapilot:latest
 $(aws ecr get-login --region eu-central-1 --no-include-email)
 docker pull $docker_repo
-docker run -i -v `dirname $ROOTDIR`:/home/docker1000/qgroundcontrol:rw -v /tmp/datapilot_build:/tmp/datapilot_build:rw $docker_repo
+docker run -t -v `dirname $ROOTDIR`:/home/docker1000/qgroundcontrol:rw -v /tmp/datapilot_build:/tmp/datapilot_build:rw $docker_repo
 [ -L $GSTSYMLINK ] && rm $GSTSYMLINK
+echo "Debug: dockerbuild.sh reaches this line (11) and will exit with exit 0"
 [ -L $GSTSYMLINK-orig ] && mv $GSTSYMLINK-orig $GSTSYMLINK
+echo "Debug: dockerbuild.sh reaches this line (13) and will exit with exit 0"
