48655f468733cf980b3aa85b791a7cb5b1417c9b
Dario Röthlisberger <dario.roethlisberger@gmail.com>
2017-07-31 13:20:37 +0200

    dockerbuild: fix 'the input device is not a TTY'

--- custom/dockerbuild.sh
+++ custom/dockerbuild.sh
@@ -6,6 +6,6 @@ GSTSYMLINK=$ROOTDIR/../gstreamer-1.0-android-x86-1.5.2
 docker_repo=879326759580.dkr.ecr.eu-central-1.amazonaws.com/datapilot:latest
 $(aws ecr get-login --region eu-central-1 --no-include-email)
 docker pull $docker_repo
-docker run -it -v `dirname $ROOTDIR`:/home/docker1000/qgroundcontrol:rw -v /tmp/datapilot_build:/tmp/datapilot_build:rw $docker_repo
+docker run -i -v `dirname $ROOTDIR`:/home/docker1000/qgroundcontrol:rw -v /tmp/datapilot_build:/tmp/datapilot_build:rw $docker_repo
 [ -L $GSTSYMLINK ] && rm $GSTSYMLINK
 [ -L $GSTSYMLINK-orig ] && mv $GSTSYMLINK-orig $GSTSYMLINK
