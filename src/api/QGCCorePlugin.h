e05bf7a75b6e98a621624fcb8767342dbe150eca
DonLakeFlyer <don@thegagnes.com>
2017-08-15 13:18:00 -0700

    QGCCorePlugin::customMapItems support
    Also rename QmlPageInfo to QmlComponentInfo to cover more use cases

--- src/api/QGCCorePlugin.h
+++ src/api/QGCCorePlugin.h
@@ -12,6 +12,7 @@
 #include "QGCToolbox.h"
 #include "QGCPalette.h"
 #include "QGCMAVLink.h"
+#include "QmlObjectListModel.h"
 
 #include <QObject>
 #include <QVariantList>
@@ -31,7 +32,7 @@ class QGeoPositionInfoSource;
 class QQmlApplicationEngine;
 class Vehicle;
 class LinkInterface;
-
+class QmlObjectListModel;
 class QGCCorePlugin : public QGCTool
 {
     Q_OBJECT
@@ -39,17 +40,16 @@ public:
     QGCCorePlugin(QGCApplication* app, QGCToolbox* toolbox);
     ~QGCCorePlugin();
 
-    Q_PROPERTY(QVariantList settingsPages       READ settingsPages      NOTIFY settingsPagesChanged)
-    Q_PROPERTY(QVariantList instrumentPages     READ instrumentPages    NOTIFY instrumentPagesChanged)
-    Q_PROPERTY(int          defaultSettings     READ defaultSettings    CONSTANT)
-    Q_PROPERTY(QGCOptions*  options             READ options            CONSTANT)
-
-    Q_PROPERTY(bool         showTouchAreas      READ showTouchAreas     WRITE setShowTouchAreas    NOTIFY showTouchAreasChanged)
-    Q_PROPERTY(bool         showAdvancedUI      READ showAdvancedUI     WRITE setShowAdvancedUI    NOTIFY showAdvancedUIChanged)
-    Q_PROPERTY(QString                  showAdvancedUIMessage           READ showAdvancedUIMessage          CONSTANT)
-
-    Q_PROPERTY(QString      brandImageIndoor    READ brandImageIndoor   CONSTANT)
-    Q_PROPERTY(QString      brandImageOutdoor   READ brandImageOutdoor  CONSTANT)
+    Q_PROPERTY(QVariantList         settingsPages           READ settingsPages                                  NOTIFY settingsPagesChanged)
+    Q_PROPERTY(QVariantList         instrumentPages         READ instrumentPages                                NOTIFY instrumentPagesChanged)
+    Q_PROPERTY(int                  defaultSettings         READ defaultSettings                                CONSTANT)
+    Q_PROPERTY(QGCOptions*          options                 READ options                                        CONSTANT)
+    Q_PROPERTY(bool                 showTouchAreas          READ showTouchAreas         WRITE setShowTouchAreas NOTIFY showTouchAreasChanged)
+    Q_PROPERTY(bool                 showAdvancedUI          READ showAdvancedUI         WRITE setShowAdvancedUI NOTIFY showAdvancedUIChanged)
+    Q_PROPERTY(QString              showAdvancedUIMessage   READ showAdvancedUIMessage                          CONSTANT)
+    Q_PROPERTY(QString              brandImageIndoor        READ brandImageIndoor                               CONSTANT)
+    Q_PROPERTY(QString              brandImageOutdoor       READ brandImageOutdoor                              CONSTANT)
+    Q_PROPERTY(QmlObjectListModel*  customMapItems          READ customMapItems                                 CONSTANT)
 
     /// The list of settings under the Settings Menu
     /// @return A list of QGCSettings
@@ -101,6 +101,10 @@ public:
     /// Allows the plugin to see all mavlink traffic to a vehicle
     /// @return true: Allow vehicle to continue processing, false: Vehicle should not process message
     virtual bool mavlinkMessage(Vehicle* vehicle, LinkInterface* link, mavlink_message_t message);
+
+    /// Allows custom builds to add custom items to the FlightMap. Objects put into QmlObjectListModel
+    /// should derive from QmlComponentInfo and set the url property.
+    virtual QmlObjectListModel* customMapItems(void);
 
     bool showTouchAreas(void) const { return _showTouchAreas; }
     bool showAdvancedUI(void) const { return _showAdvancedUI; }
