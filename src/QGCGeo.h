a1acb1d0c724adf5f5be93a2ab31264c933afbe2
DonLakeFlyer <don@thegagnes.com>
2017-12-13 10:05:31 -0800

    Support for detailed Geo and UTM position editing

--- src/QGCGeo.h
+++ src/QGCGeo.h
@@ -41,4 +41,44 @@ void convertGeoToNed(QGeoCoordinate coord, QGeoCoordinate origin, double* x, dou
  */
 void convertNedToGeo(double x, double y, double z, QGeoCoordinate origin, QGeoCoordinate *coord);
 
+// LatLonToUTMXY
+// Converts a latitude/longitude pair to x and y coordinates in the
+// Universal Transverse Mercator projection.
+//
+// Inputs:
+//   lat - Latitude of the point, in radians.
+//   lon - Longitude of the point, in radians.
+//   zone - UTM zone to be used for calculating values for x and y.
+//          If zone is less than 1 or greater than 60, the routine
+//          will determine the appropriate zone from the value of lon.
+//
+// Outputs:
+//   x - The x coordinate (easting) of the computed point. (in meters)
+//   y - The y coordinate (northing) of the computed point. (in meters)
+//
+// Returns:
+//   The UTM zone used for calculating the values of x and y.
+int convertGeoToUTM(const QGeoCoordinate& coord, double& easting, double& northing);
+
+// UTMXYToLatLon
+//
+// Converts x and y coordinates in the Universal Transverse Mercator//   The UTM zone parameter should be in the range [1,60].
+
+// projection to a latitude/longitude pair.
+//
+// Inputs:
+// x - The easting of the point, in meters.
+// y - The northing of the point, in meters.
+// zone - The UTM zone in which the point lies.
+// southhemi - True if the point is in the southern hemisphere;
+//               false otherwise.
+//
+// Outputs:
+// lat - The latitude of the point, in radians.
+// lon - The longitude of the point, in radians.
+//
+// Returns:
+// The function does not return a value.
+void convertUTMToGeo(double easting, double northing, int zone, bool southhemi, QGeoCoordinate& coord);
+
 #endif // QGCGEO_H
