566ff1f3a7e28fe7751e6e95c26e1252bed637ef
nanthony21 <nickexists@hotmail.com>
2018-01-21 14:15:13 -0600

    Fixed issue with JoystickAndroid:discover where a joystick that was uplugged would never be removed from the mapping of active joysticks. Hotplugging of joysticks now appears to be working perfectly.

--- src/Joystick/JoystickManager.cc
+++ src/Joystick/JoystickManager.cc
@@ -61,7 +61,7 @@ void JoystickManager::init() {
     }
 #elif defined(__android__)
     _setActiveJoystickFromSettings();
-    connect(QGamepadManager::instance(), &QGamepadManager::connectedGamepadsChanged, this, &JoystickManager::_updateAvailableJoysticks);
+    connect(QGamepadManager::instance(), &QGamepadManager::connectedGamepadsChanged, this, &JoystickManager::_setActiveJoystickFromSettings);
 #endif
 }
 
@@ -206,23 +206,5 @@ void JoystickManager::_updateAvailableJoysticks(void)
             break;
         }
     }
-#elif defined(__android__)
-    QList<int> gamepads = QGamepadManager::instance()->connectedGamepads();
-    static QList<int> previousGamepads;
-    if (gamepads != previousGamepads){
-        previousGamepads = gamepads;
-        qCDebug(JoystickManagerLog) << "GPChange!: "<<previousGamepads;
-        _setActiveJoystickFromSettings();
-    }
-    /*
-     * TODO: Investigate Android events for Joystick hot plugging
-     */
 #endif
-}
-void JoystickManager::androidGPConnected(int id){
-    qDebug()<<"conn";
-
-}
-void JoystickManager::androidGPDisconnected(int id){
-    qDebug()<< "disconn";
 }
