9235608c441fb64d503a4992934215b0d2aba1a9
Julian Oes <julian@oes.ch>
2018-01-23 17:32:20 +0100

    PlanManager: fix mission download progress
    By adding a member variable for the total mission item count, we can
    actually correctly estimate the progress.

--- src/MissionManager/PlanManager.cc
+++ src/MissionManager/PlanManager.cc
@@ -30,6 +30,7 @@ PlanManager::PlanManager(Vehicle* vehicle, MAV_MISSION_TYPE planType)
     , _transactionInProgress(TransactionNone)
     , _resumeMission(false)
     , _lastMissionRequest(-1)
+    , _missionItemCountToRead(-1)
     , _currentMissionIndex(-1)
     , _lastCurrentIndex(-1)
 {
@@ -317,6 +318,7 @@ void PlanManager::_handleMissionCount(const mavlink_message_t& message)
         for (int i=0; i<missionCount.count; i++) {
             _itemIndicesToRead << i;
         }
+        _missionItemCountToRead = missionCount.count;
         _requestNextMissionItem();
     }
 }
@@ -460,7 +462,7 @@ void PlanManager::_handleMissionItem(const mavlink_message_t& message, bool miss
         return;
     }
 
-    emit progressPct((double)seq / (double)_missionItems.count());
+    emit progressPct((double)seq / (double)_missionItemCountToRead);
     
     _retryCount = 0;
     if (_itemIndicesToRead.count() == 0) {
