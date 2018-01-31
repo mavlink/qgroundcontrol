d07b02471c661d64b601b2c493dcf3860589cd68
DonLakeFlyer <don@thegagnes.com>
2018-01-31 15:14:58 -0800

    Batch terrain requests

--- src/Terrain.h
+++ src/Terrain.h
@@ -9,47 +9,72 @@
 
 #pragma once
 
+#include "QGCLoggingCategory.h"
+
 #include <QObject>
 #include <QGeoCoordinate>
 #include <QNetworkAccessManager>
+#include <QTimer>
 
-/* usage example:
-    ElevationProvider *p = new ElevationProvider();
-    QList<QGeoCoordinate> coordinates;
-    QGeoCoordinate c(47.379243, 8.548265);
-    coordinates.push_back(c);
-    c.setLatitude(c.latitude()+0.01);
-    coordinates.push_back(c);
-    p->queryTerrainData(coordinates);
- */
+Q_DECLARE_LOGGING_CATEGORY(ElevationProviderLog)
 
+class ElevationProvider;
 
-class ElevationProvider : public QObject
-{
+/// Used internally by ElevationProvider to batch requests together
+class TerrainBatchManager : public QObject {
     Q_OBJECT
+
 public:
-    ElevationProvider(QObject* parent = NULL);
+    TerrainBatchManager(void);
 
-    /**
-     * Async elevation query for a list of lon,lat coordinates. When the query is done, the terrainData() signal
-     * is emitted.
-     * @param coordinates
-     * @return true on success
-     */
-    bool queryTerrainData(const QList<QGeoCoordinate>& coordinates);
-
-signals:
-    void terrainData(bool success, QList<float> altitudes);
+    void addQuery(ElevationProvider* elevationProvider, const QList<QGeoCoordinate>& coordinates);
 
 private slots:
-    void _requestFinished();
+    void _sendNextBatch     (void);
+    void _requestFinished   (void);
+
 private:
+    typedef struct {
+        ElevationProvider*      elevationProvider;
+        QList<QGeoCoordinate>   coordinates;
+    } QueuedRequestInfo_t;
+
+    typedef struct {
+        ElevationProvider*      elevationProvider;
+        int                     cCoord;
+    } SentRequestInfo_t;
+
 
     enum class State {
         Idle,
         Downloading,
     };
 
-    State                   _state = State::Idle;
-    QNetworkAccessManager   _networkManager;
+    void _batchFailed(void);
+
+    QList<QueuedRequestInfo_t>  _requestQueue;
+    QList<SentRequestInfo_t>    _sentRequests;
+    State                       _state = State::Idle;
+    QNetworkAccessManager       _networkManager;
+    const int                   _batchTimeout = 500;
+    QTimer                      _batchTimer;
+};
+
+/// NOTE: ElevationProvider is not thread safe. All instances/calls to ElevationProvider must be on main thread.
+class ElevationProvider : public QObject
+{
+    Q_OBJECT
+public:
+    ElevationProvider(QObject* parent = NULL);
+
+     /// Async elevation query for a list of lon,lat coordinates. When the query is done, the terrainData() signal
+     /// is emitted.
+     ///    @param coordinates to query
+    void queryTerrainData(const QList<QGeoCoordinate>& coordinates);
+
+    // Internal method
+    void _signalTerrainData(bool success, QList<float>& altitudes);
+
+signals:
+    void terrainData(bool success, QList<float> altitudes);
 };
