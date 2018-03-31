464766f33af6633893df12670fcf8c0408679edb
nanthony21 <nickexists@hotmail.com>
2018-03-30 20:16:25 -0500

    Separate startJoystick from enableJoystick. Just because the joystick is not enabled doesn't mean that we don't want to be able to monitor its behavior

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -1787,6 +1787,7 @@ void Vehicle::_loadSettings(void)
     // Joystick enabled is a global setting so first make sure there are any joysticks connected
     if (_toolbox->joystickManager()->joysticks().count()) {
         setJoystickEnabled(settings.value(_joystickEnabledSettingsKey, false).toBool());
+        _startJoystick(true);
     }
 }
 
@@ -1839,7 +1840,6 @@ bool Vehicle::joystickEnabled(void)
 void Vehicle::setJoystickEnabled(bool enabled)
 {
     _joystickEnabled = enabled;
-    _startJoystick(_joystickEnabled);
     _saveSettings();
     emit joystickEnabledChanged(_joystickEnabled);
 }
@@ -1849,9 +1849,7 @@ void Vehicle::_startJoystick(bool start)
     Joystick* joystick = _joystickManager->activeJoystick();
     if (joystick) {
         if (start) {
-            if (_joystickEnabled) {
-                joystick->startPolling(this);
-            }
+            joystick->startPolling(this);
         } else {
             joystick->stopPolling();
         }
