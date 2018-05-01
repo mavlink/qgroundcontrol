36db082c491b9cc5310094928b989177e6ad8fd0
Jacob Walser <jwalser90@gmail.com>
2018-05-01 10:10:00 -0400

    APM Sub: default attitude streamrate to 20Hz

--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
@@ -115,6 +115,8 @@ public:
 
     int defaultJoystickTXMode(void) final { return 3; }
 
+    void initializeStreamRates(Vehicle* vehicle) override final;
+
     bool supportsThrottleModeCenterZero(void) final;
 
     bool supportsRadio(void) final;
