acd18168df3613a01ec8beb584388ac6816d0178
Patrick José Pereira <patrickelectric@gmail.com>
2018-06-20 00:35:32 -0300

    Use Qt5 connect style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/QtLocationPlugin/QGeoCodeReplyQGC.cpp
+++ src/QtLocationPlugin/QGeoCodeReplyQGC.cpp
@@ -158,7 +158,7 @@ JasonMonger kMonger;
 QGeoCodeReplyQGC::QGeoCodeReplyQGC(QNetworkReply *reply, QObject *parent)
 :   QGeoCodeReply(parent), m_reply(reply)
 {
-    connect(m_reply, SIGNAL(finished()), this, SLOT(networkReplyFinished()));
+    connect(m_reply, &QNetworkReply::finished, this, &QGeoCodeReplyQGC::networkReplyFinished);
     connect(m_reply, SIGNAL(error(QNetworkReply::NetworkError)),
             this, SLOT(networkReplyError(QNetworkReply::NetworkError)));
 
