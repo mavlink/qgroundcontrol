c5f273172a4d4a2d3cb9a6454cfb54596c566620
Don Gagne <dongagne@outlook.com>
2018-08-28 16:51:27 -0700

    Support GuidedActionsController logging

--- src/QGCLoggingCategory.cc
+++ src/QGCLoggingCategory.cc
@@ -16,13 +16,14 @@
 #include <QSettings>
 
 // Add Global logging categories (not class specific) here using QGC_LOGGING_CATEGORY
-QGC_LOGGING_CATEGORY(FirmwareUpgradeLog,        "FirmwareUpgradeLog")
-QGC_LOGGING_CATEGORY(FirmwareUpgradeVerboseLog, "FirmwareUpgradeVerboseLog")
-QGC_LOGGING_CATEGORY(MissionCommandsLog,        "MissionCommandsLog")
-QGC_LOGGING_CATEGORY(MissionItemLog,            "MissionItemLog")
-QGC_LOGGING_CATEGORY(ParameterManagerLog,       "ParameterManagerLog")
-QGC_LOGGING_CATEGORY(GeotaggingLog,             "GeotaggingLog")
-QGC_LOGGING_CATEGORY(RTKGPSLog,                 "RTKGPSLog")
+QGC_LOGGING_CATEGORY(FirmwareUpgradeLog,            "FirmwareUpgradeLog")
+QGC_LOGGING_CATEGORY(FirmwareUpgradeVerboseLog,     "FirmwareUpgradeVerboseLog")
+QGC_LOGGING_CATEGORY(MissionCommandsLog,            "MissionCommandsLog")
+QGC_LOGGING_CATEGORY(MissionItemLog,                "MissionItemLog")
+QGC_LOGGING_CATEGORY(ParameterManagerLog,           "ParameterManagerLog")
+QGC_LOGGING_CATEGORY(GeotaggingLog,                 "GeotaggingLog")
+QGC_LOGGING_CATEGORY(RTKGPSLog,                     "RTKGPSLog")
+QGC_LOGGING_CATEGORY(GuidedActionsControllerLog,    "GuidedActionsControllerLog")
 
 QGCLoggingCategoryRegister* _instance = NULL;
 const char* QGCLoggingCategoryRegister::_filterRulesSettingsGroup = "LoggingFilters";
