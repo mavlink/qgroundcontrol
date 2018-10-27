8646ad9fc8cf363b6d8a36fc9f4de78f65519322
Patrick José Pereira <patrickelectric@gmail.com>
2018-10-27 11:46:27 -0300

    ViewWidgets: Move to pragma once
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/ViewWidgets/ViewWidgetController.h
+++ src/ViewWidgets/ViewWidgetController.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   (c) 2009-2016 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *   (c) 2009-2018 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
  *
  * QGroundControl is licensed according to the terms in the file
  * COPYING.md in the root of the source code directory.
@@ -8,8 +8,7 @@
  ****************************************************************************/
 
 
-#ifndef VIEWWIDGETCONTROLLER_H
-#define VIEWWIDGETCONTROLLER_H
+#pragma once
 
 #include <QObject>
 
@@ -38,4 +37,3 @@ private:
 	UASInterface*           _uas;
 };
 
-#endif
