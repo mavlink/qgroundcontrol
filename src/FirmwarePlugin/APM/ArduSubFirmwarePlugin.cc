abcfb9976c2ab97e3b2953d2e29dea358610e2d3
Willian Galvani <williangalvani@gmail.com>
2018-10-30 08:33:35 -0300

    ArduSubFirmwarePlugin: Add BATT_ARM_VOLT to remapper

--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.cc
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.cc
@@ -112,8 +112,8 @@ ArduSubFirmwarePlugin::ArduSubFirmwarePlugin(void):
 
         FirmwarePlugin::remapParamNameMap_t& remapV3_6 = _remapParamName[3][6];
 
-        remapV3_6["ARMING_VOLT_MIN"] = QStringLiteral("ARMING_MIN_VOLT");
-        remapV3_6["ARMING_VOLT2_MIN"] = QStringLiteral("ARMING_MIN_VOLT2");
+        remapV3_6["BATT_ARM_VOLT"] = QStringLiteral("ARMING_MIN_VOLT");
+        remapV3_6["BATT2_ARM_VOLT"] = QStringLiteral("ARMING_MIN_VOLT2");
         remapV3_6["BATT_AMP_PERVLT"] =  QStringLiteral("BATT_AMP_PERVOLT");
         remapV3_6["BATT2_AMP_PERVLT"] = QStringLiteral("BATT2_AMP_PERVOL");
         remapV3_6["BATT_LOW_MAH"] = QStringLiteral("FS_BATT_MAH");
