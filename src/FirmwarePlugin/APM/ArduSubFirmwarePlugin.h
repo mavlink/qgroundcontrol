1350c78d3a3f3be40322b5a4f9ab3b9c469663b6
Don Gagne <dongagne@outlook.com>
2018-11-17 13:04:53 -0800

    Don't starts streams from QGC side. Assume firmware side does the right thing.

--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
@@ -115,8 +115,6 @@ public:
 
     int defaultJoystickTXMode(void) final { return 3; }
 
-    void initializeStreamRates(Vehicle* vehicle) override final;
-
     bool isCapable(const Vehicle *vehicle, FirmwareCapabilities capabilities) final;
 
     bool supportsThrottleModeCenterZero(void) final;
