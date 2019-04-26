6fa13707ccb22ea66840daf5255de19f969f3d76
Don Gagne <dongagne@outlook.com>
2019-04-26 11:02:02 -0700

    Pattern Preset support

--- src/MissionManager/ComplexMissionItem.cc
+++ src/MissionManager/ComplexMissionItem.cc
@@ -8,11 +8,20 @@
  ****************************************************************************/
 
 #include "ComplexMissionItem.h"
+#include "QGCApplication.h"
+
+#include <QSettings>
 
 const char* ComplexMissionItem::jsonComplexItemTypeKey = "complexItemType";
 
+const char* ComplexMissionItem::_presetSettingsKey =        "_presets";
+const char* ComplexMissionItem::_presetNameKey =            "complexItemPresetName";
+const char* ComplexMissionItem::_saveCameraInPresetKey =    "complexItemCameraSavedInPreset";
+const char* ComplexMissionItem::_builtInPresetKey =         "complexItemBuiltInPreset";
+
 ComplexMissionItem::ComplexMissionItem(Vehicle* vehicle, bool flyView, QObject* parent)
-    : VisualMissionItem(vehicle, flyView, parent)
+    : VisualMissionItem         (vehicle, flyView, parent)
+    , _cameraInPreset           (true)
 {
 
 }
@@ -21,5 +30,112 @@ const ComplexMissionItem& ComplexMissionItem::operator=(const ComplexMissionItem
 {
     VisualMissionItem::operator=(other);
 
+    _cameraInPreset = other._cameraInPreset;
+
     return *this;
+}
+
+QStringList ComplexMissionItem::presetNames(void)
+{
+    QStringList names;
+
+    QSettings settings;
+
+    settings.beginGroup(presetsSettingsGroup());
+    settings.beginGroup(_presetSettingsKey);
+    return settings.childKeys();
+}
+
+void ComplexMissionItem::loadPreset(const QString& name)
+{
+    Q_UNUSED(name);
+    qgcApp()->showMessage(tr("This Pattern does not support Presets."));
+}
+
+void ComplexMissionItem::savePreset(const QString& name)
+{
+    Q_UNUSED(name);
+    qgcApp()->showMessage(tr("This Pattern does not support Presets."));
+}
+
+void ComplexMissionItem::clearCurrentPreset(void)
+{
+    _currentPreset.clear();
+    emit currentPresetChanged(_currentPreset);
+}
+
+void ComplexMissionItem::deleteCurrentPreset(void)
+{
+    qDebug() << "deleteCurrentPreset" << _currentPreset;
+    if (!_currentPreset.isEmpty()) {
+        QSettings settings;
+
+        settings.beginGroup(presetsSettingsGroup());
+        settings.beginGroup(_presetSettingsKey);
+        settings.remove(_currentPreset);
+        emit presetNamesChanged();
+
+        clearCurrentPreset();
+    }
+}
+
+void ComplexMissionItem::_savePresetJson(const QString& name, QJsonObject& presetObject)
+{
+    presetObject[_presetNameKey] = name;
+
+    QSettings settings;
+    settings.beginGroup(presetsSettingsGroup());
+    settings.beginGroup(_presetSettingsKey);
+    settings.setValue(name, QJsonDocument(presetObject).toBinaryData());
+    emit presetNamesChanged();
+
+    _currentPreset = name;
+    emit currentPresetChanged(name);
+}
+
+QJsonObject ComplexMissionItem::_loadPresetJson(const QString& name)
+{
+    QSettings settings;
+    settings.beginGroup(presetsSettingsGroup());
+    settings.beginGroup(_presetSettingsKey);
+    return QJsonDocument::fromBinaryData(settings.value(name).toByteArray()).object();
+}
+
+void ComplexMissionItem::_saveItem(QJsonObject& saveObject)
+{
+    qDebug() << "_saveItem" << _cameraInPreset;
+    saveObject[_presetNameKey] =            _currentPreset;
+    saveObject[_saveCameraInPresetKey] =    _cameraInPreset;
+    saveObject[_builtInPresetKey] =         _builtInPreset;
+}
+
+void ComplexMissionItem::_loadItem(const QJsonObject& saveObject)
+{
+    _currentPreset =    saveObject[_presetNameKey].toString();
+    _cameraInPreset =   saveObject[_saveCameraInPresetKey].toBool(false);
+    _builtInPreset =    saveObject[_builtInPresetKey].toBool(false);
+
+    if (!presetNames().contains(_currentPreset)) {
+        _currentPreset.clear();
+    }
+
+    emit cameraInPresetChanged  (_cameraInPreset);
+    emit currentPresetChanged   (_currentPreset);
+    emit builtInPresetChanged   (_builtInPreset);
+}
+
+void ComplexMissionItem::setCameraInPreset(bool cameraInPreset)
+{
+    if (cameraInPreset != _cameraInPreset) {
+        _cameraInPreset = cameraInPreset;
+        emit cameraInPresetChanged(_cameraInPreset);
+    }
+}
+
+void ComplexMissionItem::setBuiltInPreset(bool builtInPreset)
+{
+    if (builtInPreset != _builtInPreset) {
+        _builtInPreset = builtInPreset;
+        emit builtInPresetChanged(_builtInPreset);
+    }
 }
