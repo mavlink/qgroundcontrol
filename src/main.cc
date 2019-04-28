c2ccfda072c0d760c9480a95ee5447ed09be2f45
Gus Grubba <gus@auterion.com>
2019-04-28 12:22:11 -0400

    Allow plugin control of map scale visibility

--- src/main.cc
+++ src/main.cc
@@ -228,6 +228,7 @@ int main(int argc, char *argv[])
 #endif
 #endif // QT_DEBUG
 
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QGCApplication* app = new QGCApplication(argc, argv, runUnitTests);
     Q_CHECK_PTR(app);
 
