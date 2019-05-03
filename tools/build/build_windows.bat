b576dd8d6fb7a240279da4f5f410472456712302
Stefan Dunca <stefan@auterion.com>
2019-05-03 17:40:37 +0200

    Fix overlapping builds

--- tools/build/build_windows.bat
+++ tools/build/build_windows.bat
@@ -1,18 +1,29 @@
-:: echo off
+echo on
 
 :: Populate VS2015 script variables
 call "%VC_VARS_SCRIPT%" x86
 
-mkdir build
-cd build
-qmake -r "%WORKSPACE%/qgroundcontrol.pro" -spec win32-msvc CONFIG+="%QGC_CONFIG%" 
+SETLOCAL
+
+if not "%1" == "" (
+    set TARGET_DIR=%1
+) else (
+    set TARGET_DIR=release
+)
+
+if not "%2" == "" (
+    set BUILD_DIR=%2
+) else (
+    set BUILD_DIR=build
+)
+
+if not exist ".\%BUILD_DIR%\" mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+qmake -r "%WORKSPACE%/qgroundcontrol.pro" -spec win32-msvc CONFIG+="%QGC_CONFIG%"
 if %errorlevel%==0 (
     nmake
 ) else (
     echo "FATAL: Failed to build. qmake returned error level: %errorlevel%"
 )
-if %errorlevel%==0 (
-    copy /Y .\release\*-installer.exe ..\
-) else (
-    echo "WARNING: Failed to copy the installer output. copy returned error level: %errorlevel%"
-)
+
+ENDLOCAL
