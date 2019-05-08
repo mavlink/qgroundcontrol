914e2e08255e3c3ccf4d8a594dd929215d44a37c
Don Gagne <dongagne@outlook.com>
2019-05-08 14:15:14 -0700

    Switch to using firmware manifest for ArduPilot flashing

--- src/Settings/SettingsManager.cc
+++ src/Settings/SettingsManager.cc
@@ -27,6 +27,7 @@ SettingsManager::SettingsManager(QGCApplication* app, QGCToolbox* toolbox)
     , _planViewSettings             (nullptr)
     , _brandImageSettings           (nullptr)
     , _offlineMapsSettings          (nullptr)
+    , _firmwareUpgradeSettings      (nullptr)
 #if !defined(NO_ARDUPILOT_DIALECT)
     , _apmMavlinkStreamRateSettings (nullptr)
 #endif
@@ -40,20 +41,21 @@ void SettingsManager::setToolbox(QGCToolbox *toolbox)
     QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
     qmlRegisterUncreatableType<SettingsManager>("QGroundControl.SettingsManager", 1, 0, "SettingsManager", "Reference only");
 
-    _unitsSettings =                new UnitsSettings       (this);        // Must be first since AppSettings references it
-    _appSettings =                  new AppSettings         (this);
-    _autoConnectSettings =          new AutoConnectSettings (this);
-    _videoSettings =                new VideoSettings       (this);
-    _flightMapSettings =            new FlightMapSettings   (this);
-    _rtkSettings =                  new RTKSettings         (this);
-    _flyViewSettings =              new FlyViewSettings     (this);
-    _planViewSettings =             new PlanViewSettings    (this);
-    _brandImageSettings =           new BrandImageSettings  (this);
-    _offlineMapsSettings =          new OfflineMapsSettings (this);
+    _unitsSettings =                new UnitsSettings           (this);        // Must be first since AppSettings references it
+    _appSettings =                  new AppSettings             (this);
+    _autoConnectSettings =          new AutoConnectSettings     (this);
+    _videoSettings =                new VideoSettings           (this);
+    _flightMapSettings =            new FlightMapSettings       (this);
+    _rtkSettings =                  new RTKSettings             (this);
+    _flyViewSettings =              new FlyViewSettings         (this);
+    _planViewSettings =             new PlanViewSettings        (this);
+    _brandImageSettings =           new BrandImageSettings      (this);
+    _offlineMapsSettings =          new OfflineMapsSettings     (this);
+    _firmwareUpgradeSettings =      new FirmwareUpgradeSettings (this);
 #if !defined(NO_ARDUPILOT_DIALECT)
-    _apmMavlinkStreamRateSettings = new APMMavlinkStreamRateSettings     (this);
+    _apmMavlinkStreamRateSettings = new APMMavlinkStreamRateSettings(this);
 #endif
 #if defined(QGC_AIRMAP_ENABLED)
-    _airMapSettings =               new AirMapSettings      (this);
+    _airMapSettings =               new AirMapSettings          (this);
 #endif
 }
