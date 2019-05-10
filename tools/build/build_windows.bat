72725bf614c7be6762801b765ef3b0dad0613c0c
Gus Grubba <gus@auterion.com>
2019-05-10 17:42:26 -0400

    Windows CI build

--- tools/build/build_windows.bat
+++ tools/build/build_windows.bat
@@ -1,5 +1,12 @@
 :: echo off
 
+::
+:: Script assumes that the right QT bin folder is in the PATH env var
+::
+
+echo Before calling scripts
+:: Prepare the environment for windeployqt
+call qtenv2.bat
 :: Populate VS2015 script variables
 call "%VC_VARS_SCRIPT%" x86
 
@@ -17,11 +24,17 @@ if not "%2" == "" (
     set BUILD_DIR=build
 )
 
+:: These batch scripts change the current directory
+cd /d %WORKSPACE%
+echo After calling scripts
+cd
+
 if not exist ".\%BUILD_DIR%\" mkdir %BUILD_DIR%
 cd %BUILD_DIR%
-qmake -r "%WORKSPACE%/qgroundcontrol.pro" -spec win32-msvc CONFIG+="%QGC_CONFIG%"
+qmake -r "%WORKSPACE%\qgroundcontrol.pro" -spec win32-msvc CONFIG+="%QGC_CONFIG%"
 if %errorlevel%==0 (
-    nmake
+    dir
+    C:\Qt\Tools\QtCreator\bin\jom /f Makefile
 ) else (
     echo "FATAL: Failed to build. qmake returned error level: %errorlevel%"
 )
