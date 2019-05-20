84e5b4cdf7949e1f5b86d0fb5e6a2903bd7ee39f
Gus Grubba <gus@auterion.com>
2019-05-20 21:06:08 +0200

    Load Korean fonts when needed

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -391,12 +391,6 @@ void QGCApplication::setLanguage()
             break;
         case 12:
             locale = QLocale(QLocale::Korean);
-            if(QFontDatabase::addApplicationFont(":/fonts/NanumGothic-Regular") < 0) {
-                qWarning() << "Could not load /fonts/NanumGothic-Regular font";
-            }
-            if(QFontDatabase::addApplicationFont(":/fonts/NanumGothic-Bold") < 0) {
-                qWarning() << "Could not load /fonts/NanumGothic-Bold font";
-            }
             break;
         case 13:
             locale = QLocale(QLocale::Norwegian);
@@ -419,6 +413,16 @@ void QGCApplication::setLanguage()
         case 19:
             locale = QLocale(QLocale::Turkish);
             break;
+        }
+    }
+    //-- We have specific fonts for Korean
+    if(locale == QLocale::Korean) {
+        qDebug() << "Loading Korean fonts" << locale.name();
+        if(QFontDatabase::addApplicationFont(":/fonts/NanumGothic-Regular") < 0) {
+            qWarning() << "Could not load /fonts/NanumGothic-Regular font";
+        }
+        if(QFontDatabase::addApplicationFont(":/fonts/NanumGothic-Bold") < 0) {
+            qWarning() << "Could not load /fonts/NanumGothic-Bold font";
         }
     }
     qDebug() << "Loading localization for" << locale.name();
