979a3f0fe4741c6f23e236fb8eba705b6dbd8e03
Patrick José Pereira <patrickelectric@gmail.com>
2019-06-27 15:36:42 -0300

    UASMessageHandler: Remove QString constructor with tr
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/uas/UASMessageHandler.cc
+++ src/uas/UASMessageHandler.cc
@@ -153,31 +153,31 @@ void UASMessageHandler::handleTextMessage(int, int compId, int severity, QString
     switch (severity)
     {
     case MAV_SEVERITY_EMERGENCY:
-        severityText = QString(tr(" EMERGENCY:"));
+        severityText = tr(" EMERGENCY:");
         break;
     case MAV_SEVERITY_ALERT:
-        severityText = QString(tr(" ALERT:"));
+        severityText = tr(" ALERT:");
         break;
     case MAV_SEVERITY_CRITICAL:
-        severityText = QString(tr(" Critical:"));
+        severityText = tr(" Critical:");
         break;
     case MAV_SEVERITY_ERROR:
-        severityText = QString(tr(" Error:"));
+        severityText = tr(" Error:");
         break;
     case MAV_SEVERITY_WARNING:
-        severityText = QString(tr(" Warning:"));
+        severityText = tr(" Warning:");
         break;
     case MAV_SEVERITY_NOTICE:
-        severityText = QString(tr(" Notice:"));
+        severityText = tr(" Notice:");
         break;
     case MAV_SEVERITY_INFO:
-        severityText = QString(tr(" Info:"));
+        severityText = tr(" Info:");
         break;
     case MAV_SEVERITY_DEBUG:
-        severityText = QString(tr(" Debug:"));
+        severityText = tr(" Debug:");
         break;
     default:
-        severityText = QString(tr(""));
+        severityText = tr("");
         break;
     }
 
