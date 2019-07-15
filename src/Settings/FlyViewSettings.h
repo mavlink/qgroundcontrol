7bb9746688c2998f7dbaabebe65131dc83b6d8b2
Pierre TILAK <pierre.tilak@aeronavics.com>
2019-07-15 14:15:04 +1200

    Add setting to enable COG/HOME Compass

--- src/Settings/FlyViewSettings.h
+++ src/Settings/FlyViewSettings.h
@@ -23,4 +23,6 @@ public:
     DEFINE_SETTINGFACT(guidedMaximumAltitude)
     DEFINE_SETTINGFACT(showLogReplayStatusBar)
     DEFINE_SETTINGFACT(alternateInstrumentPanel)
+    DEFINE_SETTINGFACT(showHomeHeadingCompass)
+    DEFINE_SETTINGFACT(showCOGAngleCompass)
 };
