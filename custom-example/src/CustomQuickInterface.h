ece3fd4ac863f16607b5773d5df09911c17c2a09
Stefan Dunca <stefan@auterion.com>
2019-08-13 12:30:39 +0200

    custom-example: add showAttitudeWidget as a persistent option

--- custom-example/src/CustomQuickInterface.h
+++ custom-example/src/CustomQuickInterface.h
@@ -29,12 +29,20 @@ public:
     CustomQuickInterface(QObject* parent = nullptr);
     ~CustomQuickInterface();
     Q_PROPERTY(bool     showGimbalControl   READ showGimbalControl  WRITE setShowGimbalControl  NOTIFY showGimbalControlChanged)
+    Q_PROPERTY(bool         showAttitudeWidget  READ    showAttitudeWidget WRITE setShowAttitudeWidget NOTIFY showAttitudeWidgetChanged)
 
     bool    showGimbalControl           () { return _showGimbalControl; }
     void    setShowGimbalControl        (bool set);
     void    init                        ();
+
+    bool    showAttitudeWidget      () { return _showAttitudeWidget; }
+    void    setShowAttitudeWidget   (bool set);
+
 signals:
     void    showGimbalControlChanged    ();
+    void    showAttitudeWidgetChanged();
+
 private:
     bool    _showGimbalControl  = true;
+    bool _showAttitudeWidget = false;
 };
