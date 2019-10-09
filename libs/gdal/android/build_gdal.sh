e0600d4a6f5d47437a90675f78ab325794b50a98
Pierre TILAK <pierre.tilak@aeronavics.com>
2019-10-09 15:11:10 +1300

    Update methods to compile android gdal

--- libs/gdal/android/build_gdal.sh
+++ libs/gdal/android/build_gdal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # How to run
-# docker run -v/PATH-TO-SCRIPTS-FOLDER/scripts/:/scripts --rm -it mavlink/qgc_android /scripts/build_gdal.sh
-# This will create a ".libs" folder in scripts directory containing libgdal
+# docker run -v ${PWD}:/scripts --rm -it mavlink/qgc_android /scripts/build_gdal.sh
+# This will buid the libs in the current folder
 
 apt update
 apt install -y python pkg-config automake
@@ -20,7 +20,88 @@ git apply /scripts/gdal.patch
 
 ./autogen.sh
 
-CC="arm-linux-androideabi-clang" CXX="arm-linux-androideabi-clang++" CFLAGS="-mthumb -D__ANDROID_API__=21 " CXXFLAGS="-mthumb -D__ANDROID_API__=21" LIBS=" -lsupc++ -lstdc++ -l/scripts/libcrypto.so -l/scripts/libssl.so " ./configure --host=arm-linux-androideabi --without-grib --prefix=$PROJECT/external/gdal 
+CC="arm-linux-androideabi-clang" CXX="arm-linux-androideabi-clang++" CFLAGS="-mthumb -D__ANDROID_API__=21 " CXXFLAGS="-mthumb -D__ANDROID_API__=21" LIBS=" -lsupc++ -lstdc++ -l/scripts/libcrypto.so -l/scripts/libssl.so " ./configure --host=arm-linux-androideabi \
+ --prefix=$PROJECT/external/gdal \
+ --with-libtiff=internal \
+ --with-geotiff=internal \
+ --with-libjson-c=internal \
+ --with-hide-internal-symbols=yes \
+ --with-threads  \
+ --with-libz=internal  \
+ --with-geos \
+ --without-sse  \
+ --without-ssse3  \
+ --without-avx \
+ --without-liblzma  \
+ --without-pg  \
+ --without-grass  \
+ --without-libgrass  \
+ --without-cfitsio  \
+ --without-pcraster  \
+ --without-png \
+ --without-dds \
+ --without-gta \
+ --without-pcidsk \
+ --without-jpeg      \
+ --without-gif  \
+ --without-ogdi      \
+ --without-fme        \
+ --without-sosi         \
+ --without-mongocxx        \
+ --without-boost-lib-path   \
+ --without-hdf4      \
+ --without-hdf5      \
+ --without-kea       \
+ --without-netcdf    \
+ --without-jasper    \
+ --without-openjpeg  \
+ --without-fgdb      \
+ --without-ecw       \
+ --without-kakadu    \
+ --without-mrsid       \
+ --without-jp2mrsid    \
+ --without-mrsid_lidar  \
+ --without-msg           \
+ --without-bsb        \
+ --without-oci        \
+ --without-grib           \
+ --without-gnm             \
+ --without-mysql  \
+ --without-ingres \
+ --without-xerces \
+ --without-expat  \
+ --without-libkml  \
+ --without-odbc \
+ --without-curl \
+ --without-xml2 \
+ --without-spatialite \
+ --without-sqlite3 \
+ --without-rasterlite2 \
+ --without-pcre \
+ --without-teigha  \
+ --without-idb \
+ --without-sde \
+ --without-epsilon \
+ --without-webp \
+ --without-sfcgal \
+ --without-qhull \
+ --without-opencl \
+ --without-freexl \
+ --without-pam      \
+ --without-poppler  \
+ --without-podofo \
+ --without-pdfium \
+ --without-static-proj4 \
+ --without-macosx-framework    \
+ --without-perl            \
+ --without-python \
+ --without-java     \
+ --without-mdb       \
+ --without-jvm-lib \
+ --without-rasdaman   \
+ --without-armadillo  \
+ --without-cryptopp   \
+ --without-mrf
 
 make -j8
 make install
