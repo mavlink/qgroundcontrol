b2ce8b31ba466a0ea04f9c00aa7011daaaf33f02
Tomaz Canabrava <tcanabrava@kde.org>
2019-10-17 16:57:24 +0200

    Remove Shutdown Video Streaming
    This is uneeded if we follow the GStreamer documentation

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -452,8 +452,6 @@ void QGCApplication::_shutdown()
 {
     // Close out all Qml before we delete toolbox. This way we don't get all sorts of null reference complaints from Qml.
     delete _qmlAppEngine;
-
-    shutdownVideoStreaming();
     delete _toolbox;
 }
 
