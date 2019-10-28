65bcda55793b3efc93b32787b0ecca883b1a1660
Matej Frančeškin <matej@auterion.com>
2019-10-28 17:21:43 +0100

    Addressing review comments

--- src/PairingManager/openssl_rsa.cpp
+++ src/PairingManager/openssl_rsa.cpp
@@ -8,7 +8,6 @@
 #include <openssl/bn.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
-#include <openssl/engine.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
 #include <openssl/sha.h>
@@ -46,36 +45,15 @@ OpenSSL_RSA::random_string(uint length)
         "abcdefghijklmnopqrstuvwxyz";
     const int max_index = (sizeof(charset) - 1);
 
-    unsigned int seed = static_cast<unsigned int>(time(nullptr));
     std::string str(length, 0);
-
-    // Hardware random generator setup
-    ENGINE_load_rdrand();
-    ENGINE *eng = ENGINE_by_id("rdrand");
-    if (eng != nullptr && ENGINE_init(eng)) {
-        ENGINE_set_default(eng, ENGINE_METHOD_RAND);
-    }
-    // If platform doesn't support hardware random generator
-    // cryptographically strong software random generator will be used
     std::unique_ptr<unsigned char[]> buffer(new unsigned char[length]);
-    RAND_seed(&seed, sizeof(seed));
     if (RAND_bytes(buffer.get(), static_cast<int>(length)) == 1) {
         for (unsigned int i = 0; i < length; i++) {
             str[i] = charset[buffer.get()[i] % max_index];
         }
     } else {
-        // RAND_bytes failed, use weak method
-        srand(seed);
-        auto randchar = [charset]() -> char
-        {
-            return charset[rand() % max_index];
-        };
-        std::generate_n(str.begin(), length, randchar);
+        exit(-1);
     }
-
-    ENGINE_finish(eng);
-    ENGINE_free(eng);
-    ENGINE_cleanup();
 
     return str;
 }
