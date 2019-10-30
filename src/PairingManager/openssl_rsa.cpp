0e13d087023ff1ed615795f47fa9c38d94cb007d
Matej Frančeškin <Matej@auterion.com>
2019-10-30 14:22:52 +0100

    Temporarily disable signature verification

--- src/PairingManager/openssl_rsa.cpp
+++ src/PairingManager/openssl_rsa.cpp
@@ -238,7 +238,8 @@ OpenSSL_RSA::verify(std::string message, std::string signature)
     unsigned int signLen = static_cast<unsigned int>(sig.size());
     SHA256(reinterpret_cast<const unsigned char *>(message.c_str()), message.length() + 1, hash);
     int rc = RSA_verify(NID_sha256, hash, SHA256_DIGEST_LENGTH, sig.data(), signLen, _rsa_public.get());
-    return (rc == 1);
+    return true;
+    //return (rc == 1);
 }
 
 //-----------------------------------------------------------------------------
