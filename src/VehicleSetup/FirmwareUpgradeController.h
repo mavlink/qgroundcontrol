5ca5f7ae1e40f55ae242f3ebb11627cc302058d9
Travis Bottalico <travis@modalai.com>
2019-12-04 11:36:35 -0800

    Add support for ModalAI FC v1 FW Update

--- src/VehicleSetup/FirmwareUpgradeController.h
+++ src/VehicleSetup/FirmwareUpgradeController.h
@@ -220,6 +220,7 @@ private:
     QHash<FirmwareIdentifier, QString> _rgKakuteF7Firmware;
     QHash<FirmwareIdentifier, QString> _rgDurandalV1Firmware;
     QHash<FirmwareIdentifier, QString> _rgFMUK66V3Firmware;
+    QHash<FirmwareIdentifier, QString> _rgModalFCV1Firmware;
     QHash<FirmwareIdentifier, QString> _rgPX4FLowFirmware;
     QHash<FirmwareIdentifier, QString> _rg3DRRadioFirmware;
 
