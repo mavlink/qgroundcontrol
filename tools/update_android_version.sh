e0b924253e369593473611d81cb81295d5486e27
Don Gagne <dongagne@outlook.com>
2019-12-22 13:08:18 -0800

    Version code fix

--- tools/update_android_version.sh
+++ tools/update_android_version.sh
@@ -9,6 +9,12 @@ VERSIONCODE=$(($(($minor*10000)) + $VERSIONCODE))
 VERSIONCODE=$(($(($patch*1000)) + $VERSIONCODE))
 VERSIONCODE=$(($(($dev)) + $VERSIONCODE))
 
+# The android versionCode is for the entire package. It is the same for the 32 and 64 bit APKs.
+# At one point it was thought the versionCode was specific to APK. Hence the 32/64 bitness was
+# included as a prefix. That was incorrect. But now we are stuck with version codes starting with
+# a prefix of 64.
+VERSIONCODE=64$VERSIONCODE
+
 MANIFEST_FILE=android/AndroidManifest.xml
 
 # manifest package
