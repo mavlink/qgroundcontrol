13000a24b97c87f8180403babd3b0c1167a65d4f
DoinLakeFlyer <don@thegagnes.com>
2020-02-25 13:59:56 -0800

    Remove some more Follow stuff

--- src/FirmwarePlugin/APM/ArduRoverFirmwarePlugin.cc
+++ src/FirmwarePlugin/APM/ArduRoverFirmwarePlugin.cc
@@ -22,7 +22,10 @@ APMRoverMode::APMRoverMode(uint32_t mode, bool settable)
         {STEERING,       "Steering"},
         {HOLD,           "Hold"},
         {LOITER,         "Loiter"},
+#if 0
+    // Follow me not ready for Stable
         {FOLLOW,         "Follow"},
+#endif
         {SIMPLE,         "Simple"},
         {AUTO,           "Auto"},
         {RTL,            "RTL"},
@@ -40,7 +43,10 @@ ArduRoverFirmwarePlugin::ArduRoverFirmwarePlugin(void)
         APMRoverMode(APMRoverMode::STEERING     ,true),
         APMRoverMode(APMRoverMode::HOLD         ,true),
         APMRoverMode(APMRoverMode::LOITER       ,true),
+#if 0
+    // Follow me not ready for Stable
         APMRoverMode(APMRoverMode::FOLLOW       ,true),
+#endif
         APMRoverMode(APMRoverMode::SIMPLE       ,true),
         APMRoverMode(APMRoverMode::AUTO         ,true),
         APMRoverMode(APMRoverMode::RTL          ,true),
