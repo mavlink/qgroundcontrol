3e3b90875a0e8e3a72c14d3823a29ec4b721115e
DoinLakeFlyer <don@thegagnes.com>
2020-04-16 11:14:19 -0700

    Hack out missing method

--- src/VideoManager/VideoManager.cc
+++ src/VideoManager/VideoManager.cc
@@ -427,7 +427,7 @@ VideoManager::_tcpUrlChanged()
 void
 VideoManager::_lowLatencyModeChanged()
 {
-    restartVideo();
+    //restartVideo();
 }
 
 //-----------------------------------------------------------------------------
