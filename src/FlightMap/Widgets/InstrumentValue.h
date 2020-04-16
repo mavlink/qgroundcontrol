ef1911ddd58df329411b27230bbe343e94caf8cd
DonLakeFlyer <don@thegagnes.com>
2020-04-15 20:40:00 -0700

    Refactor instrument value controls

--- src/FlightMap/Widgets/InstrumentValue.h
+++ src/FlightMap/Widgets/InstrumentValue.h
@@ -28,11 +28,11 @@ public:
     };
     Q_ENUMS(FontSize)
 
-    enum IconPosition {
-        IconAbove = 0,
-        IconLeft,
+    enum LabelPosition {
+        LabelAbove = 0,
+        LabelLeft,
     };
-    Q_ENUMS(IconPosition)
+    Q_ENUMS(LabelPosition)
 
     enum RangeType {
         NoRangeInfo = 0,
@@ -44,28 +44,28 @@ public:
 
     InstrumentValue(Vehicle* activeVehicle, FontSize fontSize, QmlObjectListModel* rowModel);
 
-    Q_PROPERTY(QStringList  factGroupNames      MEMBER  _factGroupNames                             NOTIFY factGroupNamesChanged)
-    Q_PROPERTY(QStringList  factValueNames      MEMBER  _factValueNames                             NOTIFY factValueNamesChanged)
-    Q_PROPERTY(QString      factGroupName       MEMBER  _factGroupName                              NOTIFY factGroupNameChanged)
-    Q_PROPERTY(QString      factName            MEMBER  _factName                                   NOTIFY factNameChanged)
-    Q_PROPERTY(Fact*        fact                READ    fact                                        NOTIFY factChanged)
-    Q_PROPERTY(QString      label               READ    label               WRITE setLabel          NOTIFY labelChanged)
-    Q_PROPERTY(QString      icon                READ    icon                WRITE setIcon           NOTIFY iconChanged)             ///< If !isEmpty icon will be show instead of label
-    Q_PROPERTY(IconPosition iconPosition        READ    iconPosition        WRITE setIconPosition   NOTIFY iconPositionChanged)
-    Q_PROPERTY(QStringList  iconPositionNames   MEMBER _iconPositionNames                           CONSTANT)
-    Q_PROPERTY(QStringList  iconNames           MEMBER _iconNames                                   CONSTANT)
-    Q_PROPERTY(FontSize     fontSize            READ    fontSize            WRITE setFontSize       NOTIFY fontSizeChanged)
-    Q_PROPERTY(QStringList  fontSizeNames       MEMBER _fontSizeNames                               CONSTANT)
-    Q_PROPERTY(bool         showUnits           READ    showUnits           WRITE setShowUnits      NOTIFY showUnitsChanged)
-    Q_PROPERTY(QStringList  rangeTypeNames      MEMBER _rangeTypeNames                              CONSTANT)
-    Q_PROPERTY(RangeType    rangeType           READ    rangeType           WRITE setRangeType      NOTIFY rangeTypeChanged)
-    Q_PROPERTY(QVariantList rangeValues         READ    rangeValues         WRITE setRangeValues    NOTIFY rangeValuesChanged)
-    Q_PROPERTY(QVariantList rangeColors         READ    rangeColors         WRITE setRangeColors    NOTIFY rangeColorsChanged)
-    Q_PROPERTY(QVariantList rangeIcons          READ    rangeIcons          WRITE setRangeIcons     NOTIFY rangeIconsChanged)
-    Q_PROPERTY(QVariantList rangeOpacities      READ    rangeOpacities      WRITE setRangeOpacities NOTIFY rangeOpacitiesChanged)
-    Q_PROPERTY(QColor       currentColor        MEMBER _currentColor                                NOTIFY currentColorChanged)
-    Q_PROPERTY(double       currentOpacity      MEMBER _currentOpacity                              NOTIFY currentOpacityChanged)
-    Q_PROPERTY(QString      currentIcon         MEMBER _currentIcon                                 NOTIFY currentIconChanged)
+    Q_PROPERTY(QStringList      factGroupNames      MEMBER  _factGroupNames                             NOTIFY factGroupNamesChanged)
+    Q_PROPERTY(QStringList      factValueNames      MEMBER  _factValueNames                             NOTIFY factValueNamesChanged)
+    Q_PROPERTY(QString          factGroupName       MEMBER  _factGroupName                              NOTIFY factGroupNameChanged)
+    Q_PROPERTY(QString          factName            MEMBER  _factName                                   NOTIFY factNameChanged)
+    Q_PROPERTY(Fact*            fact                READ    fact                                        NOTIFY factChanged)
+    Q_PROPERTY(QString          text                READ    text                WRITE setText           NOTIFY textChanged)
+    Q_PROPERTY(QString          icon                READ    icon                WRITE setIcon           NOTIFY iconChanged)             ///< If !isEmpty icon will be show instead of label
+    Q_PROPERTY(LabelPosition    labelPosition       READ    labelPosition       WRITE setLabelPosition  NOTIFY labelPositionChanged)
+    Q_PROPERTY(QStringList      labelPositionNames  MEMBER _labelPositionNames                          CONSTANT)
+    Q_PROPERTY(QStringList      iconNames           MEMBER _iconNames                                   CONSTANT)
+    Q_PROPERTY(FontSize         fontSize            READ    fontSize            WRITE setFontSize       NOTIFY fontSizeChanged)
+    Q_PROPERTY(QStringList      fontSizeNames       MEMBER _fontSizeNames                               CONSTANT)
+    Q_PROPERTY(bool             showUnits           READ    showUnits           WRITE setShowUnits      NOTIFY showUnitsChanged)
+    Q_PROPERTY(QStringList      rangeTypeNames      MEMBER _rangeTypeNames                              CONSTANT)
+    Q_PROPERTY(RangeType        rangeType           READ    rangeType           WRITE setRangeType      NOTIFY rangeTypeChanged)
+    Q_PROPERTY(QVariantList     rangeValues         READ    rangeValues         WRITE setRangeValues    NOTIFY rangeValuesChanged)
+    Q_PROPERTY(QVariantList     rangeColors         READ    rangeColors         WRITE setRangeColors    NOTIFY rangeColorsChanged)
+    Q_PROPERTY(QVariantList     rangeIcons          READ    rangeIcons          WRITE setRangeIcons     NOTIFY rangeIconsChanged)
+    Q_PROPERTY(QVariantList     rangeOpacities      READ    rangeOpacities      WRITE setRangeOpacities NOTIFY rangeOpacitiesChanged)
+    Q_PROPERTY(QColor           currentColor        MEMBER _currentColor                                NOTIFY currentColorChanged)
+    Q_PROPERTY(double           currentOpacity      MEMBER _currentOpacity                              NOTIFY currentOpacityChanged)
+    Q_PROPERTY(QString          currentIcon         MEMBER _currentIcon                                 NOTIFY currentIconChanged)
 
     Q_INVOKABLE void    setFact         (const QString& factGroupName, const QString& factName);
     Q_INVOKABLE void    clearFact       (void);
@@ -76,20 +76,20 @@ public:
 
     Fact*           fact                    (void) { return _fact; }
     FontSize        fontSize                (void) const { return _fontSize; }
-    QString         label                   (void) const { return _label; }
+    QString         text                    (void) const { return _text; }
     bool            showUnits               (void) const { return _showUnits; }
     QString         icon                    (void) const { return _icon; }
-    IconPosition    iconPosition            (void) const { return _iconPosition; }
+    LabelPosition   labelPosition           (void) const { return _labelPosition; }
     RangeType       rangeType               (void) const { return _rangeType; }
     QVariantList    rangeValues             (void) const { return _rangeValues; }
     QVariantList    rangeColors             (void) const { return _rangeColors; }
     QVariantList    rangeIcons              (void) const { return _rangeIcons; }
     QVariantList    rangeOpacities          (void) const { return _rangeOpacities; }
     void            setFontSize             (FontSize fontSize);
-    void            setLabel                (const QString& label);
+    void            setText                 (const QString& text);
     void            setShowUnits            (bool showUnits);
     void            setIcon                 (const QString& icon);
-    void            setIconPosition         (IconPosition iconPosition);
+    void            setLabelPosition        (LabelPosition labelPosition);
     void            setRangeType            (RangeType rangeType);
     void            setRangeValues          (const QVariantList& rangeValues);
     void            setRangeColors          (const QVariantList& rangeColors);
@@ -103,11 +103,11 @@ signals:
     void factChanged            (Fact* fact);
     void factNameChanged        (const QString& factName);
     void factGroupNameChanged   (const QString& factGroup);
-    void labelChanged           (QString label);
+    void textChanged            (QString text);
     void fontSizeChanged        (FontSize fontSize);
     void showUnitsChanged       (bool showUnits);
     void iconChanged            (const QString& icon);
-    void iconPositionChanged    (IconPosition iconPosition);
+    void labelPositionChanged   (LabelPosition labelPosition);
     void factGroupNamesChanged  (const QStringList& factGroupNames);
     void factValueNamesChanged  (const QStringList& factValueNames);
     void rangeTypeChanged       (RangeType rangeType);
@@ -135,11 +135,11 @@ private:
     Fact*               _fact =             nullptr;
     QString             _factName;
     QString             _factGroupName;
-    QString             _label;
+    QString             _text;
     bool                _showUnits =        true;
     FontSize            _fontSize =         DefaultFontSize;
     QString             _icon;
-    IconPosition        _iconPosition =     IconLeft;
+    LabelPosition       _labelPosition =    LabelLeft;
     QStringList         _factGroupNames;
     QStringList         _factValueNames;
     QColor              _currentColor;
@@ -162,18 +162,18 @@ private:
 
     // These are user facing string for the various enums.
     static const QStringList _rangeTypeNames;
-    static const QStringList _iconPositionNames;
+    static const QStringList _labelPositionNames;
     static       QStringList _iconNames;
     static const QStringList _fontSizeNames;
 
     static const char*  _versionKey;
     static const char*  _factGroupNameKey;
     static const char*  _factNameKey;
-    static const char*  _labelKey;
+    static const char*  _textKey;
     static const char*  _fontSizeKey;
     static const char*  _showUnitsKey;
     static const char*  _iconKey;
-    static const char*  _iconPositionKey;
+    static const char*  _labelPositionKey;
     static const char*  _rangeTypeKey;
     static const char*  _rangeValuesKey;
     static const char*  _rangeColorsKey;
@@ -183,5 +183,5 @@ private:
 };
 
 Q_DECLARE_METATYPE(InstrumentValue::FontSize)
-Q_DECLARE_METATYPE(InstrumentValue::IconPosition)
+Q_DECLARE_METATYPE(InstrumentValue::LabelPosition)
 Q_DECLARE_METATYPE(InstrumentValue::RangeType)
