2cd46af08014405d7c673e83aa9dbcf00eb0bd71
Don Gagne <dongagne@outlook.com>
2020-05-15 10:28:31 -0700

    Clean up and turn on compiler warnings as errors
    More warnings cleanup
    Android fix
    More
    More
    More
    Fix typo
    Update android compiler warnings
    Fix android compiler warnings
    More android compiler warnings

--- src/MissionManager/SimpleMissionItem.cc
+++ src/MissionManager/SimpleMissionItem.cc
@@ -1008,6 +1008,9 @@ double SimpleMissionItem::amslEntryAlt(void) const
         return _missionItem.param7();
     case QGroundControlQmlGlobal::AltitudeModeRelative:
         return _missionItem.param7() + _masterController->missionController()->plannedHomePosition().altitude();
+    case QGroundControlQmlGlobal::AltitudeModeNone:
+        qWarning() << "Internal Error SimpleMissionItem::amslEntryAlt: Invalid altitudeMode:AltitudeModeNone";
+        return qQNaN();
     }
 
     qWarning() << "Internal Error SimpleMissionItem::amslEntryAlt: Invalid altitudeMode:" << _altitudeMode;
