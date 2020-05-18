92370b404d959711b5fd03022d7f1cafe26deb7b
Patrick José Pereira <patrickelectric@gmail.com>
2020-05-18 16:02:33 -0300

    PlanMasterController: Fix deprecated QString::sprintf
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/MissionManager/PlanMasterController.cc
+++ src/MissionManager/PlanMasterController.cc
@@ -403,7 +403,7 @@ void PlanMasterController::loadFromFile(const QString& filename)
     }
 
     if(success){
-        _currentPlanFile.sprintf("%s/%s.%s", fileInfo.path().toLocal8Bit().data(), fileInfo.completeBaseName().toLocal8Bit().data(), AppSettings::planFileExtension);
+        _currentPlanFile = QString::asprintf("%s/%s.%s", fileInfo.path().toLocal8Bit().data(), fileInfo.completeBaseName().toLocal8Bit().data(), AppSettings::planFileExtension);
     } else {
         _currentPlanFile.clear();
     }
