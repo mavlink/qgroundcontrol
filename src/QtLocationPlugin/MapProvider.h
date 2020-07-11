01716a667b9c3275d01ce1322faed446e392f874
DonLakeFlyer <don@thegagnes.com>
2020-07-11 09:57:44 -0700

    Ability to determine Bing provider

--- src/QtLocationPlugin/MapProvider.h
+++ src/QtLocationPlugin/MapProvider.h
@@ -45,7 +45,8 @@ public:
 
     virtual int lat2tileY(const double lat, const int z) const;
 
-    virtual bool _isElevationProvider() const;
+    virtual bool _isElevationProvider() const { return false; }
+    virtual bool _isBingProvider() const { return false; }
 
     virtual QGCTileSet getTileCount(const int zoom, const double topleftLon,
                                      const double topleftLat, const double bottomRightLon,
