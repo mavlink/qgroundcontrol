f2b47246f95162291305ab1b0ed36843a9f2b8f3
Remek Zajac <remek@airmap.com>
2020-08-18 13:33:28 +0200

    Ensure showGimbalOnlyWhenSet as custom builds may have other defaults

--- src/MissionManager/MissionControllerTest.cc
+++ src/MissionManager/MissionControllerTest.cc
@@ -172,6 +172,7 @@ void MissionControllerTest::_testGimbalRecalc(void)
     SimpleMissionItem* item = _missionController->visualItems()->value<SimpleMissionItem*>(yawIndex);
     item->cameraSection()->setSpecifyGimbal(true);
     item->cameraSection()->gimbalYaw()->setRawValue(0.0);
+    qgcApp()->toolbox()->settingsManager()->planViewSettings()->showGimbalOnlyWhenSet()->setRawValue(false);
     QTest::qWait(100); // Recalcs in MissionController are queued to remove dups. Allow return to main message loop.
     for (int i=1; i<_missionController->visualItems()->count(); i++) {
         //qDebug() << i;
