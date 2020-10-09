c274751b42f1b69033f37e899d511229800f1ccc
Don Gagne <DonLakeFlyer@users.noreply.github.com>
2020-10-09 14:56:03 -0700

    Switch to new translations directory (#9105)

--- localization/to-crowdin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is set to find lupdate in my particular installation. You will need to set the path
-# where you have Qt installed.
-QT_PATH=~//Qt/5.12.6/gcc_64/bin
-rm qgc.ts
-$QT_PATH/lupdate ../src -ts qgc.ts
-python qgc-lupdate-json.py
-mv qgc.ts.new qgc.ts
