853e04d1d1843ed0187a231934bb853277bd5b28
Patrick José Pereira <patrickelectric@gmail.com>
2020-12-27 15:53:24 -0300

    main: Fix mac build when running Qt 5.15 (#9269)
    QProces::execute(QString) was deprecated, QProcess::execute(QString, QStringList) should be used
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -250,7 +250,7 @@ int main(int argc, char *argv[])
 #ifndef __ios__
     // Prevent Apple's app nap from screwing us over
     // tip: the domain can be cross-checked on the command line with <defaults domains>
-    QProcess::execute("defaults write org.qgroundcontrol.qgroundcontrol NSAppSleepDisabled -bool YES");
+    QProcess::execute("defaults", {"write org.qgroundcontrol.qgroundcontrol NSAppSleepDisabled -bool YES"});
 #endif
 #endif
 
