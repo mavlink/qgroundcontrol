4f9059ded86fc78924f1e3cd0b24fe1490a8a82a
Aleksey Kontsevich <akontsevich@gmail.com>
2021-01-20 17:54:02 +0200

    Migrated to new Mapbox provider API

--- src/QtLocationPlugin/QGCMapUrlEngine.cpp
+++ src/QtLocationPlugin/QGCMapUrlEngine.cpp
@@ -65,8 +65,7 @@ UrlFactory::UrlFactory() : _timeout(5 * 1000) {
     _providersTable["Mapbox Hybrid"]       = new MapboxHybridMapProvider(this);
     _providersTable["Mapbox StreetsBasic"] = new MapboxStreetsBasicMapProvider(this);
     _providersTable["Mapbox Outdoors"]     = new MapboxOutdoorsMapProvider(this);
-    _providersTable["Mapbox RunBikeHike"]  = new MapboxRunBikeHikeMapProvider(this);
-    _providersTable["Mapbox HighContrast"] = new MapboxHighContrastMapProvider(this);
+    _providersTable["Mapbox Bright"]       = new MapboxBrightMapProvider(this);
     _providersTable["Mapbox Custom"]       = new MapboxCustomMapProvider(this);
 
     //_providersTable["MapQuest Map"] = new MapQuestMapMapProvider(this);
