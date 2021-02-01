28e36431d7e40a14fb3f0888155a1ea27cf1c5dd
Matthias Grob <maetugr@gmail.com>
2021-02-01 14:18:10 +0100

    Add setup script to build QGC on arch linux

---
+++ tools/setup/arch.sh
@@ -0,0 +1,13 @@
+sudo pacman -Sy --noconfirm --needed \
+    speech-dispatcher \
+    qt5-base \
+    qt5-charts \
+    qt5-graphicaleffects \
+    qt5-location \
+    qt5-multimedia \
+    qt5-quickcontrols \
+    qt5-quickcontrols2 \
+    qt5-serialport \
+    qt5-speech \
+    qt5-tools \
+    ;
