e69548a56d7ead4de7642d2c2ba7f46efe76f410
DonLakeFlyer <don@thegagnes.com>
2021-03-17 11:37:37 -0700

    Add Vehicle::vehicleClass method

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -422,6 +422,7 @@ public:
     int id() { return _id; }
     MAV_AUTOPILOT firmwareType() const { return _firmwareType; }
     MAV_TYPE vehicleType() const { return _vehicleType; }
+    QGCMAVLink::VehicleClass_t vehicleClass(void) const { return QGCMAVLink::vehicleClass(_vehicleType); }
     Q_INVOKABLE QString vehicleTypeName() const;
 
     /// Sends a message to the specified link
