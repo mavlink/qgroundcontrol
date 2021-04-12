83f7664c9d1afddb829d76600aac880b0273817f
Beat Küng <beat-kueng@gmx.net>
2021-04-12 10:56:22 +0200

    Revert "Control COMPONENT_INFORMATION query from app setting"
    This reverts commit ec13a2d0ab2b4604e35d83f41378972c37ec0028.
    
    Now ready to be used by default.

--- src/Settings/AppSettings.cc
+++ src/Settings/AppSettings.cc
@@ -137,7 +137,6 @@ DECLARE_SETTINGSFACT(AppSettings, saveCsvTelemetry)
 DECLARE_SETTINGSFACT(AppSettings, firstRunPromptIdsShown)
 DECLARE_SETTINGSFACT(AppSettings, forwardMavlink)
 DECLARE_SETTINGSFACT(AppSettings, forwardMavlinkHostName)
-DECLARE_SETTINGSFACT(AppSettings, useComponentInformationQuery)
 
 DECLARE_SETTINGSFACT_NO_FUNC(AppSettings, indoorPalette)
 {
