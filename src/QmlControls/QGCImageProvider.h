9c6dff4a7fb3b2c1e1d85f90b42bb3bef134b024
DonLakeFlyer <don@thegagnes.com>
2021-05-23 12:24:01 -0700

    Rewrite Mavlink image protocol supportt and fix bugs

--- src/QmlControls/QGCImageProvider.h
+++ src/QmlControls/QGCImageProvider.h
@@ -7,40 +7,32 @@
  *
  ****************************************************************************/
 
-
-/**
- * @file
- *   @brief Image Provider
- *
- *   @author Gus Grubba <gus@auterion.com>
- *
- */
-
-
-#ifndef QGCIMAGEPROVIDER_H
-#define QGCIMAGEPROVIDER_H
-
+#pragma once
 #include <QObject>
 #include <QQmlListProperty>
 #include <QQuickImageProvider>
 
 #include "QGCToolbox.h"
 
+// This is used to expose images from ImageProtocolHandler
 class QGCImageProvider : public QGCTool, public QQuickImageProvider
 {
 public:
     QGCImageProvider        (QGCApplication* app, QGCToolbox* toolbox);
     ~QGCImageProvider       ();
-    QImage  requestImage    (const QString & id, QSize * size, const QSize & requestedSize);
+
     void    setImage        (QImage* pImage, int id = 0);
-    void    setToolbox      (QGCToolbox *toolbox);
+
+    // Overrdies from QQuickImageProvider
+    QImage  requestImage    (const QString& id, QSize* size, const QSize& requestedSize) override;
+
+    // Overrides from QGCTool
+    void    setToolbox      (QGCToolbox *toolbox) override;
+
 private:
     //-- TODO: For now this is holding a single image. If you happen to have two
     //   or more vehicles with flow, it will not work. To properly manage that condition
     //   this should be a map between each vehicle and its image. The URL provided
     //   for the image request would contain the vehicle identification.
-    QImage _pImage;
+    QImage _image;
 };
-
-
-#endif // QGCIMAGEPROVIDER_H
