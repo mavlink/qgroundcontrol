9891ada762256d6035ca32578cdb464fe967af4b
Julian Oes <julian@oes.ch>
2021-11-02 10:00:49 -0700

    MavlinkConsoleController: add target sysid/compid
    The SERIAL_CONSOLE message now includes a target_system and
    target_comonent which prevents serial messages being sent to the wrong
    vehicle in case there are multiple vehicles connected.
    
    More background:
    https://github.com/mavlink/mavlink/pull/1725
    
    Former-commit-id: 6cec60a55ab49c098dd9ca2b6e4922e1f25a3d76

--- src/AnalyzeView/MavlinkConsoleController.cc
+++ src/AnalyzeView/MavlinkConsoleController.cc
@@ -158,7 +158,8 @@ MavlinkConsoleController::_sendSerialData(QByteArray data, bool close)
                     0,
                     0,
                     chunk.size(),
-                    reinterpret_cast<uint8_t*>(chunk.data()));
+                    reinterpret_cast<uint8_t*>(chunk.data()),
+                    _vehicle->id(), _vehicle->defaultComponentId());
         _vehicle->sendMessageOnLinkThreadSafe(sharedLink.get(), msg);
         data.remove(0, chunk.size());
     }
