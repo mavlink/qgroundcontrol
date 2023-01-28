1ab8b9b244fde3141c5ba1dc2839d147e864b6f2
Tevhit Karsli <tevhitkarsli@hotmail.com>
2023-01-28 17:06:31 +0300

    Update QGCMapUrlEngine for wrong new() constructor
    _providersTable["Google Labels"] has constructed with GoogleTerrainMapProvider class as wrongly. It have to construct with GoogleLabelsMapProvider class.

--- src/QtLocationPlugin/QGCMapUrlEngine.cpp
+++ src/QtLocationPlugin/QGCMapUrlEngine.cpp
@@ -42,7 +42,7 @@ UrlFactory::UrlFactory() : _timeout(5 * 1000) {
     _providersTable["Google Satellite"]  = new GoogleSatelliteMapProvider(this);
     _providersTable["Google Terrain"]    = new GoogleTerrainMapProvider(this);
     _providersTable["Google Hybrid"]    = new GoogleHybridMapProvider(this);
-    _providersTable["Google Labels"]     = new GoogleTerrainMapProvider(this);
+    _providersTable["Google Labels"]     = new GoogleLabelsMapProvider(this);
 #endif
 
     _providersTable["Bing Road"]      = new BingRoadMapProvider(this);
