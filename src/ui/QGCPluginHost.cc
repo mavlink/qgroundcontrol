02cc249dfc918b98cb2508fb26a9efe00cc92af0
Patrick José Pereira <patrickelectric@gmail.com>
2023-03-14 13:16:37 -0300

    ui: Use clang-format
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/ui/QGCPluginHost.cc
+++ src/ui/QGCPluginHost.cc
@@ -1,14 +1,11 @@
 #include "QGCPluginHost.h"
 #include "ui_QGCPluginHost.h"
 
-QGCPluginHost::QGCPluginHost(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::QGCPluginHost)
+QGCPluginHost::QGCPluginHost(QWidget* parent)
+    : QWidget(parent)
+    , ui(new Ui::QGCPluginHost)
 {
     ui->setupUi(this);
 }
 
-QGCPluginHost::~QGCPluginHost()
-{
-    delete ui;
-}
+QGCPluginHost::~QGCPluginHost() { delete ui; }
--- src/ui/QGCPluginHost.h
+++ src/ui/QGCPluginHost.h
@@ -6,15 +6,13 @@ namespace Ui {
 class QGCPluginHost;
 }
 
-class QGCPluginHost : public QWidget
-{
+class QGCPluginHost : public QWidget {
     Q_OBJECT
-    
-public:
-    explicit QGCPluginHost(QWidget *parent = 0);
-    ~QGCPluginHost();
-    
-private:
-    Ui::QGCPluginHost *ui;
-};
 
+public:
+    explicit QGCPluginHost(QWidget* parent = 0);
+    ~QGCPluginHost();
+
+private:
+    Ui::QGCPluginHost* ui;
+};
