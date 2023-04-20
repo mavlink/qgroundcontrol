b8f22bd2847a34cb94a9557b72ac9fb0e0a42d24
Harsh Mittal <harshmittal2210@gmail.com>
2023-04-20 16:56:59 +0530

    Used NO_ARDUPILOT_DIALECT for RangeFinder Message Callback

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -1033,7 +1033,6 @@ private:
     void _handleGlobalPositionInt       (mavlink_message_t& message);
     void _handleAltitude                (mavlink_message_t& message);
     void _handleVfrHud                  (mavlink_message_t& message);
-    void _handleRangefinder             (mavlink_message_t& message);
     void _handleNavControllerOutput     (mavlink_message_t& message);
     void _handleHighLatency             (mavlink_message_t& message);
     void _handleHighLatency2            (mavlink_message_t& message);
@@ -1048,6 +1047,7 @@ private:
     // ArduPilot dialect messages
 #if !defined(NO_ARDUPILOT_DIALECT)
     void _handleCameraFeedback          (const mavlink_message_t& message);
+    void _handleRangefinder             (mavlink_message_t& message);
 #endif
     void _handleCameraImageCaptured     (const mavlink_message_t& message);
     void _handleADSBVehicle             (const mavlink_message_t& message);
