6b1817cda010ec93f7b0c89716064e7df202b650
davidsastresas <davidsastresas@gmail.com>
2023-06-14 03:20:02 +0200

    FlightMapSettings: add elevation map provider setting

--- src/Settings/FlightMapSettings.cc
+++ src/Settings/FlightMapSettings.cc
@@ -23,3 +23,4 @@ DECLARE_SETTINGGROUP(FlightMap, "FlightMap")
 
 DECLARE_SETTINGSFACT(FlightMapSettings, mapProvider)
 DECLARE_SETTINGSFACT(FlightMapSettings, mapType)
+DECLARE_SETTINGSFACT(FlightMapSettings, elevationMapProvider)
--- src/Settings/FlightMapSettings.h
+++ src/Settings/FlightMapSettings.h
@@ -22,5 +22,6 @@ public:
     DEFINE_SETTING_NAME_GROUP()
     DEFINE_SETTINGFACT(mapProvider)
     DEFINE_SETTINGFACT(mapType)
+    DEFINE_SETTINGFACT(elevationMapProvider)
 
 };
