8ebc1ac195258d20745892bc3374fa874198a358
davidsastresas <davidsastresas@gmail.com>
2023-06-19 19:19:51 +0200

    Ardupilot: Add support for fixed wing airspeed change

--- src/FirmwarePlugin/APM/APMFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/APMFirmwarePlugin.h
@@ -80,6 +80,10 @@ public:
     QString             getHobbsMeter                   (Vehicle* vehicle) override; 
     bool                hasGripper                      (const Vehicle* vehicle) const override;
     const QVariantList& toolIndicators                  (const Vehicle* vehicle) override;
+    double              maximumEquivalentAirspeed       (Vehicle* vehicle) override;
+    double              minimumEquivalentAirspeed       (Vehicle* vehicle) override;
+    bool                fixedWingAirSpeedLimitsAvailable(Vehicle* vehicle) override;
+    void                guidedModeChangeEquivalentAirspeed(Vehicle* vehicle, double airspeed_equiv) override;
 
 protected:
     /// All access to singleton is through stack specific implementation
