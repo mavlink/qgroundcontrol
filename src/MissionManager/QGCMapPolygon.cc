91a4e118ba4f52b3359f3bd85e5cb7910e3f1baa
Yogesh Pandey <110195131+i-am-yogesh@users.noreply.github.com>
2023-07-27 07:59:33 +0530

    Followed code style of QGC

--- src/MissionManager/QGCMapPolygon.cc
+++ src/MissionManager/QGCMapPolygon.cc
@@ -279,7 +279,9 @@ void QGCMapPolygon::appendVertices(const QList<QGeoCoordinate>& coordinates)
         objects.append(new QGCQGeoCoordinate(coordinate, this));
         _polygonPath.append(QVariant::fromValue(coordinate));
     }
-    if(!objects.empty())  _polygonModel.append(objects);
+    if (!objects.empty()) {
+        _polygonModel.append(objects);
+    }
     _endResetIfNotActive();
 
     emit pathChanged();
