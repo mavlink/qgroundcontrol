ba952d399f76e9265751de4cc9e1b6e5c3061a14
Roman Dvořák <romandvorak@mlab.cz>
2023-08-03 16:42:22 +0200

    Update VehicleRpmFactGroup.cc

--- src/Vehicle/VehicleRpmFactGroup.cc
+++ src/Vehicle/VehicleRpmFactGroup.cc
@@ -10,7 +10,6 @@
 #include "VehicleSetpointFactGroup.h"
 #include "Vehicle.h"
 
-//const char* VehicleRpmFactGroup::_rpmFactName =      "rpm";
 const char* VehicleRpmFactGroup::_rpm1FactName =      "rpm1";
 const char* VehicleRpmFactGroup::_rpm2FactName =      "rpm2";
 const char* VehicleRpmFactGroup::_rpm3FactName =      "rpm3";
@@ -18,20 +17,17 @@ const char* VehicleRpmFactGroup::_rpm4FactName =      "rpm4";
 
 VehicleRpmFactGroup::VehicleRpmFactGroup(QObject* parent)
     : FactGroup(1000, ":/json/Vehicle/RpmFact.json", parent)
-  //  , _rpmFact    (0, _rpmFactName,     FactMetaData::valueTypeDouble)
     , _rpm1Fact    (0, _rpm1FactName,     FactMetaData::valueTypeDouble)
     , _rpm2Fact    (0, _rpm2FactName,     FactMetaData::valueTypeDouble)
     , _rpm3Fact    (0, _rpm3FactName,     FactMetaData::valueTypeDouble)
     , _rpm4Fact    (0, _rpm4FactName,     FactMetaData::valueTypeDouble)
 {
-  //  _addFact(&_rpmFact,       _rpmFactName);
     _addFact(&_rpm1Fact,       _rpm1FactName);
     _addFact(&_rpm2Fact,       _rpm2FactName);
     _addFact(&_rpm3Fact,       _rpm3FactName);
     _addFact(&_rpm4Fact,       _rpm4FactName);
 
     // Start out as not available "--.--"
-   // _rpmFact.setRawValue      (qQNaN());
     _rpm1Fact.setRawValue      (qQNaN());
     _rpm2Fact.setRawValue      (qQNaN());
     _rpm3Fact.setRawValue      (qQNaN());
@@ -53,7 +49,6 @@ void VehicleRpmFactGroup::_handleRawRPM(mavlink_message_t& message)
 {
     mavlink_raw_rpm_t raw_rpm;
     mavlink_msg_raw_rpm_decode(&message, &raw_rpm);
-    //rpm()->setRawValue(raw_rpm.frequency);
     switch (raw_rpm.index) {
         case 0:
             rpm1()->setRawValue(raw_rpm.frequency);
