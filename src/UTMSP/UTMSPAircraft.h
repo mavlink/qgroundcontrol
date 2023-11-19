ba97cd4b39808fc47dd6989de55f36abadb38cbf
rpashchapur <ravi.pashchapur@tii.ae>
2023-11-19 19:53:44 +0400

    Add Flight Plan Manager service and its Controller and dependencies for UTM adapter
    - This commit introduces the flight planning capabilities within the UTM adapter.

---
+++ src/UTMSP/UTMSPAircraft.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ * (c) 2023 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include "MAVLinkProtocol.h"
+#include "MAVLinkStreamConfig.h"
+#include <string>
+
+class UTMSPAircraft {
+public:
+    UTMSPAircraft();
+    std::string aircraftSerialNo(const mavlink_message_t &message);
+    std::string aircraftModel();
+    std::string aircraftType(const mavlink_message_t& message);
+    std::string aircraftClass();
+
+private:
+    int             _mavType;
+    uint64_t        _mavSerialNumber;
+
+    static std::vector<std::string> _mavTypes;
+};
