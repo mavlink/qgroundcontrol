00b799572bf26ad504edfaa58b806facef07d118
Bartek Zdanowski (Zdanek) <bartek.zdanowski@gmail.com>
2023-12-08 19:29:28 +0100

    Migrate GstGLVideoItem to Qt6 with fixes
    In particular
    * uses new Qt6 qmlsink gstreamer plugin code (switch to new submodule repo)
    * removes unnecessary dependency of QGC to VideoReceiverApp
    * removes dependency of VideoReceiver to Settings submodule (uses only enum from Settings)
    * removes registering of GstGLVideoItem in GStreamer.cc (it's registered in own code)
    * minor cleanup and fixes in VideoReceiverApp
    * fixes dragging map when resizing/clicking on Pip Window

--- src/VideoReceiver/GStreamer.h
+++ src/VideoReceiver/GStreamer.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <QObject>
+#include <QLoggingCategory>
 #include <QQuickItem>
 
+#include "Settings/VideoDecoderOptions.h"
 #include "VideoReceiver.h"
-#include "VideoSettings.h"
 
 Q_DECLARE_LOGGING_CATEGORY(GStreamerLog)
 Q_DECLARE_LOGGING_CATEGORY(GStreamerAPILog)
 
 class GStreamer {
 public:
-    static void blacklist(VideoSettings::VideoDecoderOptions option);
+    static void blacklist(VideoDecoderOptions option);
     static void initialize(int argc, char* argv[], int debuglevel);
     static void* createVideoSink(QObject* parent, QQuickItem* widget);
     static void releaseVideoSink(void* sink);
