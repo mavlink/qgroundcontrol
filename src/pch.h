b1f35a4920e44557326de4ce678b2fbcecd72aba
Holden <htramsey98@gmail.com>
2024-02-17 03:01:06 -0500

    Correct precompiled headers
    Former-commit-id: 591143098c4e80303825e635ab0475181c6c64cd

---
+++ src/pch.h
@@ -0,0 +1,9 @@
+#ifdef __cplusplus
+#include <QtQml/QQmlApplicationEngine>
+#include <QtGui/QGuiApplication>
+#ifdef Q_OS_ANDROID
+#include <QtCore/QJniEnvironment>
+#endif
+#include <QtCore/QDebug>
+#include <QtCore/QLoggingCategory>
+#endif
--- src/stable_headers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <QtCore>
-#include <QtGui>
-#include <QtWidgets>
-
-
