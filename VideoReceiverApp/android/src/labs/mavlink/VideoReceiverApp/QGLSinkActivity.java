c1fcdcba3f41e6d8d49374683082ac0abd83f7e5
Holden <68555040+HTRamsey@users.noreply.github.com>
2024-02-21 13:52:40 -0500

    Update VideoReceiverApp CMake (#11080)

--- VideoReceiverApp/android/src/labs/mavlink/VideoReceiverApp/QGLSinkActivity.java
+++ VideoReceiverApp/android/src/labs/mavlink/VideoReceiverApp/QGLSinkActivity.java
@@ -1,78 +1,27 @@
 package labs.mavlink.VideoReceiverApp;
 
-/* Copyright 2013 Google Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * Project home page: http://code.google.com/p/usb-serial-for-android/
- */
-///////////////////////////////////////////////////////////////////////////////////////////
-//  Written by: Mike Goza April 2014
-//
-//  These routines interface with the Android USB Host devices for serial port communication.
-//  The code uses the usb-serial-for-android software library.  The QGCActivity class is the
-//  interface to the C++ routines through jni calls.  Do not change the functions without also
-//  changing the corresponding calls in the C++ routines or you will break the interface.
-//
-////////////////////////////////////////////////////////////////////////////////////////////
+import org.qtproject.qt.android.bindings.QtActivity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.io.IOException;
-
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbAccessory;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
-import android.widget.Toast;
 import android.util.Log;
-import android.os.PowerManager;
 import android.os.Bundle;
-import android.app.PendingIntent;
-import android.view.WindowManager;
-import android.os.Bundle;
-import android.bluetooth.BluetoothDevice;
-
-import org.qtproject.qt5.android.bindings.QtActivity;
-import org.qtproject.qt5.android.bindings.QtApplication;
 
 public class QGLSinkActivity extends QtActivity
 {
+    private static final String LOG_TAG = "QGLSinkActivity";
+
     public native void nativeInit();
 
-    // QGLSinkActivity singleton
-    public QGLSinkActivity() {
+    public QGLSinkActivity()
+    {
+        Log.i(LOG_TAG, "MainActivity");
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         nativeInit();
+        Log.i(LOG_TAG, "Created");
     }
 
     @Override
