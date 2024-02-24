53b4ff87adaceb2681c7162d12bd0a900f91185f
Don Gagne <don@thegagnes.com>
2024-02-23 17:02:42 -0800

    Request location permissions

--- src/PositionManager/PositionManager.h
+++ src/PositionManager/PositionManager.h
@@ -60,6 +60,8 @@ signals:
     void gcsPositionHorizontalAccuracyChanged();
 
 private:
+    void _setupPositionSources(QGCToolbox *toolbox);
+
     int                 _updateInterval =   0;
     QGeoPositionInfo    _geoPositionInfo;
     QGeoCoordinate      _gcsPosition;
