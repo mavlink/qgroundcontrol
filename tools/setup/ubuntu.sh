1620acfd808d2e93c552395e215e528517da1415
Don Gagne <don@thegagnes.com>
2024-02-27 10:27:38 -0800

    Added shell script to setup ubuntu tools for building
    Former-commit-id: 4a7be30b85b6ea6979f034c9a249249dca033c3c

---
+++ tools/setup/ubuntu.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+set -e
+
+sudo apt-get update -y --quiet
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+	build-essential \
+	ccache \
+	cmake \
+	cppcheck \
+	file \
+	g++ \
+	gcc \
+	gdb \
+	git \
+	libfuse2 \
+	make \
+	ninja-build \
+	rsync \
+	libsdl2-dev \
+    libgstreamer-plugins-base1.0-dev \
+    libgstreamer1.0-0:amd64 \
+    libgstreamer1.0-dev \
+    binutils \
+    patchelf \
+    libxcb-xinerama0 \
+    libxkbcommon-x11-0 \
+    libxcb-cursor0 \
