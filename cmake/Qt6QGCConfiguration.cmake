8f84472f9dd62248749abc45acb05c55954d9a96
Holden <htramsey98@gmail.com>
2024-03-06 23:22:06 -0500

    Cmake: add translations

--- cmake/Qt6QGCConfiguration.cmake
+++ cmake/Qt6QGCConfiguration.cmake
@@ -25,7 +25,7 @@ if(NOT QT_MKSPEC)
 	elseif(LINUX)
 		set(QT_MKSPEC gcc_64)
 	elseif(WIN32)
-		set(QT_MKSPEC msvc2017_64)
+		set(QT_MKSPEC msvc2019_64)
 	elseif(ANDROID)
 		if(${ANDROID_ABI} STREQUAL armeabi-v7a)
 			set(QT_MKSPEC android_armv7)
@@ -36,9 +36,16 @@ if(NOT QT_MKSPEC)
 endif()
 
 set(QT_LIBRARY_HINTS
-		$ENV{HOME}/Qt/${QT_VERSION}/${QT_MKSPEC}
-		$ENV{QT_PATH}/${QT_VERSION}/${QT_MKSPEC}
-		C:/Qt
+	$ENV{QT_PATH}/${QT_VERSION}/${QT_MKSPEC}
+	${Qt6_DIR}
 )
+
+if(ANDROID)
+	list(APPEND QT_LIBRARY_HINTS ${QT_HOST_PATH}/lib/cmake)
+elseif(WIN32)
+	list(APPEND QT_LIBRARY_HINTS C:/Qt/${QT_VERSION}/${QT_MKSPEC})
+elseif(LINUX)
+	list(APPEND QT_LIBRARY_HINTS $ENV{HOME}/Qt/${QT_VERSION}/${QT_MKSPEC})
+endif()
 
 message(STATUS "lib hints ${QT_LIBRARY_HINTS}")
