b14a70c3c7ad8de7554d590803ac01ba0aae8c8a
Nick Exton <NExton@sypaq.com.au>
2024-03-08 16:46:05 +1100

    Vehicle: Remove centi-degrees conversion in MOUNT_CONTROL
    Fixes: #11215

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -4292,12 +4292,6 @@ void Vehicle::_altitudeAboveTerrainReceived(bool success, QList<double> heights)
 
 void Vehicle::gimbalControlValue(double pitch, double yaw)
 {
-    if (apmFirmware()) {
-        // ArduPilot firmware treats this values as centi-degrees
-        pitch *= 100;
-        yaw *= 100;
-    }
-
     //qDebug() << "Gimbal:" << pitch << yaw;
     sendMavCommand(
                 _defaultComponentId,
