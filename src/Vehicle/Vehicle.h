5785532393df1d391dc91f4393b11e490b5222e6
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2024-03-27 22:01:39 +0100

    Update guidedModeGotoLocation doc comment (#10978)

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -392,7 +392,7 @@ public:
     /// @return Minumum equivalent airspeed.
     Q_INVOKABLE double minimumEquivalentAirspeed();
 
-    /// Command vehicle to move to specified location (altitude is included and relative)
+    /// Command vehicle to move to specified location (altitude is ignored)
     Q_INVOKABLE void guidedModeGotoLocation(const QGeoCoordinate& gotoCoord);
 
     /// Command vehicle to change altitude
