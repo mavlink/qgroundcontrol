9549ac5ee5ea4db165c9b6022c6dda4a5e1bc3e7
Don Gagne <don@thegagnes.com>
2024-04-06 14:19:54 -0700

    Modify hidden flight modes to be vehicle class specific

--- src/Settings/FlightModeSettings.cc
+++ src/Settings/FlightModeSettings.cc
@@ -16,5 +16,15 @@ DECLARE_SETTINGGROUP(FlightMode, "FlightMode")
     qmlRegisterUncreatableType<FlightModeSettings>("QGroundControl.SettingsManager", 1, 0, "FlightModeSettings", "Reference only");
 }
 
-DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModes)
-DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModes)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesMultiRotor)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesFixedWing)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesVTOL)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesRoverBoat)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesSub)
+DECLARE_SETTINGSFACT(FlightModeSettings, px4HiddenFlightModesAirship)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesMultiRotor)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesFixedWing)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesVTOL)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesRoverBoat)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesSub)
+DECLARE_SETTINGSFACT(FlightModeSettings, apmHiddenFlightModesAirship)
--- src/Settings/FlightModeSettings.h
+++ src/Settings/FlightModeSettings.h
@@ -19,6 +19,16 @@ public:
     FlightModeSettings(QObject* parent = nullptr);
 
     DEFINE_SETTING_NAME_GROUP()
-    DEFINE_SETTINGFACT(px4HiddenFlightModes)
-    DEFINE_SETTINGFACT(apmHiddenFlightModes)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesMultiRotor)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesFixedWing)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesVTOL)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesRoverBoat)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesSub)
+    DEFINE_SETTINGFACT(px4HiddenFlightModesAirship)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesMultiRotor)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesFixedWing)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesVTOL)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesRoverBoat)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesSub)
+    DEFINE_SETTINGFACT(apmHiddenFlightModesAirship)
 };
