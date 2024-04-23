890cf3701b4e50c68680fa8d5d672516822d5ad9
Holden <htramsey98@gmail.com>
2024-04-23 04:54:11 -0400

    CMake: Deploy Adjustments

--- cmake/Git.cmake
+++ cmake/Git.cmake
@@ -1,35 +1,54 @@
 find_package(Git)
 
-if(NOT GIT_FOUND OR NOT EXISTS "${PROJECT_SOURCE_DIR}/.git")
-    return()
+if(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    option(GIT_SUBMODULE "Check submodules during build" ON)
+    if(GIT_SUBMODULE)
+        message(STATUS "Submodule update")
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+            RESULT_VARIABLE GIT_SUBMODULE_RESULT
+            OUTPUT_VARIABLE GIT_SUBMODULE_OUTPUT
+            ERROR_VARIABLE GIT_SUBMODULE_ERROR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        if(NOT GIT_SUBMODULE_RESULT EQUAL 0)
+            cmake_print_variables(GIT_SUBMODULE_RESULT GIT_SUBMODULE_OUTPUT GIT_SUBMODULE_ERROR)
+            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMODULE_RESULT}, please checkout submodules")
+        endif()
+    endif()
 endif()
 
-# Update submodules as needed
-option(GIT_SUBMODULE "Check submodules during build" ON)
-if(NOT GIT_SUBMODULE)
-    return()
-endif()
+include(CMakePrintHelpers)
 
-message(STATUS "Submodule update")
-execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-                RESULT_VARIABLE GIT_SUBMODULE_RESULT)
-if(NOT GIT_SUBMODULE_RESULT EQUAL "0")
-    message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMODULE_RESULT}, please checkout submodules")
-endif()
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --always --tags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE APP_VERSION_STR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+cmake_print_variables(APP_VERSION_STR)
 
-# Fetch the necessary git variables
-execute_process(COMMAND ${GIT_EXECUTABLE} describe --always --tags
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                OUTPUT_VARIABLE APP_VERSION_STR
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_BRANCH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+cmake_print_variables(GIT_BRANCH)
 
-execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                OUTPUT_VARIABLE REL_VERSION
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
+# execute_process(
+#     COMMAND ${GIT_EXECUTABLE} describe --abbrev=0
+#     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+#     OUTPUT_VARIABLE REL_VERSION
+#     OUTPUT_STRIP_TRAILING_WHITESPACE
+# )
+# cmake_print_variables(REL_VERSION)
 
-execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%aI ${REL_VERSION}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                OUTPUT_VARIABLE REL_DATE
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
+# execute_process(
+#     COMMAND ${GIT_EXECUTABLE} log -1 --format=%aI ${REL_VERSION}
+#     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+#     OUTPUT_VARIABLE REL_DATE
+#     OUTPUT_STRIP_TRAILING_WHITESPACE
+# )
+# cmake_print_variables(REL_DATE)
