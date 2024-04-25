0fa2f0e03f80daaf779cb9d754198e36bb9cf6c2
Holden <htramsey98@gmail.com>
2024-04-25 04:14:01 -0400

    CMake: FetchContent Improvements

--- cmake/BuildSdl2.cmake
+++ cmake/BuildSdl2.cmake
@@ -1,5 +1,8 @@
 function(build_sdl2)
     include(FetchContent)
+
+    set(SDL2_DISABLE_INSTALL TRUE CACHE INTERNAL "")
+    set(SDL2_DISABLE_UNINSTALL TRUE CACHE INTERNAL "")
     set(SDL2_DISABLE_SDL2MAIN TRUE CACHE INTERNAL "")
     set(SDL_SHARED FALSE CACHE INTERNAL "")
     set(SDL_STATIC TRUE CACHE INTERNAL "")
@@ -24,8 +27,11 @@ function(build_sdl2)
     set(SDL_TIMERS FALSE CACHE INTERNAL "")
     set(SDL_VIDEO FALSE CACHE INTERNAL "")
 
-    FetchContent_Declare(
-        SDL
+    set(SDL_3DNOW FALSE CACHE INTERNAL "")
+    set(SDL_MMX FALSE CACHE INTERNAL "")
+    set(SDL_VIRTUAL_JOYSTICK FALSE CACHE INTERNAL "")
+
+    FetchContent_Declare(SDL
         GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
         GIT_TAG release-2.30.2
         GIT_SHALLOW TRUE
