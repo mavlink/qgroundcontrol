a1a424f9d16f66286ebd877ae62cd72ca8e23b80
Holden <68555040+HTRamsey@users.noreply.github.com>
2024-04-27 05:25:32 -0400

    CI: Action to Build GStreamer (#11386)
    Former-commit-id: a5e566a9fcaafa7df3a09eaba794b92e4bc1917d

--- tools/setup/ubuntu.sh
+++ tools/setup/ubuntu.sh
@@ -3,6 +3,7 @@
 set -e
 
 sudo apt-get update -y --quiet
+
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
 	build-essential \
 	ccache \
@@ -17,17 +18,39 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends i
 	make \
 	ninja-build \
 	rsync \
-	libsdl2-dev \
-    libgstreamer-plugins-base1.0-dev \
-    libgstreamer1.0-0:amd64 \
-    libgstreamer1.0-dev \
     binutils \
-    patchelf \
-    libxcb-xinerama0 \
+    patchelf
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet install \
+	libxcb-xinerama0 \
     libxkbcommon-x11-0 \
     libxcb-cursor0 \
     libdrm-dev \
+	libsdl2-dev \
 	libspeechd2 \
 	flite \
 	speech-dispatcher \
 	speech-dispatcher-flite
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet install \
+	libgstreamer1.0-dev \
+	libgstreamer-plugins-base1.0-dev \
+	libgstreamer-plugins-good1.0-dev \
+	libgstreamer-plugins-bad1.0-dev \
+	gstreamer1.0-plugins-base \
+	gstreamer1.0-plugins-good \
+	gstreamer1.0-plugins-bad \
+	gstreamer1.0-plugins-ugly \
+	gstreamer1.0-plugins-rtp \
+	gstreamer1.0-libav \
+	gstreamer1.0-tools \
+	gstreamer1.0-x \
+	gstreamer1.0-alsa \
+	gstreamer1.0-gl \
+	gstreamer1.0-gtk3 \
+	gstreamer1.0-qt5 \
+	gstreamer1.0-pulseaudio \
+	gstreamer1.0-gl \
+	gstreamer1.0-libav \
+	gstreamer1.0-vaapi \
+	gstreamer1.0-rtsp
