ca3046c1362c477860578e03e924a97f649982f8
Holden <htramsey98@gmail.com>
2024-05-01 14:06:13 -0400

    Test: Add Compression Test
    Former-commit-id: 19374c00487c06802847307f74edc2bd1b5d25fc

---
+++ test/Compression/DecompressionTest.cc
@@ -0,0 +1,26 @@
+#include "DecompressionTest.h"
+#include "QGCLZMA.h"
+#include "QGCZlib.h"
+
+#include <QtTest/QTest>
+
+DecompressionTest::DecompressionTest()
+{
+
+}
+
+void DecompressionTest::_testDecompressGzip()
+{
+	const QString gzippedFileName = QStringLiteral("manifest.json.gz");
+	const QString decompressedFilename = QStringLiteral("manifest.json");
+    const bool result = QGCZlib::inflateGzipFile(gzippedFileName, decompressedFilename);
+	QVERIFY(result);
+}
+
+void DecompressionTest::_testDecompressLZMA()
+{
+	const QString lzmaFilename = QStringLiteral("manifest.json.xz");
+	const QString decompressedFilename = QStringLiteral("manifest.json");
+    const bool result = QGCLZMA::inflateLZMAFile(lzmaFilename, decompressedFilename);
+	QVERIFY(result);
+}
