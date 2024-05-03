f783773e13e4b436cbb1d14148bec6f5ed616c7d
Holden <htramsey98@gmail.com>
2024-05-02 23:35:12 -0400

    CMake: Adjust pch

--- src/pch.h
+++ src/pch.h
@@ -1,25 +1,25 @@
 #ifdef __cplusplus
-#include <QtBluetooth/QBluetoothSocket>
-#include <QtCharts/QAbstractSeries>
-#include <QtConcurrent/QtConcurrent>
+// #include <QtBluetooth/QBluetoothSocket>
+// #include <QtCharts/QAbstractSeries>
+// #include <QtConcurrent/QtConcurrent>
 #include <QtCore/QCoreApplication>
 #include <QtGui/QGuiApplication>
-#include <QtLocation/QGeoServiceProvider>
-#include <QtMultimedia/QMediaDevices>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtPositioning/QGeoCoordinate>
+// #include <QtLocation/QGeoServiceProvider>
+// #include <QtMultimedia/QMediaDevices>
+// #include <QtNetwork/QNetworkAccessManager>
+// #include <QtPositioning/QGeoCoordinate>
 #include <QtQml/QQmlApplicationEngine>
-#include <QtQmlIntegration/QtQmlIntegration>
+// #include <QtQmlIntegration/QtQmlIntegration>
 #include <QtQuick/QQuickItem>
 #include <QtQuickControls2/QQuickStyle>
-#include <QtSql/QSqlDatabase>
-#include <QtTest/QSignalSpy>
-#include <QtTextToSpeech/QTextToSpeech>
+// #include <QtSql/QSqlDatabase>
+// #include <QtTest/QSignalSpy>
+// #include <QtTextToSpeech/QTextToSpeech>
 #include <QtWidgets/QApplication>
-#include <QtXml/QDomDocument>
-#if !defined(Q_OS_IOS) && !defined(Q_OS_ANDROID)
-#include <QtSerialPort/QSerialPort>
-#endif
+// #include <QtXml/QDomDocument>
+// #if !defined(Q_OS_IOS) && !defined(Q_OS_ANDROID)
+// #include <QtSerialPort/QSerialPort>
+// #endif
 #ifdef Q_OS_ANDROID
 #include <QtCore/QJniEnvironment>
 #endif
