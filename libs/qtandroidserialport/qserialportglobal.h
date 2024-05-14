71efca99daff2fdc3d4b405a2fc191264872dffd
Holden <68555040+HTRamsey@users.noreply.github.com>
2024-05-13 17:23:22 -0700

    QtAndroidSerialPort: Rebase on Latest QSerialPortInfo (#11525)

---
+++ libs/qtandroidserialport/qserialportglobal.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2012 Denis Shienkov <denis.shienkov@gmail.com>
+// Copyright (C) 2012 Laszlo Papp <lpapp@kde.org>
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QSERIALPORTGLOBAL_H
+#define QSERIALPORTGLOBAL_H
+
+#include <QtCore/qstring.h>
+#include <QtCore/qglobal.h>
+#include "qtserialportexports.h"
+
+#endif // QSERIALPORTGLOBAL_H
