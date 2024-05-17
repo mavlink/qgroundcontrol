17569a457bc944bcfbab440d7adcb849f6b8255a
Holden <htramsey98@gmail.com>
2024-05-17 00:41:24 -0400

    AnalyzeView: Add ULogParser UnitTest

--- src/AnalyzeView/PX4LogParser.h
+++ src/AnalyzeView/PX4LogParser.h
@@ -8,31 +8,6 @@
 
 Q_DECLARE_LOGGING_CATEGORY(PX4LogParserLog)
 
-class PX4LogParser
-{
-public:
-    PX4LogParser() = default;
-    ~PX4LogParser() = default;
-
-    bool getTagsFromLog(QByteArray& log, QList<GeoTagWorker::cameraFeedbackPacket>& cameraFeedback);
-
-private:
-    // general message header
-    static constexpr const char header[] = {(char)0xA3, (char)0x95, (char)0x00};
-
-    // header for GPOS message header
-    static constexpr const char gposHeaderHeader[] = {(char)0xA3, (char)0x95, (char)0x80, (char)0x10, (char)0x00};
-
-    // header for GPOS message
-    static constexpr const char gposHeader[] = {(char)0xA3, (char)0x95, (char)0x10, (char)0x00};
-    static constexpr const int gposOffsets[3] = {3, 7, 11};
-    static constexpr const int gposLengths[3] = {4, 4, 4};
-
-    // header for trigger message header
-    static constexpr const char triggerHeaderHeader[] = {(char)0xA3, (char)0x95, (char)0x80, (char)0x37, (char)0x00};
-
-    // header for trigger message
-    static constexpr const char triggerHeader[] = {(char)0xA3, (char)0x95, (char)0x37, (char)0x00};
-    static constexpr const int triggerOffsets[2] = {3, 11};
-    static constexpr const int triggerLengths[2] = {8, 4};
-};
+namespace PX4LogParser {
+    bool getTagsFromLog(const QByteArray &log, QList<GeoTagWorker::cameraFeedbackPacket> &cameraFeedback);
+}
