c8c3f5b33e85b7f4b68e748ef37726acf15d4847
Holden <htramsey98@gmail.com>
2024-05-31 09:03:39 -0400

    Tools: Update Linux Dependencies

--- tools/setup/install-dependencies-debian.sh
+++ tools/setup/install-dependencies-debian.sh
@@ -1,11 +1,12 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 
-apt-get update -y --quiet
+apt update -y --quiet
 
 #Build Tools
-DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+DEBIAN_FRONTEND=noninteractive apt -y --quiet --no-install-recommends install \
+	binutils \
 	build-essential \
 	ccache \
 	cmake \
@@ -16,45 +17,72 @@ DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends instal
 	gdb \
 	git \
 	libfuse2 \
+	libfuse3-3 \
+	libtool \
+	locales \
 	make \
 	ninja-build \
-	rsync \
-    binutils \
-    locales \
-    patchelf
+	patchelf \
+	pkgconf \
+	rsync
 
-#Qt Required
-DEBIAN_FRONTEND=noninteractive apt-get -y --quiet install \
+#Qt Required - https://doc.qt.io/qt-6/linux-requirements.html
+DEBIAN_FRONTEND=noninteractive apt -y --quiet install \
+	libfontconfig1 \
+	libfreetype6 \
+	libx11-6 \
+	libx11-xcb1 \
+	libxcb-cursor0 \
+	libxcb-glx0 \
+	libxcb-icccm4 \
+	libxcb-image0 \
+	libxcb-keysyms1 \
+	libxcb-randr0 \
+	libxcb-render-util0 \
+	libxcb-render0 \
+	libxcb-shape0 \
+	libxcb-shm0 \
+	libxcb-sync1 \
+	libxcb-util1 \
+	libxcb-xfixes0 \
 	libxcb-xinerama0 \
-    libxkbcommon-x11-0 \
-    libxcb-cursor0
+	libxcb-xkb1 \
+	libxcb1 \
+	libxext6 \
+	libxfixes3 \
+	libxi6 \
+	libxkbcommon-x11-0 \
+	libxkbcommon0 \
+	libxrender1
 
-#QGC
-DEBIAN_FRONTEND=noninteractive apt-get -y --quiet install \
-	libsdl2-dev \
-	libspeechd2 \
+#Qt Additional
+DEBIAN_FRONTEND=noninteractive apt -y --quiet install \
 	flite \
+	libgl1-mesa-dev \
+	libpulse-dev \
+	libspeechd2 \
+	libzstd-dev \
 	speech-dispatcher \
 	speech-dispatcher-flite
 
+#QGC
+DEBIAN_FRONTEND=noninteractive apt -y --quiet install \
+	libsdl2-dev
+
 #GStreamer
-DEBIAN_FRONTEND=noninteractive apt-get -y --quiet install \
-	libgstreamer1.0-dev \
-	libgstreamer-plugins-base1.0-dev \
-	libgstreamer-plugins-good1.0-dev \
-	libgstreamer-plugins-bad1.0-dev \
+DEBIAN_FRONTEND=noninteractive apt -y --quiet install \
+	libgstreamer1.0-0 \
+	libgstreamer-plugins-bad1.0-0 \
+	libgstreamer-plugins-base1.0-0 \
+	libgstreamer-plugins-good1.0-0 \
+	libgstreamer-gl1.0-0 \
+	gstreamer1.0-plugins-bad \
 	gstreamer1.0-plugins-base \
 	gstreamer1.0-plugins-good \
-	gstreamer1.0-plugins-bad \
 	gstreamer1.0-plugins-ugly \
 	gstreamer1.0-plugins-rtp \
+	gstreamer1.0-gl \
 	gstreamer1.0-libav \
-	gstreamer1.0-tools \
-	gstreamer1.0-x \
-	gstreamer1.0-alsa \
-	gstreamer1.0-gl \
-	gstreamer1.0-gtk3 \
-	gstreamer1.0-gl \
-	gstreamer1.0-vaapi \
 	gstreamer1.0-rtsp \
-    libdrm-dev
+	gstreamer1.0-vaapi \
+	gstreamer1.0-x
