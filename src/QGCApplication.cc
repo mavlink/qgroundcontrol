993ea034cc311d81dcf339b3b2e508b9ddcfe394
Holden <htramsey98@gmail.com>
2024-06-04 02:31:24 -0400

    CMake: Add LGPL Option

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -100,9 +100,9 @@
 #include "CustomActionManager.h"
 #include "AudioOutput.h"
 #include "JsonHelper.h"
-// #ifdef QGC_VIEWER3D
+#ifndef QGC_DISABLE_VIEWER3D
 #include "Viewer3DManager.h"
-// #endif
+#endif
 #ifndef NO_SERIAL_LINK
 #include "FirmwareUpgradeController.h"
 #include "SerialLink.h"
@@ -344,9 +344,9 @@ void QGCApplication::init()
     qmlRegisterSingletonType<ScreenToolsController>  ("QGroundControl.ScreenToolsController", 1, 0, "ScreenToolsController",  screenToolsControllerSingletonFactory);
 
 
-    // #ifdef QGC_VIEWER3D
+#ifndef QGC_DISABLE_VIEWER3D
     Viewer3DManager::registerQmlTypes();
-    // #endif
+#endif
 
     qmlRegisterUncreatableType<Autotune>              ("QGroundControl.Vehicle",   1, 0, "Autotune",               "Reference only");
     qmlRegisterUncreatableType<RemoteIDManager>       ("QGroundControl.Vehicle",   1, 0, "RemoteIDManager",        "Reference only");
