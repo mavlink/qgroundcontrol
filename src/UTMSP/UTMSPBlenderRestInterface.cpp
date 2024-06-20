5118283601953b08311a691030512e6bce72f521
rpashchapur <ravi.pashchapur@tii.ae>
2024-06-20 18:28:07 +0400

    Refactor UTMSP service files

--- src/UTMSP/UTMSPBlenderRestInterface.cpp
+++ src/UTMSP/UTMSPBlenderRestInterface.cpp
@@ -15,25 +15,34 @@ UTMSPBlenderRestInterface::UTMSPBlenderRestInterface(QObject *parent):
     setHost("BlenderClient");
 }
 
-QPair<int, QString> UTMSPBlenderRestInterface::setFlightPlan(const QString& body)
+QPair<int, std::string> UTMSPBlenderRestInterface::setFlightPlan(const std::string& body)
 {
     // Post Flight plan
     QString setFlightPlanTarget = "/flight_declaration_ops/set_flight_declaration";
-    modifyRequest(setFlightPlanTarget, QNetworkAccessManager::PostOperation, body);
+    modifyRequest(setFlightPlanTarget, QNetworkAccessManager::PostOperation, QString::fromStdString(body));
 
     return executeRequest();
 }
 
-QPair<int, QString> UTMSPBlenderRestInterface::requestTelemetry(const QString& body)
+QPair<int, std::string> UTMSPBlenderRestInterface::requestTelemetry(const std::string& body)
 {
     // Post RID data
     QString target = "/flight_stream/set_telemetry";
-    modifyRequest(target, QNetworkAccessManager::PutOperation, body);
+    modifyRequest(target, QNetworkAccessManager::PutOperation, QString::fromStdString(body));
 
     return executeRequest();
 }
 
-QPair<int, QString> UTMSPBlenderRestInterface::ping()
+QPair<int, std::string> UTMSPBlenderRestInterface::updateFlightState(const std::string& body, const std::string &flightID)
+{
+    // Post RID data
+    QString target = "/flight_declaration_ops/flight_declaration_state/" + QString::fromStdString(flightID);
+    modifyRequest(target, QNetworkAccessManager::PutOperation, QString::fromStdString(body));
+
+    return executeRequest();
+}
+
+QPair<int, std::string> UTMSPBlenderRestInterface::ping()
 {
     QString target = "/ping";
     modifyRequest(target, QNetworkAccessManager::GetOperation);
