ce6cad12cfd0d2d23ed5c8654c5db80061fd7e3a
A908gao <a908gao@126.com>
2024-06-21 17:11:33 +0800

    Update FirmwareUpgradeController.cc
    Greetings, I am a member of the development team at Siyi Technology. I am currently seeking to contribute to the online upgrade support for the Siyi N7 flight controller's PX4 firmware. My colleague has communicated with your team via Discord. I appreciate your attention to this commit,thanks!

--- src/VehicleSetup/FirmwareUpgradeController.cc
+++ src/VehicleSetup/FirmwareUpgradeController.cc
@@ -99,7 +99,8 @@ static QMap<int, QString> px4_board_name_map {
     {1048, "holybro_kakuteh7_default"},
     {1053, "holybro_kakuteh7v2_default"},
     {1054, "holybro_kakuteh7mini_default"},
-    {1110, "jfb_jfb110_default"},    
+    {1110, "jfb_jfb110_default"},
+    {1123, "siyi_n7_default"},    
 };
 
 uint qHash(const FirmwareUpgradeController::FirmwareIdentifier& firmwareId)
