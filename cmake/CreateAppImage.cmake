28b88a5cb3b2ac71459c8c35d00adf12cfab2933
Holden <htramsey98@gmail.com>
2024-06-24 12:30:13 -0400

    CMake: Add New Installers

--- cmake/CreateAppImage.cmake
+++ cmake/CreateAppImage.cmake
@@ -1,9 +1,12 @@
 message(STATUS "Creating AppImage")
+# TODO: https://github.com/AppImageCommunity/AppImageUpdate
 
 set(APPDIR_PATH "${CMAKE_BINARY_DIR}/AppDir")
 set(LD_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-x86_64.AppImage")
 set(LD_APPIMAGEPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-appimage-x86_64.AppImage")
 set(LD_QTPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-qt-x86_64.AppImage")
+# set(LD_GSTPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-gstreamer.sh")
+# set(LD_GTKPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-gtk.sh")
 
 if(NOT EXISTS "${LD_PATH}")
     file(DOWNLOAD https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage "${LD_PATH}")
@@ -17,5 +20,13 @@ if(NOT EXISTS "${LD_QTPLUGIN_PATH}")
     file(DOWNLOAD https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage "${LD_QTPLUGIN_PATH}")
     execute_process(COMMAND chmod a+x "${LD_QTPLUGIN_PATH}")
 endif()
+# if(NOT EXISTS "${LD_GTKPLUGIN_PATH}")
+#     file(DOWNLOAD https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh "${LD_GTKPLUGIN_PATH}")
+#     execute_process(COMMAND chmod a+x "${LD_GTKPLUGIN_PATH}")
+# endif()
+# if(NOT EXISTS "${LD_GSTPLUGIN_PATH}")
+#     file(DOWNLOAD https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gstreamer/master/linuxdeploy-plugin-gstreamer.sh "${LD_GSTPLUGIN_PATH}")
+#     execute_process(COMMAND chmod a+x "${LD_GSTPLUGIN_PATH}")
+# endif()
 
-execute_process(COMMAND ${LD_PATH} --appdir ${APPDIR_PATH} --output appimage) # --plugin qt
+execute_process(COMMAND ${LD_PATH} --appdir ${APPDIR_PATH} --output appimage) # --plugin qt --plugin gtk --plugin gstreamer
