42f267142421e6f919dc0523083f12d7be27f186
Holden <htramsey98@gmail.com>
2024-07-30 11:41:10 -0400

    CMake: Fix Git Version Without Tag

--- cmake/Git.cmake
+++ cmake/Git.cmake
@@ -22,6 +22,22 @@ endif()
 include(CMakePrintHelpers)
 
 execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref @
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE QGC_GIT_BRANCH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+cmake_print_variables(QGC_GIT_BRANCH)
+
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --short @
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE QGC_GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+cmake_print_variables(QGC_GIT_HASH)
+
+execute_process(
     COMMAND ${GIT_EXECUTABLE} describe --always --tags
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE QGC_APP_VERSION_STR
@@ -35,16 +51,23 @@ execute_process(
     OUTPUT_VARIABLE QGC_APP_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+cmake_print_variables(QGC_APP_VERSION)
 
 execute_process(
     COMMAND ${GIT_EXECUTABLE} log -1 --format=%aI ${QGC_APP_VERSION}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE REL_DATE
+    OUTPUT_VARIABLE QGC_REL_DATE
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-cmake_print_variables(REL_DATE)
+cmake_print_variables(QGC_REL_DATE)
 
-string(REPLACE "v" "" QGC_APP_VERSION ${QGC_APP_VERSION})
+string(FIND ${QGC_APP_VERSION} "v" QGC_APP_VERSION_VALID)
+cmake_print_variables(QGC_APP_VERSION_VALID)
+if(QGC_APP_VERSION_VALID GREATER -1)
+    string(REPLACE "v" "" QGC_APP_VERSION ${QGC_APP_VERSION})
+else()
+    set(QGC_APP_VERSION "0.0.0")
+endif()
 string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" QGC_APP_VERSION_MATCH ${QGC_APP_VERSION})
 set(QGC_APP_VERSION_MAJOR ${CMAKE_MATCH_1})
 set(QGC_APP_VERSION_MINOR ${CMAKE_MATCH_2})
