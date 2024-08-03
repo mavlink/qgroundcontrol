dd1a6979df014ca0dea078b0994c57943e06633b
Holden <htramsey98@gmail.com>
2024-08-03 01:45:27 -0400

    AnalyzeView: Add ulog_cpp dependency
    Former-commit-id: e9eac451acbfeddb0270b53212d40767431b0962

--- src/AnalyzeView/ULogParser.h
+++ src/AnalyzeView/ULogParser.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#include <QtCore/QByteArray>
 #include <QtCore/QList>
-#include <QtCore/QString>
 #include <QtCore/QLoggingCategory>
 
 #include "GeoTagWorker.h"
 
+class QByteArray;
+class QString;
+
 Q_DECLARE_LOGGING_CATEGORY(ULogParserLog)
 
 namespace ULogParser {
-    /// @return true: failed, errorMessage set
+    /// Get GeoTags from a ULog
+    ///     @return true if failed, errorMessage set
     bool getTagsFromLog(const QByteArray &log, QList<GeoTagWorker::cameraFeedbackPacket> &cameraFeedback, QString &errorMessage);
-}
+} // namespace ULogParser
