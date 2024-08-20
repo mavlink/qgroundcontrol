b8334c5927355164ce223b70e46496dfb2fa1edf
Holden <htramsey98@gmail.com>
2024-08-19 23:06:21 -0400

    AnalzeView: Use easyexif library

--- src/AnalyzeView/ExifParser.h
+++ src/AnalyzeView/ExifParser.h
@@ -1,13 +1,14 @@
 #pragma once
 
-#include <QtCore/QByteArray>
 #include <QtCore/QLoggingCategory>
 
 #include "GeoTagWorker.h"
 
+class QByteArray;
+
 Q_DECLARE_LOGGING_CATEGORY(ExifParserLog)
 
 namespace ExifParser {
-    double readTime(const QByteArray& buf);
-    bool write(QByteArray& buf, const GeoTagWorker::cameraFeedbackPacket& geotag);
-};
+    double readTime(const QByteArray &buf);
+    bool write(QByteArray &buf, const GeoTagWorker::cameraFeedbackPacket &geotag);
+} // namespace ExifParser
