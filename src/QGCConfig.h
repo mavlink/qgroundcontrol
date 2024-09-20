02ef4ff1ffafd8f2e81203cd8aa5ac7a8589ab6d
Holden <htramsey98@gmail.com>
2024-09-20 10:59:19 -0400

    Tools: Create File Header Updater Tool

--- src/QGCConfig.h
+++ src/QGCConfig.h
@@ -1,3 +1,12 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
 #pragma once
 
 // If you need to make an incompatible changes to stored settings, bump this version number
--- src/Viewer3D/CityMapGeometry.cc
+++ src/Viewer3D/CityMapGeometry.cc
@@ -1,3 +1,12 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
 #include "CityMapGeometry.h"
 #include "QGCApplication.h"
 #include "SettingsManager.h"
