396a71959552a6ccbf2ec741c8aaa1442a26207f
Mate Soos <soos.mate@gmail.com>
2024-09-27 23:02:11 +0200

    Fixing channel issue with ELRS MavLink-RC

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -1434,6 +1434,16 @@ void Vehicle::_handleRCChannels(mavlink_message_t& message)
     };
     int pwmValues[QGCMAVLink::maxRcChannels];
 
+    // Below is a hack that's needed by ELRS
+    // ELRS is not sending a full RC_CHANNELS packet, only channel update
+    // packets via RC_CHANNELS_RAW, to update the position of the values.
+    // Therefore, the number of channels is not set.
+    if (channels.chancount == 0) {
+        for(const auto& channelValue : _rgChannelvalues) {
+            if (*channelValue != UINT16_MAX) channels.chancount++;
+        }
+    }
+
     for (int i=0; i<QGCMAVLink::maxRcChannels; i++) {
         uint16_t channelValue = *_rgChannelvalues[i];
 
