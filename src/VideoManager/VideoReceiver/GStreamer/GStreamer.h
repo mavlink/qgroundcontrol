9686b0b9e3805b55318a85c24f26fb1eb2515ffc
Holden <htramsey98@gmail.com>
2024-10-06 04:33:26 -0400

    Video: GStreamer Plugin Improvements

--- src/VideoManager/VideoReceiver/GStreamer/GStreamer.h
+++ src/VideoManager/VideoReceiver/GStreamer/GStreamer.h
@@ -10,20 +10,27 @@
 #pragma once
 
 #include <QtCore/QLoggingCategory>
-#include <QtQuick/QQuickItem>
-
-#include "Settings/VideoDecoderOptions.h"
 
 Q_DECLARE_LOGGING_CATEGORY(GStreamerLog)
 Q_DECLARE_LOGGING_CATEGORY(GStreamerAPILog)
 
 class VideoReceiver;
+class QQuickItem;
 
-class GStreamer {
-public:
-    static void blacklist(VideoDecoderOptions option);
-    static void initialize(int argc, char* argv[], int debuglevel);
-    static void* createVideoSink(QObject* parent, QQuickItem* widget);
-    static void releaseVideoSink(void* sink);
-    static VideoReceiver* createVideoReceiver(QObject* parent);
+namespace GStreamer
+{
+enum VideoDecoderOptions {
+    ForceVideoDecoderDefault = 0,
+    ForceVideoDecoderSoftware,
+    ForceVideoDecoderNVIDIA,
+    ForceVideoDecoderVAAPI,
+    ForceVideoDecoderDirectX3D,
+    ForceVideoDecoderVideoToolbox,
+};
+
+void initialize();
+void blacklist(VideoDecoderOptions option);
+void *createVideoSink(QObject *parent, QQuickItem *widget);
+void releaseVideoSink(void *sink);
+VideoReceiver *createVideoReceiver(QObject *parent = nullptr);
 };
