b4e4061b3931be30d6e30355c8dd6b3d16d8e0c7
Holden <htramsey98@gmail.com>
2024-10-08 02:26:13 -0400

    Utilities: Cleanup File Classes
    Former-commit-id: 077c72cd317c2046406476bcf21fc85d2493997c

---
+++ test/Utilities/QGCFileDownloadTest.cc
@@ -0,0 +1,15 @@
+#include "QGCFileDownloadTest.h"
+#include "QGCCachedFileDownload.h"
+#include "QGCFileDownload.h"
+
+#include <QtTest/QTest>
+
+void QGCFileDownloadTest::_testFileDownload()
+{
+    QGCFileDownload file;
+}
+
+void QGCFileDownloadTest::_testCachedFileDownload()
+{
+    QGCCachedFileDownload file("");
+}
