c4fea729caa596dad69a94f329caff87e89d2d56
Eric Johnson <eric.emj.johnson@gmail.com>
2024-10-14 01:37:53 -0500

    WIP: more changes to make build more similar to CI

--- deploy/docker/run-docker-android.sh
+++ deploy/docker/run-docker-android.sh
@@ -5,7 +5,8 @@ set -e
 # Build the Docker image for Android
 docker build --file ./deploy/docker/Dockerfile-build-android -t qgc-android-docker .
 
-docker run --rm -v ${PWD}:/project/source -v ${PWD}/build:/workspace qgc-android-docker
+# Run the Docker container with adjusted mount points
+docker run --rm -v ${PWD}:/project/source -v ${PWD}/build:/workspace/build qgc-android-docker
 
 # Add Git safe directory setting
 # docker run --rm qgc-android-docker git config --global --add safe.directory /project/source
