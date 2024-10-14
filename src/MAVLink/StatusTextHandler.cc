0fbfd4e26bab07b8a432f8eb0936806335edf6c5
Don Gagne <don@thegagnes.com>
2024-10-14 10:44:43 -0700

    Fix status text escaping/signalling
    Regular status text messages should move around system un-formatted
    
    Add missing textMessageReceived signaling
    
    Former-commit-id: 4165a0d656ea317c558ead8bc7eec791b4f7ba32

--- src/MAVLink/StatusTextHandler.cc
+++ src/MAVLink/StatusTextHandler.cc
@@ -137,9 +137,10 @@ void StatusTextHandler::resetErrorLevelMessages()
     }
 }
 
-void StatusTextHandler::handleTextMessage(MAV_COMPONENT compId, MAV_SEVERITY severity, const QString &text, const QString &description)
+void StatusTextHandler::handleHTMLEscapedTextMessage(MAV_COMPONENT compId, MAV_SEVERITY severity, const QString &text, const QString &description)
 {
     QString htmlText(text);
+
     (void) htmlText.replace("\n", "<br/>");
 
     // TODO: handle text + description separately in the UI
@@ -335,7 +336,7 @@ void StatusTextHandler::_chunkedStatusTextCompleted(MAV_COMPONENT compId)
 
     (void) m_chunkedStatusTextInfoMap.remove(compId);
 
-    emit textMessageReceived(compId, severity, messageText.toHtmlEscaped(), "");
+    emit textMessageReceived(compId, severity, messageText, "");
 }
 
 void StatusTextHandler::_handleTextMessage(uint32_t newCount, MessageType messageType)
