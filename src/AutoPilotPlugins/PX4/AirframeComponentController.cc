1cb5ceda7b26e71d715ca6d8307c32649d0f7aca
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-10-14 16:23:15 -0800

    fix application crash when selecting Airframe type
    Former-commit-id: d36ae1961482065e214b450f4c3a18630dc3ad2d

--- src/AutoPilotPlugins/PX4/AirframeComponentController.cc
+++ src/AutoPilotPlugins/PX4/AirframeComponentController.cc
@@ -90,7 +90,7 @@ void AirframeComponentController::changeAutostart(void)
 		return;
 	}
 
-    QGuiApplication::overrideCursor()->setShape(Qt::WaitCursor);
+    QGuiApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
     
     Fact* sysAutoStartFact  = getParameterFact(-1, "SYS_AUTOSTART");
     Fact* sysAutoConfigFact = getParameterFact(-1, "SYS_AUTOCONFIG");
@@ -100,7 +100,7 @@ void AirframeComponentController::changeAutostart(void)
     connect(sysAutoStartFact, &Fact::vehicleUpdated, this, &AirframeComponentController::_waitParamWriteSignal);
     connect(sysAutoConfigFact, &Fact::vehicleUpdated, this, &AirframeComponentController::_waitParamWriteSignal);
     
-    // We use forceSetValue to params are sent even if the previous value is that same as the new value
+    // We use forceSetValue to ensure params are sent even if the previous value is that same as the new value
     sysAutoStartFact->forceSetRawValue(_autostartId);
     sysAutoConfigFact->forceSetRawValue(1);
 }
