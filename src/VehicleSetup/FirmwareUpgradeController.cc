01167b5fe66fa960cca776bf638ebc07b8afa29a
alexklimaj <alex@arkelectron.com>
2024-10-18 16:45:32 -0600

    boards:  add ARK FPV

--- src/VehicleSetup/FirmwareUpgradeController.cc
+++ src/VehicleSetup/FirmwareUpgradeController.cc
@@ -62,6 +62,7 @@ static QMap<int, QString> px4_board_name_map {
     {54, "px4_fmu-v6u_default"},
     {56, "px4_fmu-v6c_default"},
     {57, "ark_fmu-v6x_default"},
+    {59, "ark_fpv_default"},
     {35, "px4_fmu-v6xrt_default"},
     {55, "sky-drones_smartap-airlink_default"},
     {88, "airmind_mindpx-v2_default"},
