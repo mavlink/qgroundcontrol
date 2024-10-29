9f0e5f777c2aeb039258e399152870cf3046892f
Holden <htramsey98@gmail.com>
2024-10-29 04:02:00 -0400

    Utilities: Fix decompression of firmware downloads
    Former-commit-id: 486b7af5a9fdb7f274060ed07baa9cffe4a3f11e

--- src/Utilities/Compression/QGCZlib.h
+++ src/Utilities/Compression/QGCZlib.h
@@ -14,9 +14,11 @@
 
 Q_DECLARE_LOGGING_CATEGORY(QGCZlibLog)
 
-namespace QGCZlib {
+namespace QGCZlib
+{
     /// Decompresses the specified file to the specified directory
     ///     @param gzippedFileName      Fully qualified path to gzip file
     ///     @param decompressedFilename Fully qualified path to for file to decompress to
+    /// @return bool Success
     bool inflateGzipFile(const QString &gzippedFileName, const QString &decompressedFilename);
-} // namespace QGCZlib
+}
