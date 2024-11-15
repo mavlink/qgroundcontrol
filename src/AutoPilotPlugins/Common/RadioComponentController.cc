399125a4630e022f7962df7f569c7b0eaa56ce74
benjinne <benjinne@gmail.com>
2024-11-15 16:16:29 -0500

    bind RC use enums
    Former-commit-id: 3a92c45ab36b66c445bee6b11ccca56835106754

--- src/AutoPilotPlugins/Common/RadioComponentController.cc
+++ src/AutoPilotPlugins/Common/RadioComponentController.cc
@@ -605,12 +605,12 @@ void RadioComponentController::_setInternalCalibrationValuesFromParameters(void)
 
 void RadioComponentController::spektrumBindMode(int mode)
 {
-    _vehicle->pairRX(0, mode);
+    _vehicle->pairRX(RC_TYPE_SPEKTRUM, mode);
 }
 
 void RadioComponentController::crsfBindMode()
 {
-    _vehicle->pairRX(1, 0);
+    _vehicle->pairRX(RC_TYPE_CRSF, 0);
 }
 
 /// @brief Validates the current settings against the calibration rules resetting values as necessary.
