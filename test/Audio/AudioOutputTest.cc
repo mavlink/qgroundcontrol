04fd964bfeaff981239e8d8771f6ee07ca52aaf8
Holden <htramsey98@gmail.com>
2024-11-21 16:59:47 -0500

    AudioOutput: Fix queue size and improve thread safety

--- test/Audio/AudioOutputTest.cc
+++ test/Audio/AudioOutputTest.cc
@@ -12,23 +12,24 @@
 
 #include <QtTest/QTest>
 
-AudioOutputTest::AudioOutputTest(void)
+void AudioOutputTest::_testSpokenReplacements()
 {
-
-}
-
-void AudioOutputTest::_testSpokenReplacements(void)
-{
-    QString result = AudioOutput::fixTextMessageForAudio(QStringLiteral("-10.5m, -10.5m. -10.5 m"));
-    QCOMPARE(result, QStringLiteral(" negative 10 point 5 meters,  negative 10 point 5 meters.  negative 10 point 5  meters"));
-    result = AudioOutput::fixTextMessageForAudio(QStringLiteral("-10m -10 m"));
-    QCOMPARE(result, QStringLiteral(" negative 10 meters  negative 10  meters"));
-    result = AudioOutput::fixTextMessageForAudio(QStringLiteral("foo -10m -10 m bar"));
-    QCOMPARE(result, QStringLiteral("foo  negative 10 meters  negative 10  meters bar"));
-    result = AudioOutput::fixTextMessageForAudio(QStringLiteral("-foom"));
+    QString result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("-10.5m, -10.5m. -10.5 m"));
+    QCOMPARE(result, QStringLiteral("negative 10 point 5 meters, negative 10 point 5 meters. negative 10 point 5  meters"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("-10m -10 m"));
+    QCOMPARE(result, QStringLiteral("negative 10 meters negative 10  meters"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("foo -10m -10 m bar"));
+    QCOMPARE(result, QStringLiteral("foo negative 10 meters negative 10  meters bar"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("-foom"));
     QCOMPARE(result, QStringLiteral("-foom"));
-    result = AudioOutput::fixTextMessageForAudio(QStringLiteral("10 moo"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("10 moo"));
     QCOMPARE(result, QStringLiteral("10 moo"));
-    result = AudioOutput::fixTextMessageForAudio(QStringLiteral("10moo"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("10moo"));
     QCOMPARE(result, QStringLiteral("10moo"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("10ms"));
+    QCOMPARE(result, QStringLiteral("10ms"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("1000ms"));
+    QCOMPARE(result, QStringLiteral("1 second"));
+    result = AudioOutput::_fixTextMessageForAudio(QStringLiteral("1001ms"));
+    QCOMPARE(result, QStringLiteral("1 second and 1 millisecond"));
 }
