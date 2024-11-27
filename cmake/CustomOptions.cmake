51740c9fc2d2e53622d230d1addf093c9c25a6d9
Don Gagne <don@thegagnes.com>
2024-11-27 11:32:11 -0800

    Reorganize into general and targeted sections

--- cmake/CustomOptions.cmake
+++ cmake/CustomOptions.cmake
@@ -1,4 +1,7 @@
 # The following options can be overriden by custom builds using the CustomOverrides.cmake file
+
+# General
+
 set(QGC_APP_NAME "QGroundControl" CACHE STRING "App Name")
 set(QGC_APP_COPYRIGHT "Copyright (c) 2024 QGroundControl. All rights reserved." CACHE STRING "Copyright")
 set(QGC_APP_DESCRIPTION "Open Source Ground Control App" CACHE STRING "Description")
@@ -6,19 +9,6 @@ set(QGC_ORG_NAME "QGroundControl.org" CACHE STRING "Org Name")
 set(QGC_ORG_DOMAIN "org.qgroundcontrol" CACHE STRING "Domain")
 
 option(QGC_STABLE_BUILD "Stable Build" OFF)
-
-# MacOS
-set(QGC_BUNDLE_ID "org.qgroundcontrol.QGroundControl" CACHE STRING "MacOS Bundle ID") # MACOS
-set(QGC_MACOS_ICON_PATH "${CMAKE_SOURCE_DIR}/deploy/mac" CACHE PATH "MacOS Icon Path") # MACOS
-
-# APM
-option(QGC_DISABLE_APM_MAVLINK "Disable APM Dialect" OFF)
-option(QGC_DISABLE_APM_PLUGIN "Disable APM Plugin" OFF)
-option(QGC_DISABLE_APM_PLUGIN_FACTORY "Disable APM Plugin Factory" OFF)
-
-# PX4
-option(QGC_DISABLE_PX4_PLUGIN "Disable PX4 Plugin" OFF)
-option(QGC_DISABLE_PX4_PLUGIN_FACTORY "Disable PX4 Plugin Factory" OFF)
 
 option(QGC_ENABLE_BLUETOOTH "Enable Bluetooth Links" ON) # Qt6Bluetooth_FOUND
 option(QGC_ZEROCONF_ENABLED "Enable ZeroConf Compatibility" OFF)
@@ -34,3 +24,17 @@ option(QGC_ENABLE_QT_VIDEOSTREAMING "Enable QtMultimedia Video Backend" OFF) # Q
 
 set(QGC_MAVLINK_GIT_REPO "https://github.com/mavlink/c_library_v2.git" CACHE STRING "URL to MAVLink Git Repo")
 set(QGC_MAVLINK_GIT_TAG "4db2f67156d996eae90ef437a73353468d850407" CACHE STRING "Tag of MAVLink Git Repo")
+
+# MacOS
+set(QGC_BUNDLE_ID "org.qgroundcontrol.QGroundControl" CACHE STRING "MacOS Bundle ID") # MACOS
+set(QGC_MACOS_ICON_PATH "${CMAKE_SOURCE_DIR}/deploy/mac" CACHE PATH "MacOS Icon Path") # MACOS
+
+# APM
+option(QGC_DISABLE_APM_MAVLINK "Disable APM Dialect" OFF)
+option(QGC_DISABLE_APM_PLUGIN "Disable APM Plugin" OFF)
+option(QGC_DISABLE_APM_PLUGIN_FACTORY "Disable APM Plugin Factory" OFF)
+
+# PX4
+option(QGC_DISABLE_PX4_PLUGIN "Disable PX4 Plugin" OFF)
+option(QGC_DISABLE_PX4_PLUGIN_FACTORY "Disable PX4 Plugin Factory" OFF)
+
