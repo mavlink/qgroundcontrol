1c4027b062b948cee2ea972a28ce9bcb9c4d2f31
Don Gagne <don@thegagnes.com>
2024-12-04 10:46:37 -0800

    Add two phase init to QGCCorePlugin
    Former-commit-id: 99a7c6fcb73f9848a1a943afcef3280fec3ad806

--- src/API/QGCCorePlugin.h
+++ src/API/QGCCorePlugin.h
@@ -63,6 +63,8 @@ public:
     static QGCCorePlugin *instance();
     static void registerQmlTypes();
 
+    virtual void init() { };
+
     Q_INVOKABLE bool guidedActionsControllerLogging() const;
 
     /// The list of pages/buttons under the Analyze Menu
