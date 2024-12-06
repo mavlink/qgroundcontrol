d43c36bdd3a143ce66b5657579b8c0999ff548de
davidsastresas <davidsastresas@gmail.com>
2024-12-06 10:32:22 +0100

    FlyviewSettings: add new requestControlTimeout setting

--- src/Settings/FlyViewSettings.h
+++ src/Settings/FlyViewSettings.h
@@ -33,4 +33,5 @@ public:
     DEFINE_SETTINGFACT(updateHomePosition)
     DEFINE_SETTINGFACT(instrumentQmlFile)
     DEFINE_SETTINGFACT(requestControlAllowTakeover)
+    DEFINE_SETTINGFACT(requestControlTimeout)
 };
