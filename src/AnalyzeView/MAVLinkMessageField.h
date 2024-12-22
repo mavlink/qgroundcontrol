941906d10cc928ee2de283820f377d23f52d4089
Holden <htramsey98@gmail.com>
2024-12-22 06:20:29 -0500

    MAVLink: update analyze view classes
    Former-commit-id: c1b3944d5e20e94e5e3b1038fe5f0f69da9286ab

--- src/AnalyzeView/MAVLinkMessageField.h
+++ src/AnalyzeView/MAVLinkMessageField.h
@@ -7,16 +7,12 @@
  *
  ****************************************************************************/
 
-/// @file
-/// @brief MAVLink message inspector and charting controller
-/// @author Gus Grubba <gus@auterion.com>
-
 #pragma once
 
-#include <QtCore/QObject>
-#include <QtCore/QString>
-#include <QtCore/QPointF>
 #include <QtCore/QLoggingCategory>
+#include <QtCore/QObject>
+#include <QtCore/QPointF>
+#include <QtCore/QString>
 #include <QtQmlIntegration/QtQmlIntegration>
 
 Q_DECLARE_LOGGING_CATEGORY(MAVLinkMessageFieldLog)
@@ -25,60 +21,59 @@ class QGCMAVLinkMessage;
 class MAVLinkChartController;
 class QAbstractSeries;
 
-//-----------------------------------------------------------------------------
-/// MAVLink message field
 class QGCMAVLinkMessageField : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
     Q_MOC_INCLUDE(<QtCharts/QAbstractSeries>)
+    Q_PROPERTY(QString                  name        READ name       CONSTANT)
+    Q_PROPERTY(QString                  label       READ label      CONSTANT)
+    Q_PROPERTY(QString                  type        READ type       CONSTANT)
+    Q_PROPERTY(QString                  value       READ value      NOTIFY valueChanged)
+    Q_PROPERTY(bool                     selectable  READ selectable NOTIFY selectableChanged)
+    Q_PROPERTY(int                      chartIndex  READ chartIndex CONSTANT)
+    Q_PROPERTY(const QAbstractSeries    *series     READ series     NOTIFY seriesChanged)
 
 public:
-    Q_PROPERTY(QString          name        READ name       CONSTANT)
-    Q_PROPERTY(QString          label       READ label      CONSTANT)
-    Q_PROPERTY(QString          type        READ type       CONSTANT)
-    Q_PROPERTY(QString          value       READ value      NOTIFY valueChanged)
-    Q_PROPERTY(bool             selectable  READ selectable NOTIFY selectableChanged)
-    Q_PROPERTY(int              chartIndex  READ chartIndex CONSTANT)
-    Q_PROPERTY(QAbstractSeries* series      READ series     NOTIFY seriesChanged)
+    QGCMAVLinkMessageField(const QString &name, const QString &type, QGCMAVLinkMessage *parent = nullptr);
+    ~QGCMAVLinkMessageField();
 
-    QGCMAVLinkMessageField(QGCMAVLinkMessage* parent, QString name, QString type);
+    QString name() const { return _name;  }
+    QString label() const;
+    QString type() const { return _type;  }
+    QString value() const { return _value; }
+    bool selectable() const { return _selectable; }
+    bool selected() const { return !!_pSeries; }
+    const QAbstractSeries *series() const { return _pSeries; }
+    const QList<QPointF> *values() const { return &_values; }
+    qreal rangeMin() const { return _rangeMin; }
+    qreal rangeMax() const { return _rangeMax; }
+    int chartIndex() const;
 
-    QString         name            () { return _name;  }
-    QString         label           ();
-    QString         type            () { return _type;  }
-    QString         value           () { return _value; }
-    bool            selectable      () const{ return _selectable; }
-    bool            selected        () { return _pSeries != nullptr; }
-    QAbstractSeries*series          () { return _pSeries; }
-    QList<QPointF>* values          () { return &_values;}
-    qreal           rangeMin        () const{ return _rangeMin; }
-    qreal           rangeMax        () const{ return _rangeMax; }
-    int             chartIndex      ();
+    void setSelectable(bool sel);
+    void updateValue(const QString &newValue, qreal v);
 
-    void            setSelectable   (bool sel);
-    void            updateValue     (QString newValue, qreal v);
-
-    void            addSeries       (MAVLinkChartController* chart, QAbstractSeries* series);
-    void            delSeries       ();
-    void            updateSeries    ();
+    void addSeries(MAVLinkChartController *chart, QAbstractSeries *series);
+    void delSeries();
+    void updateSeries();
 
 signals:
-    void            seriesChanged       ();
-    void            selectableChanged   ();
-    void            valueChanged        ();
+    void seriesChanged();
+    void selectableChanged();
+    void valueChanged();
 
 private:
-    QString     _type;
-    QString     _name;
-    QString     _value;
-    bool        _selectable = true;
-    int         _dataIndex  = 0;
-    qreal       _rangeMin   = 0;
-    qreal       _rangeMax   = 0;
+    QString _type;
+    QString _name;
+    QGCMAVLinkMessage *_msg = nullptr;
 
-    QAbstractSeries*    _pSeries = nullptr;
-    QGCMAVLinkMessage*  _msg     = nullptr;
-    MAVLinkChartController*      _chart   = nullptr;
-    QList<QPointF>      _values;
+    QString _value;
+    bool _selectable = true;
+    int _dataIndex = 0;
+    qreal _rangeMin = 0;
+    qreal _rangeMax = 0;
+    QList<QPointF> _values;
+
+    QAbstractSeries *_pSeries = nullptr;
+    MAVLinkChartController *_chart = nullptr;
 };
--- src/AnalyzeView/MAVLinkSystem.cc
+++ src/AnalyzeView/MAVLinkSystem.cc
@@ -13,137 +13,131 @@
 
 QGC_LOGGING_CATEGORY(MAVLinkSystemLog, "qgc.analyzeview.mavlinksystem")
 
-//-----------------------------------------------------------------------------
-QGCMAVLinkSystem::QGCMAVLinkSystem(QObject* parent, quint8 id)
+QGCMAVLinkSystem::QGCMAVLinkSystem(quint8 id, QObject *parent)
     : QObject(parent)
     , _id(id)
+    , _messages(new QmlObjectListModel(this))
 {
     qCDebug(MAVLinkSystemLog) << "New Vehicle:" << id;
 }
 
-//-----------------------------------------------------------------------------
 QGCMAVLinkSystem::~QGCMAVLinkSystem()
 {
-    _messages.clearAndDeleteContents();
+    _messages->clearAndDeleteContents();
 }
 
-//-----------------------------------------------------------------------------
-QGCMAVLinkMessage*
-QGCMAVLinkSystem::findMessage(uint32_t id, uint8_t compId)
+QGCMAVLinkMessage *QGCMAVLinkSystem::findMessage(uint32_t id, uint8_t compId)
 {
-    for(int i = 0; i < _messages.count(); i++) {
-        QGCMAVLinkMessage* m = qobject_cast<QGCMAVLinkMessage*>(_messages.get(i));
-        if(m) {
-            if(m->id() == id && m->compId() == compId) {
-                return m;
+    for (int i = 0; i < _messages->count(); i++) {
+        QGCMAVLinkMessage *const msg = qobject_cast<QGCMAVLinkMessage*>(_messages->get(i));
+        if(msg) {
+            if((msg->id() == id) && (msg->compId() == compId)) {
+                return msg;
             }
         }
     }
+
     return nullptr;
 }
 
-//-----------------------------------------------------------------------------
-int
-QGCMAVLinkSystem::findMessage(QGCMAVLinkMessage* message)
+int QGCMAVLinkSystem::findMessage(const QGCMAVLinkMessage *message)
 {
-    for(int i = 0; i < _messages.count(); i++) {
-        QGCMAVLinkMessage* m = qobject_cast<QGCMAVLinkMessage*>(_messages.get(i));
-        if(m && m == message) {
+    for (int i = 0; i < _messages->count(); i++) {
+        const QGCMAVLinkMessage *const msg = qobject_cast<const QGCMAVLinkMessage*>(_messages->get(i));
+        if (msg && (msg == message)) {
             return i;
         }
     }
+
     return -1;
 }
 
-//-----------------------------------------------------------------------------
-void
-QGCMAVLinkSystem::_resetSelection()
+void QGCMAVLinkSystem::_resetSelection()
 {
-    for(int i = 0; i < _messages.count(); i++) {
-        QGCMAVLinkMessage* m = qobject_cast<QGCMAVLinkMessage*>(_messages.get(i));
-        if(m && m->selected()) {
-            m->setSelected(false);
-            emit m->selectedChanged();
+    for (int i = 0; i < _messages->count(); i++) {
+        QGCMAVLinkMessage *const msg = qobject_cast<QGCMAVLinkMessage*>(_messages->get(i));
+        if (msg && msg->selected()) {
+            msg->setSelected(false);
+            emit msg->selectedChanged();
         }
     }
 }
 
-//-----------------------------------------------------------------------------
-void
-QGCMAVLinkSystem::setSelected(int sel)
+void QGCMAVLinkSystem::setSelected(int sel)
 {
-    if(sel < _messages.count()) {
-        _selected = sel;
-        emit selectedChanged();
-        _resetSelection();
-        QGCMAVLinkMessage* m = qobject_cast<QGCMAVLinkMessage*>(_messages.get(sel));
-        if(m && !m->selected()) {
-            m->setSelected(true);
-            emit m->selectedChanged();
-        }
+    if (sel >= _messages->count()) {
+        return;
+    }
+
+    _selected = sel;
+    emit selectedChanged();
+    _resetSelection();
+    QGCMAVLinkMessage *const msg = qobject_cast<QGCMAVLinkMessage*>(_messages->get(sel));
+    if(msg && !msg->selected()) {
+        msg->setSelected(true);
+        emit msg->selectedChanged();
     }
 }
 
-QGCMAVLinkMessage* QGCMAVLinkSystem::selectedMsg()
+QGCMAVLinkMessage *QGCMAVLinkSystem::selectedMsg()
 {
-    QGCMAVLinkMessage* selectedMsg = nullptr;
-    if(_messages.count())
-    {
-        selectedMsg = qobject_cast<QGCMAVLinkMessage*>(_messages.get(_selected));
+    QGCMAVLinkMessage *selectedMsg = nullptr;
+    if (_messages->count()) {
+        selectedMsg = qobject_cast<QGCMAVLinkMessage*>(_messages->get(_selected));
     }
+
     return selectedMsg;
 }
 
-//-----------------------------------------------------------------------------
-static bool
-messages_sort(QObject* a, QObject* b)
+static bool messages_sort(const QObject *a, const QObject *b)
 {
-    QGCMAVLinkMessage* aa = qobject_cast<QGCMAVLinkMessage*>(a);
-    QGCMAVLinkMessage* bb = qobject_cast<QGCMAVLinkMessage*>(b);
-    if(!aa || !bb) return false;
-    if(aa->name() == bb->name()) return aa->name() < bb->name();
-    return aa->name() < bb->name();
+    const QGCMAVLinkMessage *const aa = qobject_cast<const QGCMAVLinkMessage*>(a);
+    const QGCMAVLinkMessage *const bb = qobject_cast<const QGCMAVLinkMessage*>(b);
+    if (!aa || !bb) {
+        return false;
+    }
+
+    if (aa->name() == bb->name()) {
+        return (aa->name() < bb->name());
+    }
+
+    return (aa->name() < bb->name());
 }
 
-//-----------------------------------------------------------------------------
-void
-QGCMAVLinkSystem::append(QGCMAVLinkMessage* message)
+void QGCMAVLinkSystem::append(QGCMAVLinkMessage *message)
 {
-    //-- Save selected message
-    QGCMAVLinkMessage* selectedMsg = nullptr;
-    if(_messages.count()) {
-        selectedMsg = qobject_cast<QGCMAVLinkMessage*>(_messages.get(_selected));
+    QGCMAVLinkMessage *selectedMsg = nullptr;
+    if (_messages->count()) {
+        selectedMsg = qobject_cast<QGCMAVLinkMessage*>(_messages->get(_selected));
     } else {
-        //-- First message
         message->setSelected(true);
     }
-    _messages.append(message);
-    //-- Sort messages by id and then compId
-    if (_messages.count() > 0) {
-        _messages.beginReset();
-        std::sort(_messages.objectList()->begin(), _messages.objectList()->end(), messages_sort);
-        _messages.endReset();
+    _messages->append(message);
+
+    if (_messages->count() > 0) {
+        _messages->beginReset();
+        std::sort(_messages->objectList()->begin(), _messages->objectList()->end(), messages_sort);
+        _messages->endReset();
         _checkCompID(message);
     }
-    //-- Remember selected message
-    if(selectedMsg) {
-        int idx = findMessage(selectedMsg);
-        if(idx >= 0) {
+
+    if (selectedMsg) {
+        const int idx = findMessage(selectedMsg);
+        if (idx >= 0) {
             _selected = idx;
             emit selectedChanged();
         }
     }
 }
 
-//-----------------------------------------------------------------------------
-void
-QGCMAVLinkSystem::_checkCompID(QGCMAVLinkMessage* message)
+void QGCMAVLinkSystem::_checkCompID(const QGCMAVLinkMessage *message)
 {
-    if(_compIDsStr.isEmpty()) {
+    if (_compIDsStr.isEmpty()) {
         _compIDsStr << tr("Comp All");
     }
-    if(!_compIDs.contains(static_cast<int>(message->compId()))) {
-        int compId = static_cast<int>(message->compId());
+
+    if (!_compIDs.contains(static_cast<int>(message->compId()))) {
+        const int compId = static_cast<int>(message->compId());
         _compIDs.append(compId);
         _compIDsStr << tr("Comp %1").arg(compId);
         emit compIDsChanged();
