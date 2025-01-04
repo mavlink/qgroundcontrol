9ba96c3256ceada1295e88ced79b7bf28589dd03
Don Gagne <don@thegagnes.com>
2025-01-04 13:10:31 -0800

    Mac dependency script
    Run from CI as well

---
+++ tools/setup/macos-dependencies.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+if ! command -v brew &> /dev/null
+then
+	# install Homebrew if not installed yet
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+brew update
+brew install cmake ninja ccache geographiclib SDL2 exiv2 expat zlib shapelib pkgconf create-dmg
