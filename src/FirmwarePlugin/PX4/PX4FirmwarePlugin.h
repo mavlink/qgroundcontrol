561103acec8b5b601178f29adb2056517894b63f
Peter Barker <pbarker@barker.dropbear.id.au>
2025-01-05 17:40:15 +1100

    Vehicle: use MAV_CMD_DO_SET_MISSION_CURRENT to set current waypoint
    falls back to the old "mission_set_current" message if the command is not supported

--- src/FirmwarePlugin/PX4/PX4FirmwarePlugin.h
+++ src/FirmwarePlugin/PX4/PX4FirmwarePlugin.h
@@ -122,5 +122,19 @@ class PX4FirmwarePluginInstanceData : public FirmwarePluginInstanceData
 public:
     PX4FirmwarePluginInstanceData(QObject* parent = nullptr);
 
+    // anyVersionSupportsCommand returns
+    // CommandSupportedResult::SUPPORTED if any version of the
+    // firmware has supported cmd.  It return UNSUPPORTED if no
+    // version ever has.  It returns UNKNOWN if that information is
+    // not known.
+    CommandSupportedResult anyVersionSupportsCommand(MAV_CMD cmd) const override {
+        switch (cmd) {
+        case MAV_CMD_DO_SET_MISSION_CURRENT:
+            return CommandSupportedResult::UNSUPPORTED;
+        default:
+            return CommandSupportedResult::UNKNOWN;
+        }
+    }
+
     bool versionNotified;  ///< true: user notified over version issue
 };
