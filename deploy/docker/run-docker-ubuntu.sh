1f9088286bdbd56249778c769972c9b21873ab2f
Holden <htramsey98@gmail.com>
2025-01-07 21:47:38 -0500

    Docker: Create MacOS script
    Former-commit-id: e568fbd889075714bf358ad6327ab89e05fea9f7

--- deploy/docker/run-docker-ubuntu.sh
+++ deploy/docker/run-docker-ubuntu.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-# Run this from root directory
-
+# Exit immediately if a command exits with a non-zero status
 set -e
 
-docker build --file ./deploy/docker/Dockerfile-build-ubuntu -t qgc-ubuntu-docker .
-docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined --rm -v ${PWD}:/project/source -v ${PWD}/build:/project/build qgc-ubuntu-docker
+# Define variables for better maintainability
+DOCKERFILE_PATH="./deploy/docker/Dockerfile-build-ubuntu"
+IMAGE_NAME="qgc-ubuntu-docker"
+SOURCE_DIR="$(pwd)"
+BUILD_DIR="${SOURCE_DIR}/build"
+
+# Build the Docker image
+docker build --file "${DOCKERFILE_PATH}" -t "${IMAGE_NAME}" "${SOURCE_DIR}"
+
+# Run the Docker container with necessary permissions and volume mounts
+docker run \
+  --rm \
+  --cap-add SYS_ADMIN \
+  --device /dev/fuse \
+  --security-opt apparmor:unconfined \
+  -v "${SOURCE_DIR}:/project/source" \
+  -v "${BUILD_DIR}:/project/build" \
+  "${IMAGE_NAME}"
