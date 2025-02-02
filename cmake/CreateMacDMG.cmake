2888d456b8679b81a2bc1622f96389b170730381
Don Gagne <don@thegagnes.com>
2025-02-01 21:33:38 -0800

    More

--- cmake/CreateMacDMG.cmake
+++ cmake/CreateMacDMG.cmake
@@ -1,19 +1,21 @@
 message(STATUS "Creating Mac DMG")
 
-set(BUNDLE_PATH ${CMAKE_BINARY_DIR}/staging/${TARGET_APP_NAME}.app)
+set(BUILD_BUNDLE_PATH ${CMAKE_BINARY_DIR}/Debug/${TARGET_APP_NAME}.app)
+
 execute_process(
-    COMMAND ${MACDEPLOYQT} "${BUNDLE_PATH}" -appstore-compliant -verbose=1 -qmldir=${CMAKE_SOURCE_DIR}/src
+    COMMAND ${MACDEPLOYQT} "${BUILD_BUNDLE_PATH}" -appstore-compliant -verbose=1 -qmldir=${CMAKE_SOURCE_DIR}/../src
     COMMAND_ERROR_IS_FATAL ANY
 )
 execute_process(
-    COMMAND codesign --force --deep -s - "${BUNDLE_PATH}"
+    COMMAND codesign --force --deep -s - "${BUILD_BUNDLE_PATH}"
     COMMAND_ERROR_IS_FATAL ANY
 )
-if(EXISTS ${CMAKE_BINARY_DIR}/package)
-    file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/package)
-endif()
+
+set(PACKAGE_BUNDLE_PATH ${CMAKE_BINARY_DIR}/Debug/${TARGET_APP_NAME}.app)
+file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/package)
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/package)
-file(COPY ${BUNDLE_PATH} DESTINATION ${CMAKE_BINARY_DIR}/package)
+file(COPY ${PACKAGE_BUNDLE_PATH} DESTINATION ${CMAKE_BINARY_DIR}/package)
+
 execute_process(
     COMMAND create-dmg --volname "${TARGET_APP_NAME}" --filesystem "APFS" "${TARGET_APP_NAME}.dmg" "${CMAKE_BINARY_DIR}/package/"
     COMMAND_ERROR_IS_FATAL ANY
