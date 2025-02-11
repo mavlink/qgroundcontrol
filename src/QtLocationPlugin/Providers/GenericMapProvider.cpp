f0f5256638d51d94c943c65c83ad1ea7395d2ec2
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2025-02-11 23:14:29 +0100

    Map-added Svalbard-Maps
    Former-commit-id: c79b74fbf03b00a40d3e7c0fb3c19168763bee3b

--- src/QtLocationPlugin/Providers/GenericMapProvider.cpp
+++ src/QtLocationPlugin/Providers/GenericMapProvider.cpp
@@ -41,6 +41,11 @@ QString EniroMapProvider::_getURL(int x, int y, int zoom) const
     return _mapUrl.arg(zoom).arg(x).arg((1 << zoom) - 1 - y).arg(_imageFormat);
 }
 
+QString SvalbardMapProvider::_getURL(int x, int y, int zoom) const
+{
+    return _mapUrl.arg(zoom).arg(y).arg(x);
+}
+
 QString MapQuestMapProvider::_getURL(int x, int y, int zoom) const
 {
     return _mapUrl.arg(_getServerNum(x, y, 4)).arg(_mapName).arg(zoom).arg(x).arg(y).arg(_imageFormat);
