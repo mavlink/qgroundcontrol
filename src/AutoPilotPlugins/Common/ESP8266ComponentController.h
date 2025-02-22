3f0161f6c999142b724d05ee457d6972429d0b94
Holden <htramsey98@gmail.com>
2025-02-21 22:04:09 -0500

    AutoPilotPlugins: Cleanup
    Former-commit-id: bc724b54d886b563dca35450b8b7450d825c030e

--- src/AutoPilotPlugins/Common/ESP8266ComponentController.h
+++ src/AutoPilotPlugins/Common/ESP8266ComponentController.h
@@ -7,94 +7,85 @@
  *
  ****************************************************************************/
 
-
-
-/// @file
-///     @brief  ESP8266 WiFi Config Qml Controller
-///     @author Gus Grubba <gus@auterion.com>
-
 #pragma once
 
-#include "FactPanelController.h"
-
 #include <QtCore/QLoggingCategory>
+#include <QtCore/QVariant>
+
+#include "FactPanelController.h"
+#include "MAVLinkLib.h"
 
 Q_DECLARE_LOGGING_CATEGORY(ESP8266ComponentControllerLog)
 
+class Fact;
 class Vehicle;
-
-namespace Ui {
-    class ESP8266ComponentController;
-}
 
 class ESP8266ComponentController : public FactPanelController
 {
     Q_OBJECT
     Q_MOC_INCLUDE("Vehicle.h")
+    Q_PROPERTY(int          componentID     READ componentID                                    CONSTANT)
+    Q_PROPERTY(QString      version         READ version                                        NOTIFY versionChanged)
+    Q_PROPERTY(QString      wifiIPAddress   READ wifiIPAddress                                  CONSTANT)
+    Q_PROPERTY(QString      wifiSSID        READ wifiSSID           WRITE setWifiSSID           NOTIFY wifiSSIDChanged)
+    Q_PROPERTY(QString      wifiPassword    READ wifiPassword       WRITE setWifiPassword       NOTIFY wifiPasswordChanged)
+    Q_PROPERTY(QString      wifiSSIDSta     READ wifiSSIDSta        WRITE setWifiSSIDSta        NOTIFY wifiSSIDStaChanged)
+    Q_PROPERTY(QString      wifiPasswordSta READ wifiPasswordSta    WRITE setWifiPasswordSta    NOTIFY wifiPasswordStaChanged)
+    Q_PROPERTY(QStringList  wifiChannels    READ wifiChannels                                   CONSTANT)
+    Q_PROPERTY(QStringList  baudRates       READ baudRates                                      CONSTANT)
+    Q_PROPERTY(int          baudIndex       READ baudIndex          WRITE setBaudIndex          NOTIFY baudIndexChanged)
+    Q_PROPERTY(bool         busy            READ busy                                           NOTIFY busyChanged)
+    Q_PROPERTY(Vehicle      *vehicle        READ vehicle                                        CONSTANT)
 
 public:
-    ESP8266ComponentController      ();
-    ~ESP8266ComponentController     ();
-
-    Q_PROPERTY(int              componentID     READ componentID                                    CONSTANT)
-    Q_PROPERTY(QString          version         READ version                                        NOTIFY versionChanged)
-    Q_PROPERTY(QString          wifiIPAddress   READ wifiIPAddress                                  CONSTANT)
-    Q_PROPERTY(QString          wifiSSID        READ wifiSSID           WRITE setWifiSSID           NOTIFY wifiSSIDChanged)
-    Q_PROPERTY(QString          wifiPassword    READ wifiPassword       WRITE setWifiPassword       NOTIFY wifiPasswordChanged)
-    Q_PROPERTY(QString          wifiSSIDSta     READ wifiSSIDSta        WRITE setWifiSSIDSta        NOTIFY wifiSSIDStaChanged)
-    Q_PROPERTY(QString          wifiPasswordSta READ wifiPasswordSta    WRITE setWifiPasswordSta    NOTIFY wifiPasswordStaChanged)
-    Q_PROPERTY(QStringList      wifiChannels    READ wifiChannels                                   CONSTANT)
-    Q_PROPERTY(QStringList      baudRates       READ baudRates                                      CONSTANT)
-    Q_PROPERTY(int              baudIndex       READ baudIndex          WRITE setBaudIndex          NOTIFY baudIndexChanged)
-    Q_PROPERTY(bool             busy            READ busy                                           NOTIFY busyChanged)
-    Q_PROPERTY(Vehicle*         vehicle         READ vehicle                                        CONSTANT)
+    explicit ESP8266ComponentController(QObject *parent = nullptr);
+    ~ESP8266ComponentController();
 
     Q_INVOKABLE void restoreDefaults();
-    Q_INVOKABLE void reboot         ();
+    Q_INVOKABLE void reboot();
 
-    int             componentID     ();
-    QString         version         ();
-    QString         wifiIPAddress   ();
-    QString         wifiSSID        ();
-    QString         wifiPassword    ();
-    QString         wifiSSIDSta     ();
-    QString         wifiPasswordSta ();
-    QStringList     wifiChannels    () { return _channels; }
-    QStringList     baudRates       () { return _baudRates; }
-    int             baudIndex       ();
-    bool            busy            () const{ return _waitType != WAIT_FOR_NOTHING; }
-    Vehicle*        vehicle         () { return _vehicle; }
+    static int componentID() { return MAV_COMP_ID_UDP_BRIDGE; }
+    QString version() const;
+    QString wifiIPAddress();
+    QString wifiSSID() const;
+    QString wifiPassword() const;
+    QString wifiSSIDSta() const;
+    QString wifiPasswordSta() const;
+    QStringList wifiChannels() const { return _channels; }
+    QStringList baudRates() const { return _baudRates; }
+    int baudIndex() const;
+    bool busy() const { return (_waitType != WAIT_FOR_NOTHING); }
+    Vehicle *vehicle() const { return _vehicle; }
 
-    void        setWifiSSID         (QString id);
-    void        setWifiPassword     (QString pwd);
-    void        setWifiSSIDSta      (QString id);
-    void        setWifiPasswordSta  (QString pwd);
-    void        setBaudIndex        (int idx);
+    void setWifiSSID(const QString &id) const;
+    void setWifiPassword(const QString &pwd) const;
+    void setWifiSSIDSta(const QString &id) const;
+    void setWifiPasswordSta(const QString &pwd) const;
+    void setBaudIndex(int idx) const;
 
 signals:
-    void        versionChanged          ();
-    void        wifiSSIDChanged         ();
-    void        wifiPasswordChanged     ();
-    void        wifiSSIDStaChanged      ();
-    void        wifiPasswordStaChanged  ();
-    void        baudIndexChanged        ();
-    void        busyChanged             ();
+    void versionChanged();
+    void wifiSSIDChanged();
+    void wifiPasswordChanged();
+    void wifiSSIDStaChanged();
+    void wifiPasswordStaChanged();
+    void baudIndexChanged();
+    void busyChanged();
 
 private slots:
-    void        _mavCommandResult(int vehicleId, int component, int command, int result, bool noReponseFromVehicle);
-    void        _ssidChanged        (QVariant value);
-    void        _passwordChanged    (QVariant value);
-    void        _baudChanged        (QVariant value);
-    void        _versionChanged     (QVariant value);
+    void _mavCommandResult(int vehicleId, int component, int command, int result, bool noReponseFromVehicle);
+    void _ssidChanged(QVariant value) { emit wifiSSIDChanged(); }
+    void _passwordChanged(QVariant value) { emit wifiPasswordChanged(); }
+    void _baudChanged(QVariant value) { emit baudIndexChanged(); }
+    void _versionChanged(QVariant value) { emit versionChanged(); }
 
 private:
-    void        _reboot             ();
-    void        _restoreDefaults    ();
+    void _reboot() const;
+    void _restoreDefaults() const;
 
-private:
     QStringList _channels;
-    QStringList _baudRates;
-    QString     _ipAddress;
+    const QStringList _baudRates = { QStringLiteral("57600"), QStringLiteral("115200"), QStringLiteral("230400"), QStringLiteral("460800"), QStringLiteral("921600") };
+    QString _ipAddress;
 
     enum {
         WAIT_FOR_NOTHING,
@@ -102,6 +93,29 @@ private:
         WAIT_FOR_RESTORE
     };
 
-    int         _waitType;
-    int         _retries;
+    int _waitType = WAIT_FOR_NOTHING;
+    int _retries = 0;
+
+    Fact *_baud = nullptr;
+    Fact *_ver = nullptr;
+
+    Fact *_ssid1 = nullptr;
+    Fact *_ssid2 = nullptr;
+    Fact *_ssid3 = nullptr;
+    Fact *_ssid4 = nullptr;
+
+    Fact *_pwd1 = nullptr;
+    Fact *_pwd2 = nullptr;
+    Fact *_pwd3 = nullptr;
+    Fact *_pwd4 = nullptr;
+
+    Fact *_ssidsta1 = nullptr;
+    Fact *_ssidsta2 = nullptr;
+    Fact *_ssidsta3 = nullptr;
+    Fact *_ssidsta4 = nullptr;
+
+    Fact *_pwdsta1 = nullptr;
+    Fact *_pwdsta2 = nullptr;
+    Fact *_pwdsta3 = nullptr;
+    Fact *_pwdsta4 = nullptr;
 };
