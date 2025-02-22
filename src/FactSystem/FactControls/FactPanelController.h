290362a1ba8c2c10dffc347ff3683e55dfd3da07
Holden <htramsey98@gmail.com>
2025-02-21 23:32:26 -0500

    FactControls: Cleanup FactPanelController

--- src/FactSystem/FactControls/FactPanelController.h
+++ src/FactSystem/FactControls/FactPanelController.h
@@ -7,16 +7,12 @@
  *
  ****************************************************************************/
 
-
 #pragma once
 
-/// @file
-///     @author Don Gagne <don@thegagnes.com>
-
+#include <QtCore/QLoggingCategory>
 #include <QtCore/QObject>
 #include <QtCore/QStringList>
 #include <QtCore/QTimer>
-#include <QtCore/QLoggingCategory>
 
 Q_DECLARE_LOGGING_CATEGORY(FactPanelControllerLog)
 
@@ -24,42 +20,43 @@ class AutoPilotPlugin;
 class Vehicle;
 class Fact;
 
-/// FactPanelController is used for handling missing Facts from C++ code.
+/// Used for handling missing Facts from C++ code.
 class FactPanelController : public QObject
 {
     Q_OBJECT
     Q_MOC_INCLUDE("Vehicle.h")
     Q_MOC_INCLUDE("Fact.h")
+    Q_PROPERTY(Vehicle *vehicle MEMBER _vehicle CONSTANT)
+
 public:
     FactPanelController(QObject *parent = nullptr);
+    ~FactPanelController();
 
-    Q_PROPERTY(Vehicle* vehicle MEMBER _vehicle CONSTANT)
-
-    Q_INVOKABLE Fact*   getParameterFact    (int componentId, const QString& name, bool reportMissing = true);
-    Q_INVOKABLE bool    parameterExists     (int componentId, const QString& name);
+    Q_INVOKABLE Fact *getParameterFact(int componentId, const QString &name, bool reportMissing = true) const;
+    Q_INVOKABLE bool parameterExists(int componentId, const QString &name) const;
 
     /// Queries the vehicle for parameters which were not available on initial download but should be available now.
     /// Signals missingParametersAvailable when done. Only works for MAV_COMP_ID_AUTOPILOT1 parameters.
-    Q_INVOKABLE void    getMissingParameters(QStringList rgNames);
+    Q_INVOKABLE void getMissingParameters(const QStringList &rgNames);
 
 signals:
-    void missingParametersAvailable(void);
+    void missingParametersAvailable();
 
 protected:
     /// Checks for existence of the specified parameters
-    /// @return true: all parameters exists, false: parameters missing and reported
-    bool _allParametersExists(int componentId, QStringList names);
+    ///     @return true: all parameters exists, false: parameters missing and reported
+    bool _allParametersExists(int componentId, const QStringList &names) const;
 
     /// Report a missing parameter
-    void _reportMissingParameter(int componentId, const QString& name);
+    void _reportMissingParameter(int componentId, const QString &name) const;
 
-    Vehicle*            _vehicle    = nullptr;
-    AutoPilotPlugin*    _autopilot  = nullptr;
+    Vehicle *_vehicle = nullptr;
+    AutoPilotPlugin *_autopilot = nullptr;
 
 private slots:
-    void _checkForMissingParameters(void);
+    void _checkForMissingParameters();
 
 private:
     QStringList _missingParameterWaitList;
-    QTimer      _missingParametersTimer;
+    QTimer _missingParametersTimer;
 };
