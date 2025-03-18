275f9fc35969352f728782a4720accf667f35fad
Sam Gillam <gillamkid@gmail.com>
2025-03-18 13:07:26 -0700

    Fix so plan is shown as synced if just downloaded from vehicle
    If a vehicle with a loaded mission was connected to QGC, when QGC would download the mission, the mission would be marked as "dirty" (aka Plan View thinks it has unsaved changes and prompts the operator to upload the changes). The plan should be marked as clean if it was just downloaded. This commit has a fix so download results in the plan being marked clean (aka has no unsaved changes) as  one would expect.

--- src/MissionManager/MissionController.cc
+++ src/MissionManager/MissionController.cc
@@ -61,8 +61,8 @@ MissionController::MissionController(PlanMasterController* masterController, QOb
     connect(this,                                               &MissionController::missionPlannedDistanceChanged,      this, &MissionController::recalcTerrainProfile);
 
     // The follow is used to compress multiple recalc calls in a row to into a single call.
-    connect(this, &MissionController::_recalcMissionFlightStatusSignal, this, &MissionController::_recalcMissionFlightStatus,   Qt::QueuedConnection);
-    connect(this, &MissionController::_recalcFlightPathSegmentsSignal,  this, &MissionController::_recalcFlightPathSegments,    Qt::QueuedConnection);
+    connect(this, &MissionController::_recalcMissionFlightStatusSignal, this, &MissionController::_recalcMissionFlightStatus);
+    connect(this, &MissionController::_recalcFlightPathSegmentsSignal,  this, &MissionController::_recalcFlightPathSegments);
     qgcApp()->addCompressedSignal(QMetaMethod::fromSignal(&MissionController::_recalcMissionFlightStatusSignal));
     qgcApp()->addCompressedSignal(QMetaMethod::fromSignal(&MissionController::_recalcFlightPathSegmentsSignal));
     qgcApp()->addCompressedSignal(QMetaMethod::fromSignal(&MissionController::recalcTerrainProfile));
