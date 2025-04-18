ffc6635a5f2645d090d456fe29050b019564a703
Holden Ramsey <htramsey98@gmail.com>
2025-04-18 14:50:48 -0400

    CMake: Fix Test Build Issue for Qt 6.6
    Former-commit-id: 1a89e98d40954ba91beb8373f32e8161664fa67f

--- test/Utilities/Compression/DecompressionTest.cc
+++ test/Utilities/Compression/DecompressionTest.cc
@@ -7,7 +7,7 @@
 
 void DecompressionTest::_testDecompressGzip()
 {
-    const QString gzippedFileName = QStringLiteral(":/manifest.json.gz");
+    const QString gzippedFileName = QStringLiteral(":/unittest/manifest.json.gz");
 	const QString decompressedFilename = QStringLiteral("manifest.json");
     const bool result = QGCZlib::inflateGzipFile(gzippedFileName, decompressedFilename);
 	QVERIFY(result);
@@ -15,7 +15,7 @@ void DecompressionTest::_testDecompressGzip()
 
 void DecompressionTest::_testDecompressLZMA()
 {
-    const QString lzmaFilename = QStringLiteral(":/manifest.json.xz");
+    const QString lzmaFilename = QStringLiteral(":/unittest/manifest.json.xz");
 	const QString decompressedFilename = QStringLiteral("manifest.json");
     const bool result = QGCLZMA::inflateLZMAFile(lzmaFilename, decompressedFilename);
 	QVERIFY(result);
@@ -23,7 +23,7 @@ void DecompressionTest::_testDecompressLZMA()
 
 void DecompressionTest::_testUnzip()
 {
-    const QString zipFilename = QStringLiteral(":/manifest.json.zip");
+    const QString zipFilename = QStringLiteral(":/unittest/manifest.json.zip");
     const QString decompressedPath = QDir::tempPath() + "/QGC_DECOMPRESSION_TEST";
     const bool result = QGCZip::unzipFile(zipFilename, decompressedPath);
     QVERIFY(result);
