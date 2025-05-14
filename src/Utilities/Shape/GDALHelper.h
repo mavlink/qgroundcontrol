041a36778b36558cf52af3672d412245d65d66ba
Holden <htramsey98@gmail.com>
2025-05-14 14:26:06 -0400

    Utilities: Add GDAL

---
+++ src/Utilities/Shape/GDALHelper.h
@@ -0,0 +1,15 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+namespace GDALHelper
+{
+
+}
