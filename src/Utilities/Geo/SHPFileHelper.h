13ca9f19de53ac1ce04b723168123e83a85d048a
Holden <htramsey98@gmail.com>
2025-05-17 06:19:20 -0400

    Utilities: Add GeoJSON Handling

--- src/Utilities/Geo/SHPFileHelper.h
+++ src/Utilities/Geo/SHPFileHelper.h
@@ -19,7 +19,7 @@ Q_DECLARE_LOGGING_CATEGORY(SHPFileHelperLog)
 
 namespace SHPFileHelper
 {
-    ShapeFileHelper::ShapeType determineShapeType(const QString &shpFile, QString &errorString);
+    ShapeFileHelper::ShapeType determineShapeType(const QString &file, QString &errorString);
     bool loadPolygonFromFile(const QString &shpFile, QList<QGeoCoordinate> &vertices, QString &errorString);
     bool loadPolylineFromFile(const QString &shpFile, QList<QGeoCoordinate> &coords, QString &errorString);
 };
