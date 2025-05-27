647ddd0a34d1326a0bf3f3e06fd8b5f2e6c1ecab
Don Gagne <don@thegagnes.com>
2025-05-27 11:39:29 -0700

    Convert QGroundControl.Controls to qt_add_qml_module
    This will compile qml for performance boost
    
    Move qml file to QmlControls directory to avoid use of QT_RESOURCE_ALIAS. There appears to be some sort of Qt bug/feature which causes this to not work correctly on Windows builds. This is the simplest fix nor now which requires minimal code churn.

--- src/MissionManager/MissionSettingsItem.cc
+++ src/MissionManager/MissionSettingsItem.cc
@@ -28,7 +28,7 @@ MissionSettingsItem::MissionSettingsItem(PlanMasterController* masterController,
     , _speedSection                     (masterController)
 {
     _isIncomplete = false;
-    _editorQml = "qrc:/qml/MissionSettingsEditor.qml";
+    _editorQml = "qrc:/qml/QGroundControl/Controls/MissionSettingsEditor.qml";
 
     if (_metaDataMap.isEmpty()) {
         _metaDataMap = FactMetaData::createMapFromJsonFile(QStringLiteral(":/json/MissionSettings.FactMetaData.json"), nullptr /* metaDataParent */);
