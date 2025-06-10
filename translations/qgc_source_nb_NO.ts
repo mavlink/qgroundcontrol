475c9475a43a404ce96df1ae9e9314766cba57ba
Holden <htramsey98@gmail.com>
2025-06-10 12:06:27 -0400

    CMake: Fix MacOS Deployment Issues

--- translations/qgc_source_nb_NO.ts
+++ translations/qgc_source_nb_NO.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="no" sourcelanguage="en">
+<TS version="2.1" language="nb" sourcelanguage="en">
   <context>
     <name>APMAirframeComponent</name>
     <message>
