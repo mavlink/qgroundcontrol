a41880a4c2f07bbd1fe95a28f32346287fae8065
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-06-09 20:48:57 -0800

    commit 2

--- src/Vehicle/FactGroups/VehicleEscStatusFactGroup.cc
+++ src/Vehicle/FactGroups/VehicleEscStatusFactGroup.cc
@@ -14,50 +14,134 @@ VehicleEscStatusFactGroup::VehicleEscStatusFactGroup(QObject *parent)
     : FactGroup(1000, QStringLiteral(":/json/Vehicle/EscStatusFactGroup.json"), parent)
 {
     _addFact(&_indexFact);
+    _addFact(&_countFact);
+    _addFact(&_infoFact);
 
     _addFact(&_rpmFirstFact);
     _addFact(&_rpmSecondFact);
     _addFact(&_rpmThirdFact);
     _addFact(&_rpmFourthFact);
+    _addFact(&_rpmFifthFact);
+    _addFact(&_rpmSixthFact);
+    _addFact(&_rpmSeventhFact);
+    _addFact(&_rpmEighthFact);
 
     _addFact(&_currentFirstFact);
     _addFact(&_currentSecondFact);
     _addFact(&_currentThirdFact);
     _addFact(&_currentFourthFact);
+    _addFact(&_currentFifthFact);
+    _addFact(&_currentSixthFact);
+    _addFact(&_currentSeventhFact);
+    _addFact(&_currentEighthFact);
 
     _addFact(&_voltageFirstFact);
     _addFact(&_voltageSecondFact);
     _addFact(&_voltageThirdFact);
     _addFact(&_voltageFourthFact);
+    _addFact(&_voltageFifthFact);
+    _addFact(&_voltageSixthFact);
+    _addFact(&_voltageSeventhFact);
+    _addFact(&_voltageEighthFact);
+
+    _addFact(&_temperatureFirstFact);
+    _addFact(&_temperatureSecondFact);
+    _addFact(&_temperatureThirdFact);
+    _addFact(&_temperatureFourthFact);
+    _addFact(&_temperatureFifthFact);
+    _addFact(&_temperatureSixthFact);
+    _addFact(&_temperatureSeventhFact);
+    _addFact(&_temperatureEighthFact);
+
+    _addFact(&_errorCountFirstFact);
+    _addFact(&_errorCountSecondFact);
+    _addFact(&_errorCountThirdFact);
+    _addFact(&_errorCountFourthFact);
+    _addFact(&_errorCountFifthFact);
+    _addFact(&_errorCountSixthFact);
+    _addFact(&_errorCountSeventhFact);
+    _addFact(&_errorCountEighthFact);
 }
 
 void VehicleEscStatusFactGroup::handleMessage(Vehicle *vehicle, const mavlink_message_t &message)
 {
     Q_UNUSED(vehicle);
 
-    if (message.msgid != MAVLINK_MSG_ID_ESC_STATUS) {
-        return;
+    if (message.msgid == MAVLINK_MSG_ID_ESC_STATUS) {
+        mavlink_esc_status_t content{};
+        mavlink_msg_esc_status_decode(&message, &content);
+
+        index()->setRawValue(content.index);
+
+        // ESC_STATUS supports up to 8 motors in the arrays
+        rpmFirst()->setRawValue(content.rpm[0]);
+        rpmSecond()->setRawValue(content.rpm[1]);
+        rpmThird()->setRawValue(content.rpm[2]);
+        rpmFourth()->setRawValue(content.rpm[3]);
+        rpmFifth()->setRawValue(content.rpm[4]);
+        rpmSixth()->setRawValue(content.rpm[5]);
+        rpmSeventh()->setRawValue(content.rpm[6]);
+        rpmEighth()->setRawValue(content.rpm[7]);
+
+        currentFirst()->setRawValue(content.current[0]);
+        currentSecond()->setRawValue(content.current[1]);
+        currentThird()->setRawValue(content.current[2]);
+        currentFourth()->setRawValue(content.current[3]);
+        currentFifth()->setRawValue(content.current[4]);
+        currentSixth()->setRawValue(content.current[5]);
+        currentSeventh()->setRawValue(content.current[6]);
+        currentEighth()->setRawValue(content.current[7]);
+
+        voltageFirst()->setRawValue(content.voltage[0]);
+        voltageSecond()->setRawValue(content.voltage[1]);
+        voltageThird()->setRawValue(content.voltage[2]);
+        voltageFourth()->setRawValue(content.voltage[3]);
+        voltageFifth()->setRawValue(content.voltage[4]);
+        voltageSixth()->setRawValue(content.voltage[5]);
+        voltageSeventh()->setRawValue(content.voltage[6]);
+        voltageEighth()->setRawValue(content.voltage[7]);
+
+        _setTelemetryAvailable(true);
+    } else if (message.msgid == MAVLINK_MSG_ID_ESC_INFO) {
+        mavlink_esc_info_t content{};
+        mavlink_msg_esc_info_decode(&message, &content);
+
+        // ESC_INFO contains count, info bitmask, temperature and error count data
+        // The index field tells us which ESC this data is for
+        
+        // Store the count and info bitmask (these are global for this ESC index)
+        count()->setRawValue(content.count);
+        info()->setRawValue(content.info);
+        
+        switch (content.index) {
+        case 0:
+            // Temperature is in centi-celsius, convert to celsius
+            temperatureFirst()->setRawValue(content.temperature[0] / 100.0);
+            temperatureSecond()->setRawValue(content.temperature[1] / 100.0);
+            temperatureThird()->setRawValue(content.temperature[2] / 100.0);
+            temperatureFourth()->setRawValue(content.temperature[3] / 100.0);
+            
+            // Error counts
+            errorCountFirst()->setRawValue(content.error_count[0]);
+            errorCountSecond()->setRawValue(content.error_count[1]);
+            errorCountThird()->setRawValue(content.error_count[2]);
+            errorCountFourth()->setRawValue(content.error_count[3]);
+            break;
+        case 1:
+            // ESC index 1 (motors 5-8)
+            temperatureFifth()->setRawValue(content.temperature[0] / 100.0);
+            temperatureSixth()->setRawValue(content.temperature[1] / 100.0);
+            temperatureSeventh()->setRawValue(content.temperature[2] / 100.0);
+            temperatureEighth()->setRawValue(content.temperature[3] / 100.0);
+            
+            errorCountFifth()->setRawValue(content.error_count[0]);
+            errorCountSixth()->setRawValue(content.error_count[1]);
+            errorCountSeventh()->setRawValue(content.error_count[2]);
+            errorCountEighth()->setRawValue(content.error_count[3]);
+            break;
+        default:
+            // Support for additional ESC indices can be added here
+            break;
+        }
     }
-
-    mavlink_esc_status_t content{};
-    mavlink_msg_esc_status_decode(&message, &content);
-
-    index()->setRawValue(content.index);
-
-    rpmFirst()->setRawValue(content.rpm[0]);
-    rpmSecond()->setRawValue(content.rpm[1]);
-    rpmThird()->setRawValue(content.rpm[2]);
-    rpmFourth()->setRawValue(content.rpm[3]);
-
-    currentFirst()->setRawValue(content.current[0]);
-    currentSecond()->setRawValue(content.current[1]);
-    currentThird()->setRawValue(content.current[2]);
-    currentFourth()->setRawValue(content.current[3]);
-
-    voltageFirst()->setRawValue(content.voltage[0]);
-    voltageSecond()->setRawValue(content.voltage[1]);
-    voltageThird()->setRawValue(content.voltage[2]);
-    voltageFourth()->setRawValue(content.voltage[3]);
-
-    _setTelemetryAvailable(true);
 }
--- src/Vehicle/FactGroups/VehicleEscStatusFactGroup.h
+++ src/Vehicle/FactGroups/VehicleEscStatusFactGroup.h
@@ -15,55 +15,149 @@ class VehicleEscStatusFactGroup : public FactGroup
 {
     Q_OBJECT
     Q_PROPERTY(Fact *index          READ index          CONSTANT)
+    Q_PROPERTY(Fact *count          READ count          CONSTANT)
+    Q_PROPERTY(Fact *info           READ info           CONSTANT)
     Q_PROPERTY(Fact *rpmFirst       READ rpmFirst       CONSTANT)
     Q_PROPERTY(Fact *rpmSecond      READ rpmSecond      CONSTANT)
     Q_PROPERTY(Fact *rpmThird       READ rpmThird       CONSTANT)
     Q_PROPERTY(Fact *rpmFourth      READ rpmFourth      CONSTANT)
+    Q_PROPERTY(Fact *rpmFifth       READ rpmFifth       CONSTANT)
+    Q_PROPERTY(Fact *rpmSixth       READ rpmSixth       CONSTANT)
+    Q_PROPERTY(Fact *rpmSeventh     READ rpmSeventh     CONSTANT)
+    Q_PROPERTY(Fact *rpmEighth      READ rpmEighth      CONSTANT)
     Q_PROPERTY(Fact *currentFirst   READ currentFirst   CONSTANT)
     Q_PROPERTY(Fact *currentSecond  READ currentSecond  CONSTANT)
     Q_PROPERTY(Fact *currentThird   READ currentThird   CONSTANT)
     Q_PROPERTY(Fact *currentFourth  READ currentFourth  CONSTANT)
+    Q_PROPERTY(Fact *currentFifth   READ currentFifth   CONSTANT)
+    Q_PROPERTY(Fact *currentSixth   READ currentSixth   CONSTANT)
+    Q_PROPERTY(Fact *currentSeventh READ currentSeventh CONSTANT)
+    Q_PROPERTY(Fact *currentEighth  READ currentEighth  CONSTANT)
     Q_PROPERTY(Fact *voltageFirst   READ voltageFirst   CONSTANT)
     Q_PROPERTY(Fact *voltageSecond  READ voltageSecond  CONSTANT)
     Q_PROPERTY(Fact *voltageThird   READ voltageThird   CONSTANT)
     Q_PROPERTY(Fact *voltageFourth  READ voltageFourth  CONSTANT)
+    Q_PROPERTY(Fact *voltageFifth   READ voltageFifth   CONSTANT)
+    Q_PROPERTY(Fact *voltageSixth   READ voltageSixth   CONSTANT)
+    Q_PROPERTY(Fact *voltageSeventh READ voltageSeventh CONSTANT)
+    Q_PROPERTY(Fact *voltageEighth  READ voltageEighth  CONSTANT)
+    Q_PROPERTY(Fact *temperatureFirst   READ temperatureFirst   CONSTANT)
+    Q_PROPERTY(Fact *temperatureSecond  READ temperatureSecond  CONSTANT)
+    Q_PROPERTY(Fact *temperatureThird   READ temperatureThird   CONSTANT)
+    Q_PROPERTY(Fact *temperatureFourth  READ temperatureFourth  CONSTANT)
+    Q_PROPERTY(Fact *temperatureFifth   READ temperatureFifth   CONSTANT)
+    Q_PROPERTY(Fact *temperatureSixth   READ temperatureSixth   CONSTANT)
+    Q_PROPERTY(Fact *temperatureSeventh READ temperatureSeventh CONSTANT)
+    Q_PROPERTY(Fact *temperatureEighth  READ temperatureEighth  CONSTANT)
+    Q_PROPERTY(Fact *errorCountFirst    READ errorCountFirst    CONSTANT)
+    Q_PROPERTY(Fact *errorCountSecond   READ errorCountSecond   CONSTANT)
+    Q_PROPERTY(Fact *errorCountThird    READ errorCountThird    CONSTANT)
+    Q_PROPERTY(Fact *errorCountFourth   READ errorCountFourth   CONSTANT)
+    Q_PROPERTY(Fact *errorCountFifth    READ errorCountFifth    CONSTANT)
+    Q_PROPERTY(Fact *errorCountSixth    READ errorCountSixth    CONSTANT)
+    Q_PROPERTY(Fact *errorCountSeventh  READ errorCountSeventh  CONSTANT)
+    Q_PROPERTY(Fact *errorCountEighth   READ errorCountEighth   CONSTANT)
 
 public:
     explicit VehicleEscStatusFactGroup(QObject *parent = nullptr);
 
     Fact *index() { return &_indexFact; }
+    Fact *count() { return &_countFact; }
+    Fact *info() { return &_infoFact; }
     Fact *rpmFirst() { return &_rpmFirstFact; }
     Fact *rpmSecond() { return &_rpmSecondFact; }
     Fact *rpmThird() { return &_rpmThirdFact; }
     Fact *rpmFourth() { return &_rpmFourthFact; }
+    Fact *rpmFifth() { return &_rpmFifthFact; }
+    Fact *rpmSixth() { return &_rpmSixthFact; }
+    Fact *rpmSeventh() { return &_rpmSeventhFact; }
+    Fact *rpmEighth() { return &_rpmEighthFact; }
 
     Fact *currentFirst() { return &_currentFirstFact; }
     Fact *currentSecond() { return &_currentSecondFact; }
     Fact *currentThird() { return &_currentThirdFact; }
     Fact *currentFourth() { return &_currentFourthFact; }
+    Fact *currentFifth() { return &_currentFifthFact; }
+    Fact *currentSixth() { return &_currentSixthFact; }
+    Fact *currentSeventh() { return &_currentSeventhFact; }
+    Fact *currentEighth() { return &_currentEighthFact; }
 
     Fact *voltageFirst() { return &_voltageFirstFact; }
     Fact *voltageSecond() { return &_voltageSecondFact; }
     Fact *voltageThird() { return &_voltageThirdFact; }
     Fact *voltageFourth() { return &_voltageFourthFact; }
+    Fact *voltageFifth() { return &_voltageFifthFact; }
+    Fact *voltageSixth() { return &_voltageSixthFact; }
+    Fact *voltageSeventh() { return &_voltageSeventhFact; }
+    Fact *voltageEighth() { return &_voltageEighthFact; }
+
+    Fact *temperatureFirst() { return &_temperatureFirstFact; }
+    Fact *temperatureSecond() { return &_temperatureSecondFact; }
+    Fact *temperatureThird() { return &_temperatureThirdFact; }
+    Fact *temperatureFourth() { return &_temperatureFourthFact; }
+    Fact *temperatureFifth() { return &_temperatureFifthFact; }
+    Fact *temperatureSixth() { return &_temperatureSixthFact; }
+    Fact *temperatureSeventh() { return &_temperatureSeventhFact; }
+    Fact *temperatureEighth() { return &_temperatureEighthFact; }
+
+    Fact *errorCountFirst() { return &_errorCountFirstFact; }
+    Fact *errorCountSecond() { return &_errorCountSecondFact; }
+    Fact *errorCountThird() { return &_errorCountThirdFact; }
+    Fact *errorCountFourth() { return &_errorCountFourthFact; }
+    Fact *errorCountFifth() { return &_errorCountFifthFact; }
+    Fact *errorCountSixth() { return &_errorCountSixthFact; }
+    Fact *errorCountSeventh() { return &_errorCountSeventhFact; }
+    Fact *errorCountEighth() { return &_errorCountEighthFact; }
 
     // Overrides from FactGroup
     void handleMessage(Vehicle *vehicle, const mavlink_message_t &message) final;
 
 private:
     Fact _indexFact = Fact(0, QStringLiteral("index"), FactMetaData::valueTypeUint8);
+    Fact _countFact = Fact(0, QStringLiteral("count"), FactMetaData::valueTypeUint8);
+    Fact _infoFact = Fact(0, QStringLiteral("info"), FactMetaData::valueTypeUint64);
     Fact _rpmFirstFact = Fact(0, QStringLiteral("rpm1"), FactMetaData::valueTypeFloat);
     Fact _rpmSecondFact = Fact(0, QStringLiteral("rpm2"), FactMetaData::valueTypeFloat);
     Fact _rpmThirdFact = Fact(0, QStringLiteral("rpm3"), FactMetaData::valueTypeFloat);
     Fact _rpmFourthFact = Fact(0, QStringLiteral("rpm4"), FactMetaData::valueTypeFloat);
+    Fact _rpmFifthFact = Fact(0, QStringLiteral("rpm5"), FactMetaData::valueTypeFloat);
+    Fact _rpmSixthFact = Fact(0, QStringLiteral("rpm6"), FactMetaData::valueTypeFloat);
+    Fact _rpmSeventhFact = Fact(0, QStringLiteral("rpm7"), FactMetaData::valueTypeFloat);
+    Fact _rpmEighthFact = Fact(0, QStringLiteral("rpm8"), FactMetaData::valueTypeFloat);
 
     Fact _currentFirstFact = Fact(0, QStringLiteral("current1"), FactMetaData::valueTypeFloat);
     Fact _currentSecondFact = Fact(0, QStringLiteral("current2"), FactMetaData::valueTypeFloat);
     Fact _currentThirdFact = Fact(0, QStringLiteral("current3"), FactMetaData::valueTypeFloat);
     Fact _currentFourthFact = Fact(0, QStringLiteral("current4"), FactMetaData::valueTypeFloat);
+    Fact _currentFifthFact = Fact(0, QStringLiteral("current5"), FactMetaData::valueTypeFloat);
+    Fact _currentSixthFact = Fact(0, QStringLiteral("current6"), FactMetaData::valueTypeFloat);
+    Fact _currentSeventhFact = Fact(0, QStringLiteral("current7"), FactMetaData::valueTypeFloat);
+    Fact _currentEighthFact = Fact(0, QStringLiteral("current8"), FactMetaData::valueTypeFloat);
 
     Fact _voltageFirstFact = Fact(0, QStringLiteral("voltage1"), FactMetaData::valueTypeFloat);
     Fact _voltageSecondFact = Fact(0, QStringLiteral("voltage2"), FactMetaData::valueTypeFloat);
     Fact _voltageThirdFact = Fact(0, QStringLiteral("voltage3"), FactMetaData::valueTypeFloat);
     Fact _voltageFourthFact = Fact(0, QStringLiteral("voltage4"), FactMetaData::valueTypeFloat);
+    Fact _voltageFifthFact = Fact(0, QStringLiteral("voltage5"), FactMetaData::valueTypeFloat);
+    Fact _voltageSixthFact = Fact(0, QStringLiteral("voltage6"), FactMetaData::valueTypeFloat);
+    Fact _voltageSeventhFact = Fact(0, QStringLiteral("voltage7"), FactMetaData::valueTypeFloat);
+    Fact _voltageEighthFact = Fact(0, QStringLiteral("voltage8"), FactMetaData::valueTypeFloat);
+
+    Fact _temperatureFirstFact = Fact(0, QStringLiteral("temperature1"), FactMetaData::valueTypeFloat);
+    Fact _temperatureSecondFact = Fact(0, QStringLiteral("temperature2"), FactMetaData::valueTypeFloat);
+    Fact _temperatureThirdFact = Fact(0, QStringLiteral("temperature3"), FactMetaData::valueTypeFloat);
+    Fact _temperatureFourthFact = Fact(0, QStringLiteral("temperature4"), FactMetaData::valueTypeFloat);
+    Fact _temperatureFifthFact = Fact(0, QStringLiteral("temperature5"), FactMetaData::valueTypeFloat);
+    Fact _temperatureSixthFact = Fact(0, QStringLiteral("temperature6"), FactMetaData::valueTypeFloat);
+    Fact _temperatureSeventhFact = Fact(0, QStringLiteral("temperature7"), FactMetaData::valueTypeFloat);
+    Fact _temperatureEighthFact = Fact(0, QStringLiteral("temperature8"), FactMetaData::valueTypeFloat);
+
+    Fact _errorCountFirstFact = Fact(0, QStringLiteral("errorCount1"), FactMetaData::valueTypeUint32);
+    Fact _errorCountSecondFact = Fact(0, QStringLiteral("errorCount2"), FactMetaData::valueTypeUint32);
+    Fact _errorCountThirdFact = Fact(0, QStringLiteral("errorCount3"), FactMetaData::valueTypeUint32);
+    Fact _errorCountFourthFact = Fact(0, QStringLiteral("errorCount4"), FactMetaData::valueTypeUint32);
+    Fact _errorCountFifthFact = Fact(0, QStringLiteral("errorCount5"), FactMetaData::valueTypeUint32);
+    Fact _errorCountSixthFact = Fact(0, QStringLiteral("errorCount6"), FactMetaData::valueTypeUint32);
+    Fact _errorCountSeventhFact = Fact(0, QStringLiteral("errorCount7"), FactMetaData::valueTypeUint32);
+    Fact _errorCountEighthFact = Fact(0, QStringLiteral("errorCount8"), FactMetaData::valueTypeUint32);
 };
