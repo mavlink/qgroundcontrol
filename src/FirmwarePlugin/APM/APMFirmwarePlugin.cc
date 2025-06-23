1d64724c7c54f7a7bdb5ddae4bd1f34a67ada026
vogiahuy257 <vogiahuy257@gmail.com>
2025-06-23 14:47:33 +0700

    update

--- src/FirmwarePlugin/APM/APMFirmwarePlugin.cc
+++ src/FirmwarePlugin/APM/APMFirmwarePlugin.cc
@@ -596,7 +596,7 @@ QObject *APMFirmwarePlugin::_loadParameterMetaData(const QString &metaDataFile)
 {
     Q_UNUSED(metaDataFile);
 
-    APMParameterMetaData *const metaData = new APMParameterMetaData(this);
+    APMParameterMetaData *const metaData = new APMParameterMetaData();
     metaData->loadParameterFactMetaDataFile(metaDataFile);
     return metaData;
 }
