10dc9f46e6d2c6db067fd87b9c5cb03b984cad05
Holden Ramsey <htramsey98@gmail.com>
2025-07-01 16:00:21 -0400

    Linux: Update Deployment

--- custom-example/cmake/CustomOverrides.cmake
+++ custom-example/cmake/CustomOverrides.cmake
@@ -1,7 +1,10 @@
 set(QGC_APP_NAME "Custom-QGroundControl" CACHE STRING "App Name" FORCE)
 
 set(QGC_MACOS_ICON_PATH "${CMAKE_SOURCE_DIR}/custom/res" CACHE PATH "MacOS Icon Path" FORCE)
-set(QGC_APPIMAGE_ICON_PATH "${CMAKE_SOURCE_DIR}/custom/res/icons/custom_qgroundcontrol.png" CACHE FILEPATH "AppImage Icon Path" FORCE)
+
+if(EXISTS "${CMAKE_SOURCE_DIR}/custom/res/icons/custom_qgroundcontrol.svg")
+    set(QGC_APPIMAGE_ICON_SCALABLE_PATH "${CMAKE_SOURCE_DIR}/custom/res/icons/custom_qgroundcontrol.svg" CACHE FILEPATH "AppImage Icon SVG Path" FORCE)
+endif()
 
 if(EXISTS ${CMAKE_SOURCE_DIR}/custom/deploy/windows/installheader.bmp)
     set(QGC_WINDOWS_INSTALL_HEADER_PATH "${CMAKE_SOURCE_DIR}/custom/deploy/windows/installheader.bmp" CACHE FILEPATH "Windows Install Header Path" FORCE)
