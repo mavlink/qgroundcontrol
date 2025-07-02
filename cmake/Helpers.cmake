ca1898bd0606bbd37fe63a49ecb15afea2d9dac6
Holden Ramsey <htramsey98@gmail.com>
2025-07-02 18:07:23 -0400

    CMake: Small Caching Update

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -9,7 +9,36 @@ function(qgc_set_qt_resource_alias)
 endfunction()
 
 function(qgc_config_caching)
-    if(WIN32)
+    find_program(CCACHE_PROGRAM ccache)
+    if(CCACHE_PROGRAM)
+        execute_process(
+            COMMAND "${CCACHE_PROGRAM}" --version
+            RESULT_VARIABLE CCACHE_OK
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+        if(CCACHE_OK EQUAL 0)
+            message(STATUS "QGC: Using ccache ${CCACHE_PROGRAM}")
+
+            # set(ENV{CCACHE_CONFIGPATH} "${CMAKE_SOURCE_DIR}/tools/ccache.conf")
+            # set(ENV{CCACHE_DIR} "${CMAKE_SOURCE_DIR}/.ccache")
+            set(ENV{CCACHE_BASEDIR} "${CMAKE_SOURCE_DIR}")
+            set(ENV{CCACHE_COMPRESS} "true")
+            set(ENV{CCACHE_COMPRESSLEVEL "6")
+            # set(ENV{CCACHE_NOHASHDIR} "true")
+            # set(ENV{CCACHE_COMPILERCHECK} "content")
+            set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_mtime,include_file_ctime")
+
+            set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "C compiler launcher")
+            set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "CXX compiler launcher")
+            set(CMAKE_C_LINKER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "C linker cache used")
+            set(CMAKE_CXX_LINKER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "CXX linker cache used")
+
+            if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+                add_compile_options(-Xclang -fno-pch-timestamp)
+            endif()
+        endif()
+    else()
         find_program(SCCACHE_PROGRAM sccache)
         if(SCCACHE_PROGRAM)
             execute_process(
@@ -25,33 +54,6 @@ function(qgc_config_caching)
                 # set(CMAKE_C_LINKER_LAUNCHER "${SCCACHE_PROGRAM}" CACHE STRING "C linker cache used")
                 set(CMAKE_CXX_COMPILER_LAUNCHER "${SCCACHE_PROGRAM}" CACHE STRING "CXX compiler launcher")
                 # set(CMAKE_CXX_LINKER_LAUNCHER "${SCCACHE_PROGRAM}" CACHE STRING "CXX linker cache used")
-            endif()
-        endif()
-    else()
-        find_program(CCACHE_PROGRAM ccache)
-        if(CCACHE_PROGRAM)
-            execute_process(
-                COMMAND "${CCACHE_PROGRAM}" --version
-                RESULT_VARIABLE CCACHE_OK
-                OUTPUT_QUIET
-                ERROR_QUIET
-            )
-            if(CCACHE_OK EQUAL 0)
-                message(STATUS "QGC: Using ccache ${CCACHE_PROGRAM}")
-
-                set(ENV{CCACHE_BASEDIR} "${CMAKE_SOURCE_DIR}")
-                set(ENV{CCACHE_HASHDIR} "false")
-                set(ENV{CCACHE_COMPILER_CHECK} "content")
-                set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_mtime,include_file_ctime")
-
-                set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "C compiler launcher")
-                set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "CXX compiler launcher")
-                set(CMAKE_C_LINKER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "C linker cache used")
-                set(CMAKE_CXX_LINKER_LAUNCHER "${CCACHE_PROGRAM}" CACHE STRING "CXX linker cache used")
-
-                if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-                    add_compile_options(-Xclang -fno-pch-timestamp)
-                endif()
             endif()
         endif()
     endif()
