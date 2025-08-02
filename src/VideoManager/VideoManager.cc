15f61a16d3084b1d0bc583eab199f684b62ace1d
Julian Oes <julian@oes.ch>
2025-08-02 16:52:02 +1200

    Camera discovery fixups (#13197)
    * VideoManager: fix crash on camera disconnect
    
    This fixes a segfault in:
    
    src/VideoManager/VideoManager.cc:551
    MavlinkCameraControl *pCamera = _activeVehicle->cameraManager()->currentCameraInstance();
    
    * FlightMap: drive-by bugfix
    
    ---------
    
    Co-authored-by: Holden Ramsey <68555040+HTRamsey@users.noreply.github.com>

--- src/VideoManager/VideoManager.cc
+++ src/VideoManager/VideoManager.cc
@@ -563,17 +563,23 @@ void VideoManager::_setActiveVehicle(Vehicle *vehicle)
     _activeVehicle = vehicle;
     if (_activeVehicle) {
         (void) connect(_activeVehicle->vehicleLinkManager(), &VehicleLinkManager::communicationLostChanged, this, &VideoManager::_communicationLostChanged);
-        (void) connect(_activeVehicle->cameraManager(), &QGCCameraManager::streamChanged, this, &VideoManager::_videoSourceChanged);
-        MavlinkCameraControl *pCamera = _activeVehicle->cameraManager()->currentCameraInstance();
-        if (pCamera) {
-            pCamera->resumeStream();
+        if (_activeVehicle->cameraManager()) {
+            (void) connect(_activeVehicle->cameraManager(), &QGCCameraManager::streamChanged, this, &VideoManager::_videoSourceChanged);
+            MavlinkCameraControl *pCamera = _activeVehicle->cameraManager()->currentCameraInstance();
+            if (pCamera) {
+                pCamera->resumeStream();
+            }
         }
 
         for (VideoReceiver *receiver : std::as_const(_videoReceivers)) {
-            if (receiver->isThermal()) {
-                receiver->setVideoStreamInfo(_activeVehicle->cameraManager()->thermalStreamInstance());
+            if (_activeVehicle->cameraManager()) {
+                if (receiver->isThermal()) {
+                    receiver->setVideoStreamInfo(_activeVehicle->cameraManager()->thermalStreamInstance());
+                } else {
+                    receiver->setVideoStreamInfo(_activeVehicle->cameraManager()->currentStreamInstance());
+                }
             } else {
-                receiver->setVideoStreamInfo(_activeVehicle->cameraManager()->currentStreamInstance());
+                receiver->setVideoStreamInfo(nullptr);
             }
             // connect(receiver->videoStreamInfo(), &QGCVideoStreamInfo::infoChanged, ))
         }
