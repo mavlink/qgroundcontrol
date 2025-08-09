40393fb3a5ba28b17c97d3d106104f7f8c8bb442
Holden <htramsey98@gmail.com>
2025-08-08 22:46:27 -0400

    Utilities: Implement QCommandLineParser

--- src/main.cc
+++ src/main.cc
@@ -7,15 +7,19 @@
  *
  ****************************************************************************/
 
+#include <QtCore/QCoreApplication>
+#include <QtCore/QString>
 #include <QtQuick/QQuickWindow>
 #include <QtWidgets/QApplication>
 
 #include "QGCApplication.h"
+#include "QGCCommandLineParser.h"
 #include "QGCLogging.h"
-#include "CmdLineOptParser.h"
-#include "SettingsManager.h"
 #include "MavlinkSettings.h"
+#ifdef Q_OS_MACOS
 #include "Platform.h"
+#endif
+#include "SettingsManager.h"
 
 #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
     #include <QtWidgets/QMessageBox>
@@ -34,9 +38,11 @@
 
 #ifdef QGC_UNITTEST_BUILD
     #include "UnitTestList.h"
+    #ifdef Q_OS_WIN
+        #include <windows.h>
+    #endif
 #endif
 
-//-----------------------------------------------------------------------------
 /**
  * @brief Starts the application
  *
@@ -44,47 +50,88 @@
  * @param argv Commandline arguments
  * @return exit code, 0 for normal exit and !=0 for error cases
  */
-
 int main(int argc, char *argv[])
 {
-    bool runUnitTests = false;
-    bool simpleBootTest = false;
-    QString systemIdStr = QString();
-    bool hasSystemId = false;
-    bool bypassRunGuard = false;
+    QCoreApplication pre(argc, argv);
+    QCoreApplication::setApplicationName(QStringLiteral(QGC_APP_NAME));
+    QCoreApplication::setOrganizationName(QStringLiteral(QGC_ORG_NAME));
+    QCoreApplication::setOrganizationDomain(QStringLiteral(QGC_ORG_DOMAIN));
+    QCoreApplication::setApplicationVersion(QStringLiteral(QGC_APP_VERSION_STR));
 
-    bool stressUnitTests = false;       // Stress test unit tests
-    bool quietWindowsAsserts = false;   // Don't let asserts pop dialog boxes
-    QString unitTestOptions;
+    QGCCommandLineParser::CommandLineParseResult cli;
+    QGCCommandLineParser::parseCommandLine(QCoreApplication::arguments(), cli);
 
-    CmdLineOpt_t rgCmdLineOptions[] = {
-#ifdef QT_DEBUG
-        { "--unittest",             &runUnitTests,          &unitTestOptions },
-        { "--unittest-stress",      &stressUnitTests,       &unitTestOptions },
-        { "--no-windows-assert-ui", &quietWindowsAsserts,   nullptr },
-        { "--allow-multiple",       &bypassRunGuard,        nullptr },
+    if (cli.statusCode == QGCCommandLineParser::CommandLineParseResult::Status::HelpRequested) {
+        fputs(cli.helpText.toLocal8Bit().constData(), stdout);
+        return 0;
+    } else if (cli.statusCode == QGCCommandLineParser::CommandLineParseResult::Status::VersionRequested) {
+        fputs((cli.versionText + "\n").toLocal8Bit().constData(), stdout);
+        return 0;
+    } else if (cli.statusCode == QGCCommandLineParser::CommandLineParseResult::Status::Error) {
+        fputs((cli.errorString + "\n").toLocal8Bit().constData(), stderr);
+        return 1;
+    }
+
+#ifdef Q_OS_UNIX
+    if (!qEnvironmentVariableIsSet("QT_ASSUME_STDERR_HAS_CONSOLE")) {
+        qputenv("QT_ASSUME_STDERR_HAS_CONSOLE", "1");
+    }
+    if (!qEnvironmentVariableIsSet("QT_FORCE_STDERR_LOGGING")) {
+        qputenv("QT_FORCE_STDERR_LOGGING", "1");
+    }
 #endif
-        { "--system-id",            &hasSystemId,           &systemIdStr },
-        { "--simple-boot-test",     &simpleBootTest,        nullptr },
-        // Add additional command line option flags here
-    };
+#ifdef Q_OS_WIN
+    if (!qEnvironmentVariableIsSet("QT_WIN_DEBUG_CONSOLE")) {
+        qputenv("QT_WIN_DEBUG_CONSOLE", "attach");
+    }
+    if (cli.useDesktopGL) {
+        QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    }
+    if (cli.useSwRast) {
+        QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+    }
+    // (void) qputenv("QT_OPENGL_BUGLIST", ":/opengl/resources/opengl/buglist.json");
+#endif
 
-    ParseCmdLineOptions(argc, argv, rgCmdLineOptions, std::size(rgCmdLineOptions), false);
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    // QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+
+    QGCLogging::installHandler(
+#if defined(Q_OS_WIN) && defined(QT_DEBUG)
+        cli.quietWindowsAsserts
+#else
+        false
+#endif
+    );
+
+#ifdef QGC_UNITTEST_BUILD
+    if (cli.stressUnitTests) {
+        cli.runningUnitTests = true;
+    }
+#ifdef Q_OS_WIN
+    if (cli.runningUnitTests) {
+        const DWORD dwMode = SetErrorMode(SEM_NOGPFAULTERRORBOX);
+        SetErrorMode(dwMode | SEM_NOGPFAULTERRORBOX);
+    }
+#endif
+#endif
+
+#ifdef Q_OS_MACOS
+    Platform::disableAppNapViaInfoDict();
+#endif
+
+    QGCApplication app(argc, argv, cli);
 
 #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
-    // We make the runguard key different for custom and non custom
-    // builds, so they can be executed together in the same device.
-    // Stable and Daily have same QGC_APP_NAME so they would
-    // not be able to run at the same time
-    const QString runguardString = QStringLiteral("%1 RunGuardKey").arg(QGC_APP_NAME);
-
-    RunGuard guard(runguardString);
-    if (!bypassRunGuard && !guard.tryToRun()) {
-        const QApplication errorApp(argc, argv);
-        (void) QMessageBox::critical(nullptr, QObject::tr("Error"),
-            QObject::tr("A second instance of %1 is already running. Please close the other instance and try again.").arg(QGC_APP_NAME)
-        );
-        return -1;
+    {
+        const QString runguardString = QStringLiteral("%1 RunGuardKey").arg(QGC_APP_NAME);
+        RunGuard guard(runguardString);
+        if (!cli.allowMultiple && !guard.tryToRun()) {
+            const QApplication errorApp(argc, argv);
+            (void) QMessageBox::critical(nullptr, QObject::tr("Error"),
+                QObject::tr("A second instance of %1 is already running. Please close the other instance and try again.").arg(QGC_APP_NAME));
+            return -1;
+        }
     }
 #endif
 
@@ -93,100 +140,39 @@ int main(int argc, char *argv[])
         const QApplication errorApp(argc, argv);
         (void) QMessageBox::critical(nullptr, QObject::tr("Error"),
             QObject::tr("You are running %1 as root. "
-                "You should not do this since it will cause other issues with %1."
-                "%1 will now exit.<br/><br/>").arg(QGC_APP_NAME)
-        );
+                        "You should not do this since it will cause other issues with %1."
+                        "%1 will now exit.<br/><br/>").arg(QGC_APP_NAME));
         return -1;
     }
-#endif
-
-#ifdef Q_OS_UNIX
-    if (!qEnvironmentVariableIsSet("QT_ASSUME_STDERR_HAS_CONSOLE")) {
-        (void) qputenv("QT_ASSUME_STDERR_HAS_CONSOLE", "1");
-    }
-
-    if (!qEnvironmentVariableIsSet("QT_FORCE_STDERR_LOGGING")) {
-        (void) qputenv("QT_FORCE_STDERR_LOGGING", "1");
-    }
-#endif
-
-#ifdef Q_OS_WIN
-    if (!qEnvironmentVariableIsSet("QT_WIN_DEBUG_CONSOLE")) {
-        (void) qputenv("QT_WIN_DEBUG_CONSOLE", "attach"); // new
-    }
-#endif
-
-    QGCLogging::installHandler(quietWindowsAsserts);
-
-#ifdef QGC_UNITTEST_BUILD
-    if (stressUnitTests) {
-        runUnitTests = true;
-    }
-#endif
-
-#ifdef Q_OS_MACOS
-    Platform::disableAppNapViaInfoDict();
-#endif
-
-#ifdef Q_OS_WIN
-    // Set our own OpenGL buglist
-    // (void) qputenv("QT_OPENGL_BUGLIST", ":/opengl/resources/opengl/buglist.json");
-
-    // Allow for command line override of renderer
-    for (int i = 0; i < argc; i++) {
-        const QString arg(argv[i]);
-        if (arg == QStringLiteral("-desktop")) {
-            QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-            break;
-        } else if (arg == QStringLiteral("-swrast")) {
-            QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
-            break;
-        }
-    }
-
-#ifdef QGC_UNITTEST_BUILD
-    if (runUnitTests) {
-        // Don't pop up Windows Error Reporting dialog when app crashes.
-        const DWORD dwMode = SetErrorMode(SEM_NOGPFAULTERRORBOX);
-        SetErrorMode(dwMode | SEM_NOGPFAULTERRORBOX);
-    }
-#endif
-#endif // Q_OS_WIN
-
-    QGCApplication app(argc, argv, runUnitTests, simpleBootTest);
-
-#if defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
     SignalHandler::instance();
     (void) SignalHandler::setupSignalHandlers();
 #endif
 
     app.init();
 
-    // Set system ID if specified via command line, for example --system-id:255
-    if (hasSystemId) {
-        bool ok;
-        const int systemId = systemIdStr.toInt(&ok);
-        if (ok && (systemId >= 1) && (systemId <= 255)) {  // MAVLink system IDs are 8-bit
+    if (cli.hasSystemId) {
+        bool ok = false;
+        const int systemId = cli.systemIdStr.toInt(&ok);
+        if (ok && (systemId >= 1) && (systemId <= 255)) {
             qDebug() << "Setting MAVLink System ID to:" << systemId;
             SettingsManager::instance()->mavlinkSettings()->gcsMavlinkSystemID()->setRawValue(systemId);
         } else {
-            qDebug() << "Not setting MAVLink System ID. It must be between 0 and 255. Invalid system ID value:" << systemIdStr;
+            qDebug() << "Not setting MAVLink System ID. It must be between 0 and 255. Invalid system ID value:" << cli.systemIdStr;
         }
     }
 
     int exitCode = 0;
 
 #ifdef QGC_UNITTEST_BUILD
-    if (runUnitTests) {
-        exitCode = runTests(stressUnitTests, unitTestOptions);
+    if (cli.runningUnitTests) {
+        exitCode = runTests(cli.stressUnitTests, cli.unitTests.join(' '));
     } else
 #endif
     {
-        #ifdef Q_OS_ANDROID
-            AndroidInterface::checkStoragePermissions();
-        #endif
-
-        if (!simpleBootTest) {
+#ifdef Q_OS_ANDROID
+        (void) AndroidInterface::checkStoragePermissions();
+#endif
+        if (!cli.simpleBootTest) {
             exitCode = app.exec();
         }
     }
