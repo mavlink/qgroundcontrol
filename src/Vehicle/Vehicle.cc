0ffd22717ab5e72fd9748fcdfa072003dedcfd96
Pedro-Roque <padr@kth.se>
2025-08-20 10:40:32 -0700

    feat: add spacecraft on vehicle and QGCMAVLink. Also fixed string for proper spacing.

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -1948,6 +1948,11 @@ bool Vehicle::sub() const
     return QGCMAVLink::isSub(vehicleType());
 }
 
+bool Vehicle::spacecraft() const
+{
+    return QGCMAVLink::isSpacecraft(vehicleType());
+}
+
 bool Vehicle::multiRotor() const
 {
     return QGCMAVLink::isMultiRotor(vehicleType());
