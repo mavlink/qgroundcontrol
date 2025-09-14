c2bf5acf706414d0a03a5c00a6b680a063f6c3f7
Holden <htramsey98@gmail.com>
2025-09-13 21:42:04 -0400

    CMake: Fix MacOS Create DMG

--- cmake/install/CreateMacDMG.cmake
+++ cmake/install/CreateMacDMG.cmake
@@ -2,22 +2,7 @@
 # QGC_STAGING_BUNDLE_PATH => full path to MyApp.app
 
 # ---------------------------------------------------------------------------
-# 1. Grab or locate create-dmg
-# ---------------------------------------------------------------------------
-find_program(CREATE_DMG_PROGRAM create-dmg)
-if(NOT CREATE_DMG_PROGRAM)
-    message(STATUS "QGC: Downloading create-dmg")
-    CPMAddPackage(
-        NAME create-dmg
-        GITHUB_REPOSITORY create-dmg/create-dmg
-        GIT_TAG master
-        DOWNLOAD_ONLY
-    )
-    set(CREATE_DMG_PROGRAM "${create-dmg_SOURCE_DIR}/create-dmg")
-endif()
-
-# ---------------------------------------------------------------------------
-# 2. Build the DMG with a nice drag-and-drop layout
+# 1. Build the DMG with a nice drag-and-drop layout
 # ---------------------------------------------------------------------------
 set(QGC_DMG_PATH "${CMAKE_BINARY_DIR}/package")
 file(REMOVE_RECURSE "${QGC_DMG_PATH}")
@@ -28,6 +13,7 @@ cmake_path(GET QGC_STAGING_BUNDLE_PATH STEM QGC_TARGET_APP_NAME)
 
 set(QGC_DMG_NAME "${QGC_TARGET_APP_NAME}.dmg")
 
+find_program(CREATE_DMG_PROGRAM create-dmg REQUIRED)
 execute_process(
     COMMAND "${CREATE_DMG_PROGRAM}"
             --volname "${QGC_TARGET_APP_NAME}"
