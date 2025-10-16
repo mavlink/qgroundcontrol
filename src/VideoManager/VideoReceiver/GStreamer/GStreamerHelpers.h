f1f073cac97be0139411bd65e1eee357056363ac
Don Gagne <don@thegagnes.com>
2025-10-16 12:52:15 -0700

    Video: Added ability for force hardware decoder usage
    * Added setting to force hardware decoder usage
    * Changed forced ranking of software/hardware decoders to walk installed plugins instead of use hardcoded list
    * Added Video.GStreamerDecoderRanks category which will log the default decoder priority ranking in rank order
    * Added much better logging to various places

--- src/VideoManager/VideoReceiver/GStreamer/GStreamerHelpers.h
+++ src/VideoManager/VideoReceiver/GStreamer/GStreamerHelpers.h
@@ -10,8 +10,14 @@
 #pragma once
 
 #include <glib.h>
+// Needed for GstElementFactory
+#include <gst/gst.h>
 
 namespace GStreamer
 {
     gboolean is_valid_rtsp_uri(const gchar *uri_str);
+
+    // Returns true if the given factory likely represents a hardware-accelerated decoder.
+    // Heuristics: checks metadata/klass for "Hardware" and common vendor tags in the factory name.
+    bool is_hardware_decoder_factory(GstElementFactory *factory);
 }
