c4b97cbe3aa6ce2bc955b7e6608c8ff3bb2ad70b
Don Gagne <don@thegagnes.com>
2025-10-25 12:53:47 -0700

    ParameterManager: Converted PARAM_REQUEST_READ support to QGCStateMachine
    * Converted PARAM_REQUEST_READ support to QGCStateMachine
    * Fixed up QGCStateMachine logging to be more readable
    * Add unit tests for new PARAM_REQUEST_READ support

--- src/FactSystem/ParameterManager.h
+++ src/FactSystem/ParameterManager.h
@@ -100,7 +100,8 @@ public:
 
     // These are public for creating unit tests
     static constexpr int kParamSetRetryCount = 2;                   ///< Number of retries for PARAM_SET
-    static constexpr int kParamSetWaitForParamValueAckMs = 1000;    ///< Time to wait for param value ack after set param
+    static constexpr int kParamRequestReadRetryCount = 2;           ///< Number of retries for PARAM_REQUEST_READ
+    static constexpr int kWaitForParamValueAckMs = 1000;    ///< Time to wait for param value ack after set param
 
 signals:
     void parametersReadyChanged(bool parametersReady);
@@ -109,6 +110,12 @@ signals:
     void pendingWritesChanged(bool pendingWrites);
     void factAdded(int componentId, Fact *fact);
 
+    // These signals are used to verify unit tests
+    void _paramSetSuccess(int componentId, const QString &paramName);
+    void _paramSetFailure(int componentId, const QString &paramName);
+    void _paramRequestReadSuccess(int componentId, const QString &paramName, int paramIndex);
+    void _paramRequestReadFailure(int componentId, const QString &paramName, int paramIndex);
+
 private slots:
     void _factRawValueUpdated(const QVariant &rawValue);
 
@@ -116,13 +123,13 @@ private:
     /// Called whenever a parameter is updated or first seen.
     void _handleParamValue(int componentId, const QString &parameterName, int parameterCount, int parameterIndex, MAV_PARAM_TYPE mavParamType, const QVariant &parameterValue);
      /// Writes the parameter update to mavlink, sets up for write wait
-    void _factRawValueUpdateWorker(int componentId, const QString &name, FactMetaData::ValueType_t valueType, const QVariant &rawValue);
+    void _mavlinkParamSet(int componentId, const QString &name, FactMetaData::ValueType_t valueType, const QVariant &rawValue);
     void _waitingParamTimeout();
     void _tryCacheLookup();
     void _initialRequestTimeout();
     /// Translates ParameterManager::defaultComponentId to real component id if needed
     int _actualComponentId(int componentId) const;
-    void _readParameterRaw(int componentId, const QString &paramName, int paramIndex) const;
+    void _mavlinkParamRequestRead(int componentId, const QString &paramName, int paramIndex, bool notifyFailure);
     void _writeLocalParamCache(int vehicleId, int componentId);
     void _tryCacheHashLoad(int vehicleId, int componentId, const QVariant &hashValue);
     void _loadMetaData();
@@ -173,16 +180,12 @@ private:
 
     // Wait counts from previous parameter update cycle
     int _prevWaitingReadParamIndexCount = 0;
-    int _prevWaitingReadParamNameCount = 0;
-    int _prevWaitingWriteParamNameCount = 0;
 
     bool _readParamIndexProgressActive = false;
-    bool _readParamNameProgressActive = false;
 
     static constexpr int _maxInitialRequestListRetry = 4;       ///< Maximum retries for request list
     int _initialRequestRetryCount = 0;                          ///< Current retry count for request list
     static constexpr int _maxInitialLoadRetrySingleParam = 5;   ///< Maximum retries for initial index based load of a single param
-    static constexpr int _maxReadWriteRetry = 5;                ///< Maximum retries read/write
     bool _disableAllRetries = false;                            ///< true: Don't retry any requests (used for testing and logReplay)
 
     bool _indexBatchQueueActive = false;    ///< true: we are actively batching re-requests for missing index base params, false: index based re-request has not yet started
@@ -190,11 +193,9 @@ private:
 
     QMap<int, int> _paramCountMap;                              ///< Key: Component id, Value: count of parameters in this component
     QMap<int, QMap<int, int>> _waitingReadParamIndexMap;        ///< Key: Component id, Value: Map { Key: parameter index still waiting for, Value: retry count }
-    QMap<int, QMap<QString, int>> _waitingReadParamNameMap;     ///< Key: Component id, Value: Map { Key: parameter name still waiting for, Value: retry count }
     QMap<int, QList<int>> _failedReadParamIndexMap;             ///< Key: Component id, Value: failed parameter index
 
     int _totalParamCount = 0;                   ///< Number of parameters across all components
-    int _waitingReadParamNameBatchCount = 0;    ///< Number of parameters which are batched up waiting on read responses
     int _pendingWritesCount = 0;                ///< Number of parameters with pending writes
 
     QTimer _initialRequestTimeoutTimer;
