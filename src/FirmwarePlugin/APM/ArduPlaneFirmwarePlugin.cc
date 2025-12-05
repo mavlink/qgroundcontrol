365cd6cad5ce98a381d0feb8d76b2b8909b8bfbf
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2025-12-05 14:14:39 +0100

    Refer to ArduPlane RTL altitude param using mapping in FirmwarePlugin

--- src/FirmwarePlugin/APM/ArduPlaneFirmwarePlugin.cc
+++ src/FirmwarePlugin/APM/ArduPlaneFirmwarePlugin.cc
@@ -82,6 +82,8 @@ ArduPlaneFirmwarePlugin::ArduPlaneFirmwarePlugin(QObject *parent)
 
         remapV4_5["AIRSPEED_MIN"] = QStringLiteral("ARSPD_FBW_MIN");
         remapV4_5["AIRSPEED_MAX"] = QStringLiteral("ARSPD_FBW_MAX");
+        remapV4_5["RTL_ALTITUDE"] = QStringLiteral("ALT_HOLD_RTL");
+        // LAND_SPEED is only used in a Copter component
 
         _remapParamNameIntialized = true;
     }
