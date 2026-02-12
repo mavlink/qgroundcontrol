8d5ed67a5f5ffcf4f9f9f9f312f952dd8f987827
Don Gagne <don@thegagnes.com>
2026-02-11 17:11:07 -0800

    MockLink: Add configurable camera capabilities to MockConfiguration

--- src/Comms/MockLink/MockConfiguration.cc
+++ src/Comms/MockLink/MockConfiguration.cc
@@ -17,6 +17,14 @@ MockConfiguration::MockConfiguration(const MockConfiguration *copy, QObject *par
     , _enableCamera(copy->enableCamera())
     , _failureMode(copy->failureMode())
     , _incrementVehicleId(copy->incrementVehicleId())
+    , _cameraCaptureVideo(copy->cameraCaptureVideo())
+    , _cameraCaptureImage(copy->cameraCaptureImage())
+    , _cameraHasModes(copy->cameraHasModes())
+    , _cameraCanCaptureImageInVideoMode(copy->cameraCanCaptureImageInVideoMode())
+    , _cameraCanCaptureVideoInImageMode(copy->cameraCanCaptureVideoInImageMode())
+    , _cameraHasBasicZoom(copy->cameraHasBasicZoom())
+    , _cameraHasTrackingPoint(copy->cameraHasTrackingPoint())
+    , _cameraHasTrackingRectangle(copy->cameraHasTrackingRectangle())
 {
     qCDebug(MockConfigurationLog) << this;
 }
@@ -38,6 +46,14 @@ void MockConfiguration::copyFrom(const LinkConfiguration *source)
     setEnableCamera(mockLinkSource->enableCamera());
     setIncrementVehicleId(mockLinkSource->incrementVehicleId());
     setFailureMode(mockLinkSource->failureMode());
+    setCameraCaptureVideo(mockLinkSource->cameraCaptureVideo());
+    setCameraCaptureImage(mockLinkSource->cameraCaptureImage());
+    setCameraHasModes(mockLinkSource->cameraHasModes());
+    setCameraCanCaptureImageInVideoMode(mockLinkSource->cameraCanCaptureImageInVideoMode());
+    setCameraCanCaptureVideoInImageMode(mockLinkSource->cameraCanCaptureVideoInImageMode());
+    setCameraHasBasicZoom(mockLinkSource->cameraHasBasicZoom());
+    setCameraHasTrackingPoint(mockLinkSource->cameraHasTrackingPoint());
+    setCameraHasTrackingRectangle(mockLinkSource->cameraHasTrackingRectangle());
 }
 
 void MockConfiguration::loadSettings(QSettings &settings, const QString &root)
@@ -50,6 +66,14 @@ void MockConfiguration::loadSettings(QSettings &settings, const QString &root)
     setEnableCamera(settings.value(_enableCameraKey, false).toBool());
     setIncrementVehicleId(settings.value(_incrementVehicleIdKey, true).toBool());
     setFailureMode(static_cast<FailureMode_t>(settings.value(_failureModeKey, static_cast<int>(FailNone)).toInt()));
+    setCameraCaptureVideo(settings.value(_cameraCaptureVideoKey, true).toBool());
+    setCameraCaptureImage(settings.value(_cameraCaptureImageKey, true).toBool());
+    setCameraHasModes(settings.value(_cameraHasModesKey, true).toBool());
+    setCameraCanCaptureImageInVideoMode(settings.value(_cameraCanCaptureImageInVideoModeKey, true).toBool());
+    setCameraCanCaptureVideoInImageMode(settings.value(_cameraCanCaptureVideoInImageModeKey, false).toBool());
+    setCameraHasBasicZoom(settings.value(_cameraHasBasicZoomKey, true).toBool());
+    setCameraHasTrackingPoint(settings.value(_cameraHasTrackingPointKey, false).toBool());
+    setCameraHasTrackingRectangle(settings.value(_cameraHasTrackingRectangleKey, false).toBool());
 
     settings.endGroup();
 }
@@ -64,6 +88,14 @@ void MockConfiguration::saveSettings(QSettings &settings, const QString &root) c
     settings.setValue(_enableCameraKey, enableCamera());
     settings.setValue(_incrementVehicleIdKey, incrementVehicleId());
     settings.setValue(_failureModeKey, failureMode());
+    settings.setValue(_cameraCaptureVideoKey, cameraCaptureVideo());
+    settings.setValue(_cameraCaptureImageKey, cameraCaptureImage());
+    settings.setValue(_cameraHasModesKey, cameraHasModes());
+    settings.setValue(_cameraCanCaptureImageInVideoModeKey, cameraCanCaptureImageInVideoMode());
+    settings.setValue(_cameraCanCaptureVideoInImageModeKey, cameraCanCaptureVideoInImageMode());
+    settings.setValue(_cameraHasBasicZoomKey, cameraHasBasicZoom());
+    settings.setValue(_cameraHasTrackingPointKey, cameraHasTrackingPoint());
+    settings.setValue(_cameraHasTrackingRectangleKey, cameraHasTrackingRectangle());
 
     settings.endGroup();
 }
