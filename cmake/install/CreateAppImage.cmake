3c6d5ec58c451c24aefa57292424cabec36344e0
Holden Ramsey <htramsey98@gmail.com>
2026-02-14 21:03:02 -0500

    fix(ci): harden AppImage packaging on arm runners

--- cmake/install/CreateAppImage.cmake
+++ cmake/install/CreateAppImage.cmake
@@ -9,7 +9,22 @@
 message(STATUS "QGC: Creating AppImage...")
 
 set(APPDIR_PATH "${CMAKE_BINARY_DIR}/AppDir")
-set(APPIMAGE_PATH "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}-${CMAKE_SYSTEM_PROCESSOR}.AppImage")
+
+string(TOLOWER "${CMAKE_SYSTEM_PROCESSOR}" _qgc_system_processor)
+if(_qgc_system_processor STREQUAL "x86_64" OR _qgc_system_processor STREQUAL "amd64")
+    set(_qgc_appimage_arch "x86_64")
+elseif(_qgc_system_processor STREQUAL "aarch64" OR _qgc_system_processor STREQUAL "arm64")
+    set(_qgc_appimage_arch "aarch64")
+elseif(_qgc_system_processor STREQUAL "armhf" OR _qgc_system_processor STREQUAL "armv7l")
+    set(_qgc_appimage_arch "armhf")
+elseif(_qgc_system_processor STREQUAL "i686" OR _qgc_system_processor STREQUAL "i386")
+    set(_qgc_appimage_arch "i686")
+else()
+    message(FATAL_ERROR "QGC: Unsupported AppImage architecture '${CMAKE_SYSTEM_PROCESSOR}'.")
+endif()
+
+set(APPIMAGE_PATH "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}-${_qgc_appimage_arch}.AppImage")
+message(STATUS "QGC: AppImage architecture resolved to '${_qgc_appimage_arch}' from '${CMAKE_SYSTEM_PROCESSOR}'")
 
 # ============================================================================
 # Helper Functions
@@ -22,10 +37,24 @@ function(download_tool VAR URL)
     if(NOT EXISTS "${_dest}")
         file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/tools")
         message(STATUS "QGC: Downloading ${_name} to ${_dest}")
-        file(DOWNLOAD "${URL}" "${_dest}" STATUS _status TLS_VERIFY ON)
-        list(GET _status 0 _result)
-        if(NOT _result EQUAL 0)
-            message(FATAL_ERROR "Failed to download ${URL} to ${_dest}: ${_status}")
+        set(_download_ok FALSE)
+        set(_status "")
+        foreach(_attempt RANGE 1 3)
+            file(DOWNLOAD "${URL}" "${_dest}" STATUS _status TLS_VERIFY ON)
+            list(GET _status 0 _result)
+            if(_result EQUAL 0)
+                set(_download_ok TRUE)
+                break()
+            endif()
+
+            # Avoid reusing a partial download on the next retry.
+            file(REMOVE "${_dest}")
+            if(_attempt LESS 3)
+                message(WARNING "QGC: Download attempt ${_attempt}/3 failed for ${_name}: ${_status}")
+            endif()
+        endforeach()
+        if(NOT _download_ok)
+            message(FATAL_ERROR "Failed to download ${URL} to ${_dest} after 3 attempts: ${_status}")
         endif()
         file(CHMOD "${_dest}" FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
     endif()
@@ -38,12 +67,12 @@ endfunction()
 
 message(STATUS "QGC: Downloading AppImage build tools...")
 
-download_tool(LINUXDEPLOY https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20251107-1/linuxdeploy-${CMAKE_SYSTEM_PROCESSOR}.AppImage)
-download_tool(APPIMAGETOOL https://github.com/AppImage/appimagetool/releases/download/1.9.1/appimagetool-${CMAKE_SYSTEM_PROCESSOR}.AppImage)
+download_tool(LINUXDEPLOY https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20251107-1/linuxdeploy-${_qgc_appimage_arch}.AppImage)
+download_tool(APPIMAGETOOL https://github.com/AppImage/appimagetool/releases/download/1.9.1/appimagetool-${_qgc_appimage_arch}.AppImage)
 
 # AppImageLint is only available for x86_64
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
-    download_tool(APPIMAGELINT https://github.com/TheAssassin/appimagelint/releases/download/continuous/appimagelint-${CMAKE_SYSTEM_PROCESSOR}.AppImage)
+if(_qgc_appimage_arch STREQUAL "x86_64")
+    download_tool(APPIMAGELINT https://github.com/TheAssassin/appimagelint/releases/download/continuous/appimagelint-${_qgc_appimage_arch}.AppImage)
 endif()
 
 # ============================================================================
@@ -51,6 +80,9 @@ endif()
 # ============================================================================
 
 message(STATUS "QGC: Bundling runtime dependencies with linuxdeploy...")
+
+# Hosted CI runners often do not support FUSE mounts for AppImage binaries.
+set(ENV{APPIMAGE_EXTRACT_AND_RUN} 1)
 
 execute_process(
     COMMAND "${LINUXDEPLOY_PATH}"
@@ -69,7 +101,7 @@ execute_process(
 
 message(STATUS "QGC: Building AppImage package...")
 
-set(ENV{ARCH} ${CMAKE_SYSTEM_PROCESSOR})
+set(ENV{ARCH} ${_qgc_appimage_arch})
 set(ENV{VERSION} ${CMAKE_PROJECT_VERSION})
 
 execute_process(
