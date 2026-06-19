3f2eac057eeabc29c02b504a8e275b4b47e770c2
OlofNisshagen <olof@nisshagen.com>
2026-06-19 12:47:14 +0200

    Added and connected new mavlink dialect ++

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -3285,11 +3285,11 @@ void Vehicle::_handleControlStatus(const mavlink_message_t& message)
         _gcsControlStatusFlags_TakeoverAllowed = controlStatus.flags & GCS_CONTROL_STATUS_FLAGS_TAKEOVER_ALLOWED;
         updateControlStatusSignals = true;
     }
-
-    if (_gcsMain != controlStatus.gcs_main) {
+    /*if (_gcsMain != controlStatus.gcs_main) {
         _gcsMain = controlStatus.gcs_main;
         updateControlStatusSignals = true;
-    }
+    }*/
+    
 
     if (!_firstControlStatusReceived) {
         _firstControlStatusReceived = true;
