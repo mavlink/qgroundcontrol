1e77346fe4cfe171b180bf8caa909e7f5fd8d237
Anton Zhuravlyav <coolebyak@gmail.com>
2026-08-11 15:23:41 +0500

    fix(i18n): make custom flight mode fallback translatable

--- src/FirmwarePlugin/FirmwarePlugin.cc
+++ src/FirmwarePlugin/FirmwarePlugin.cc
@@ -65,7 +65,7 @@ QString FirmwarePlugin::flightMode(uint8_t base_mode, uint32_t custom_mode) cons
     if (base_mode == 0) {
         flightMode = "PreFlight";
     } else if (base_mode & MAV_MODE_FLAG_CUSTOM_MODE_ENABLED) {
-        flightMode = _modeEnumToString.value(custom_mode, QStringLiteral("Custom:0x%1").arg(custom_mode, 0, 16));
+        flightMode = _modeEnumToString.value(custom_mode, tr("Custom:0x%1").arg(custom_mode, 0, 16));
     } else {
         for (size_t i = 0; i < std::size(rgBit2Name); i++) {
             if (base_mode & rgBit2Name[i].baseModeBit) {
